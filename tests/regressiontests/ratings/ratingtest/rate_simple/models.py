56bc9819d58577920daea89df9303ab948bccda7
Honza Kral <honza.kral@gmail.com>
2007-05-29 12:36:20 +0000

    Ratings app, draft 1. Everything is working except cookie-based antispam and karma computation.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@19 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ tests/regressiontests/ratings/ratingtest/rate_simple/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+
+from nc.ratings.models import RatedManager
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    karma = models.IntegerField(default=5)
+
+class ExpensiveSampleModel(models.Model):
+    owner = models.ForeignKey(User)
+    title = models.CharField(maxlength=100)
+
+    objects = RatedManager()
+
+class CheapSampleModel(models.Model):
+    owner = models.ForeignKey(User)
+    title = models.CharField(maxlength=100)
+
+    objects = RatedManager()
---
+++ tests/regressiontests/ratings/ratingtest/rate_simple/tests.py
@@ -0,0 +1,78 @@
+rating = r'''
+>>> from nc.ratings.models import *
+>>> Rating.objects.count()
+0
+
+>>>
+>>> from rate_simple.models import *
+>>> cheap_obj = CheapSampleModel.objects.all()[0]
+>>> cheap_obj.owner.id
+1
+
+# rating on not-rated object:
+>>> Rating.objects.get_for_object(cheap_obj)
+0
+
+# store content_types for further use
+>>> from django.contrib.contenttypes.models import ContentType
+>>> cheap_ct = ContentType.objects.get_for_model(cheap_obj)
+>>> expensive_ct = ContentType.objects.get_for_model(ExpensiveSampleModel)
+
+>>> from django.test import Client
+>>> cl = Client()
+
+# rate cheap model up anonymously
+>>> resp = cl.get('/ratings/rate/%s/%s/up/' % (cheap_ct.id, cheap_obj.id))
+>>> resp.status_code
+302
+>>> Rating.objects.get_for_object(cheap_obj) == ANONYMOUS_KARMA
+True
+>>> csm = CheapSampleModel.objects.all()[0]
+>>> csm.rating == ANONYMOUS_KARMA
+True
+>>> resp = cl.get('/ratings/rate/%s/%s/down/' % (cheap_ct.id, cheap_obj.id))
+>>> resp.status_code
+302
+>>> Rating.objects.get_for_object(cheap_obj)
+0
+>>> Rating.objects.count()
+2
+>>> csm = CheapSampleModel.objects.all()[0]
+>>> csm.rating
+0
+'''
+
+template_tags = r'''
+>>> from nc.ratings.models import *
+>>> from rate_simple.models import *
+>>> from django.contrib.contenttypes.models import ContentType
+>>> from django.template import Template, Context
+>>> cheap_obj = CheapSampleModel.objects.all()[0]
+>>> cheap_ct = ContentType.objects.get_for_model(cheap_obj)
+
+
+>>> t = Template('{% load ratings.tags %}{% rating for obj as rating %}{{rating}}')
+>>> t.render(Context({'obj' : cheap_obj}))
+'0'
+>>> r = Rating(target_ct=cheap_ct, target_id=cheap_obj.id, amount=6)
+>>> r.save()
+>>> t.render(Context({'obj' : cheap_obj}))
+'6'
+>>> t = Template('{% load ratings.tags %}{% rate_urls for obj as rate_up rate_down %}{{rate_up}} {{rate_down}}')
+>>> t.render(Context({'obj' : cheap_obj})) == '/ratings/rate/%(ct_id)s/%(obj_id)s/up/ /ratings/rate/%(ct_id)s/%(obj_id)s/down/' % {
+...     'obj_id' : cheap_obj.id,
+...     'ct_id' : cheap_ct.id
+...}
+True
+>>> r.delete()
+'''
+
+__test__ = {
+    'rating' : rating,
+    'template_tags' : template_tags
+}
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
