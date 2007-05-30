9ff3afa488c67a285a1728b010c033460a479fab
Jakub Vysoky <jakub@borka.cz>
2007-05-30 15:21:22 +0000

    comment forms
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@22 2d143e24-0a30-0410-89d7-a2e95868dc81

--- tests/regressiontests/comments/komments/sample/models.py
+++ tests/regressiontests/comments/komments/sample/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Apple(models.Model):
     color = models.CharField(maxlength=20)
+    def __str__(self):
+        return '%s apple' % (self.color,)
 
 class Orange(models.Model):
-    weight = models.PositiveIntegerField()
+    cm = models.PositiveIntegerField()
+    def __str__(self):
+        return '%scm orange' % (self.cm,)
 
--- tests/regressiontests/comments/komments/sample/tests.py
+++ tests/regressiontests/comments/komments/sample/tests.py
@@ -8,19 +8,11 @@ ahoj
 
 
 # few init instances
->>> Orange(weight=10).save()
->>> Orange(weight=20).save()
+>>> Orange(cm=10).save()
+>>> Orange(cm=20).save()
 >>> Apple(color='green').save()
 >>> Apple(color='red').save()
 
-# dump my init test data for future move to fixtures/initial_data.json
->>> from django.conf import settings
->>> from django.core import management
-
-#>>> management.syncdb(0, False)
->>> f = file('/tmp/hovno.json', 'w')
->>> f.write(management.dump_data(['sample'], 'json', True))
->>> f.close()
 
 
 # get contenttype for model in app
@@ -37,18 +29,48 @@ True
 >>> k = cmodels.Comment(target_ct=contenttype, target_id=a1.id, content='comment on red apple')
 >>> k.save()
 >>> cmodels.Comment.objects.filter(target_ct=contenttype, target_id=a1.id)
-[<Comment: comment[1] 'comment on ...' on Apple object>]
+[<Comment: comment[1] 'comment on ...' on red apple>]
 >>> cmodels.Comment.objects.all()
-[<Comment: comment[1] 'comment on ...' on Apple object>]
+[<Comment: comment[1] 'comment on ...' on red apple>]
 
 # and to another
 >>> package, module = ('sample', 'orange')
 >>> contenttype = ContentType.objects.get(app_label__exact=package, model__exact=module)
->>> o_id = Orange.objects.get(weight=10).id
+>>> o_id = Orange.objects.get(cm=10).id
 >>> k = cmodels.Comment(target_ct=contenttype, target_id=o_id, content='content on 10kg orange')
 >>> k.save()
 >>> cmodels.Comment.objects.all()
-[<Comment: comment[1] 'comment on ...' on Apple object>, <Comment: comment[2] 'content on ...' on Orange object>]
+[<Comment: comment[1] 'comment on ...' on red apple>, <Comment: comment[2] 'content on ...' on 10cm orange>]
+
+
+# comment forms
+>>> from nc.comments import forms as cforms
+
+
+# simple rendering comment form
+>>> c = cforms.CommentForm(opts={'options':'', 'target':'9:1'})
+>>> print c.as_p()
+<p><label for="id_content">Content:</label> <textarea id="id_content" rows="10" cols="40" name="content"></textarea><input type="hidden" name="parent" id="id_parent" /><input type="hidden" name="hash" value="9631189444103e381fd27ba051f3c4b2" id="id_hash" /><input type="hidden" name="opts" id="id_opts" /><input type="hidden" name="target" value="9:1" id="id_target" /></p>
+
+# valid comment form
+>>> c = cforms.CommentForm({'hash':'9631189444103e381fd27ba051f3c4b2', 'opts':'', 'target':'9:1', 'content': 'hohoho'})
+>>> c.is_valid()
+True
+>>> newc = c.save()
+>>> cmodels.Comment.objects.all()[2]
+<Comment: comment[3] 'hohoho ...' on green apple>
+
+# invalid forms
+>>> c = cforms.CommentForm({'hash':'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 'opts':'', 'target':'9:1', 'content': 'hohoho'})
+>>> c.is_valid()
+False
+>>> c = cforms.CommentForm({'hash':'9631189444103e381fd27ba051f3c4b2', 'opts':'some:invalid:opts', 'target':'9:1', 'content': 'hohoho'})
+>>> c.is_valid()
+False
+>>> c = cforms.CommentForm({'hash':'9631189444103e381fd27ba051f3c4b2', 'opts':'', 'target':'10:5', 'content': 'hohoho'})
+>>> c.is_valid()
+False
+
 
 
 """
