7ac833717d01780baf5af81125e1586be6926e17
Honza Kral <honza.kral@gmail.com>
2007-08-05 18:36:29 +0000

    Update to keep up with unicode.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@89 2d143e24-0a30-0410-89d7-a2e95868dc81

--- tests/regressiontests/adverts/advert_test/simple_test/tests.py
+++ tests/regressiontests/adverts/advert_test/simple_test/tests.py
@@ -2,23 +2,23 @@ adverts = r'''
 >>> from django import template
 >>> t=template.Template('{%load adverts %}{% advert %}')
 >>> t.render(template.Context({}))
-'<advert >'
+u'<advert >'
 >>> t=template.Template('{%load adverts %}{% advert type "aaa" %}')
 >>> t.render(template.Context({}))
-'<advert type="aaa">'
+u'<advert type="aaa">'
 >>> t=template.Template('{%load adverts %}{% advert type aaa %}')
 >>> t.render(template.Context({}))
-'<advert type="">'
+u'<advert type="">'
 >>> t.render(template.Context({'aaa':123}))
-'<advert type="123">'
+u'<advert type="123">'
 >>> t=template.Template('{%load adverts %}{% advert type %}')
 Traceback (most recent call last):
 ...
-TemplateSyntaxError: 'advert' tag requires even number of arguments
+TemplateSyntaxError: u'advert' tag requires even number of arguments
 >>> t=template.Template('{%load adverts %}{% advert unknown_parameter "aaa" %}')
 Traceback (most recent call last):
 ...
-TemplateSyntaxError: advert tag does not accept 'unknown_parameter' parameter
+TemplateSyntaxError: advert tag does not accept u'unknown_parameter' parameter
 '''
 
 __test__ = {
