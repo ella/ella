c81a04aacfadd7998db109a9c116befd366b7e2a
Maxim Izmaylov <maxim.izmaylov@centrumholdings.com>
2007-08-21 18:04:47 +0000

    generic relation object lookups ready
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@190 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ ella/ellaadmin/admin_media/js/admin/GenericRelatedObjectLookups.js
@@ -0,0 +1,37 @@
+/* File editor.js
+ * Author: Maxim Izmaylov [izmaylov.maxim@netcentrum.cz]
+ * © 2007 NetCentrum s.r.o.
+ * Powered with jQuery javascript library
+ * Requirements: jquery.js
+ */
+
+$(function(){
+	var selects = $('select.target_ct');
+	var targets = $('input.target_id');
+	for(var x = 0; x < selects.length; x++){
+		var nLt = document.getElementById('lookup_' + targets[x].id) || false;
+		if(!nLt){
+			var newLoupe = document.createElement('img');
+			newLoupe.src = adminmediapath + 'img/admin/selector-search.gif';
+			newLoupe.style.cssText = 'cursor: pointer;';
+			newLoupe.id = 'lookup_' + targets[x].id;
+			newLoupe.alt = x;
+		} else {
+			newLoupe = nLt;
+		}
+		targets[x].parentNode.appendChild(document.createTextNode(' '));
+		targets[x].parentNode.appendChild(newLoupe);
+		$('#lookup_' + targets[x].id).click(function(){
+			var num = this.alt;
+			if(selects[num].selectedIndex > 0){
+				var name = this.id.replace(/^lookup_/, '').replace(/\./g, '___');
+				var path = paths[selects[num].value];
+				var addr = '../../../' + path + '/?pop=1';
+				var win = window.open(addr, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+				win.focus();
+			} else {
+				alert('Choose "Target ct"');
+			}
+		});
+	}
+});
