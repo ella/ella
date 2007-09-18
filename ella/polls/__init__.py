24ab3517a1c4dae8d08afa65d214f4aba06b9020
Honza Kral <honza.kral@gmail.com>
2007-09-18 17:08:23 +0000

    Moved contets and quizes directly to their detail url.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@291 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/polls/__init__.py
+++ ella/polls/__init__.py
@@ -1,15 +1,15 @@
 from ella.core.custom_urls import dispatcher
 from ella.polls.models import Quiz, Contest
 
-def contest(request, bits, context):
+def contest(request, context):
     from ella.polls.views import ContestWizard
     contest = context['object']
     return ContestWizard(contest)(request)
 
-def quiz(request, bits, context):
+def quiz(request, context):
     from ella.polls.views import QuizWizard
     quiz = context['object']
     return QuizWizard(quiz)(request)
 
-dispatcher.register('take', quiz, model=Quiz)
-dispatcher.register('contest', contest, model=Contest)
+dispatcher.register_custom_detail(Quiz, quiz)
+dispatcher.register_custom_detail(Contest, contest)
