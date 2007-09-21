f2202f775d0fb07f9e04601642278c46952a1e62
Honza Kral <honza.kral@gmail.com>
2007-09-21 11:39:54 +0000

    Gallery template filters
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@310 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ ella/galleries/templatetags/galleries.py
@@ -0,0 +1,25 @@
+import math
+
+from django import template
+
+register = template.Library()
+
+def gallery_func(func, value, param):
+    if not ',' in param:
+        raise template.TemplateSyntaxError, "gallery filters take one parameter in format COUNT_PER_PAGE,PAGE_WIDTH"
+    on_page, per_page = param.split(',')
+    try:
+        value = int(value)
+        on_page, per_page = int(on_page), int(per_page)
+    except ValueError:
+        raise template.TemplateSyntaxError, "Value and parameters to gallery filters must be integers"
+
+    return func(value, on_page, per_page)
+
+@register.filter
+def gallery_offset(value, param):
+    return gallery_func(lambda x,y,z: (math.floor((x-1) / float(y))) * z, value, param)
+
+@register.filter
+def gallery_width(value, param):
+    return gallery_func(lambda x,y,z: (math.ceil(x / float(y))) * z, value, param)
