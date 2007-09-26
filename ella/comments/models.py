555147bcf29a66c5174e81a6a0773fe4f069992d
Honza Kral <honza.kral@gmail.com>
2007-09-26 14:53:10 +0000

    Added parent as a raw_id field.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@353 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/comments/models.py
+++ ella/comments/models.py
@@ -169,6 +169,7 @@ from django import VERSION
 from django.contrib import admin
 
 class CommentsOptions(admin.ModelAdmin):
+    raw_id_fields = ('parent',)
     list_display = ('subject', 'target', 'author', 'is_public', 'path',)
     search_fields = ('subject', 'content',)
 
