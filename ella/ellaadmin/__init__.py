8a2bd95a84101f736e1623b0be54fff19e467c25
Honza Kral <honza.kral@gmail.com>
2007-11-14 08:18:19 +0000

    Useful things can now be imported directly from ella.ellaadmin
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@506 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/ellaadmin/__init__.py
+++ ella/ellaadmin/__init__.py
@@ -1,5 +1,6 @@
 from ella.ellaadmin import filterspecs
 from ella.ellaadmin.options import EllaAdminSite
+from ella.ellaadmin.models import applicable_sites, applicable_categories, has_category_permission, has_site_permission
 
 site = EllaAdminSite()
 
