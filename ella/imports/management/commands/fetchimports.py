e16c65ec8c774cc27f9f7aa43e88521c3854b6b7
Vaclav Barta <vaclav.barta@centrumholdings.com>
2007-11-19 16:25:19 +0000

    Return exit code (count of errors)
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@520 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/imports/management/commands/fetchimports.py
+++ ella/imports/management/commands/fetchimports.py
@@ -1,8 +1,11 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import NoArgsCommand
 from optparse import make_option
+import sys
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = 'Fetch all registered imports'
     def handle(self, *test_labels, **options):
         from ella.imports.models import fetch_all
-        fetch_all()
+        errors = fetch_all()
+        if errors:
+            sys.exit(errors)
