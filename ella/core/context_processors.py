b36bad58853cbaff1d1b106e7934d144d21ac2d0
Jakub Vysoky <jakub@borka.cz>
2007-12-20 15:14:26 +0000

    revision version moved into fe settings
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@579 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/core/context_processors.py
+++ ella/core/context_processors.py
@@ -6,16 +6,8 @@ def url_info(request):
     available in template code.
     """
 
-    VERSION = '$Revision$'
-    try:
-        VERSION = int(VERSION.split(' ')[1])
-    except IndexError:
-        VERSION = 1
-    except ValueError:
-        VERSION = 1
-
     return {
         'MEDIA_URL' : settings.MEDIA_URL,
-        'VERSION' : VERSION,
+        'VERSION' : settings.VERSION,
 }
 
