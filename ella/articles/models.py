aaad0189542ada93a0839d478033651af9588c43
Jakub Vysoky <jakub@borka.cz>
2008-01-03 17:53:55 +0000

    first attempt to divide admin definitions from models
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@586 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/articles/models.py
+++ ella/articles/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from django.db import models
-from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.utils.timesince import timesince
 from django.utils.translation import ugettext, ugettext_lazy as _
@@ -10,7 +9,6 @@ from ella.core.models import Category, Author, Source, Listing
 from ella.core.box import Box
 from ella.core.managers import RelatedManager
 from ella.photos.models import Photo
-from ella.ellaadmin import widgets
 
 class InfoBox(models.Model):
     title = models.CharField(_('Title'), maxlength=255)
@@ -140,50 +138,4 @@ class ArticleContents(models.Model):
 
     def __unicode__(self):
         return self.title
-
-class ArticleContentInlineOptions(admin.TabularInline):
-    model = ArticleContents
-    extra = 1
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'content':
-            kwargs['widget'] = widgets.RichTextAreaWidget
-        return super(self.__class__, self).formfield_for_dbfield(db_field, **kwargs)
-
-class InfoBoxOptions(admin.ModelAdmin):
-    list_display = ('title', 'created',)
-    date_hierarchy = 'created'
-    list_filter = ('created', 'updated',)
-    search_fields = ('title', 'content',)
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'content':
-            kwargs['widget'] = widgets.RichTextAreaWidget
-        return super(self.__class__, self).formfield_for_dbfield(db_field, **kwargs)
-
-from ella.core.admin.models import ListingInlineOptions, HitCountInlineOptions
-from tagging.models import TaggingInlineOptions
-class ArticleOptions(admin.ModelAdmin):
-    #raw_id_fields = ('authors',)
-    list_display = ('title', 'category', 'photo_thumbnail', 'created', 'article_age', 'full_url',)
-    date_hierarchy = 'created'
-    ordering = ('-created',)
-    fieldsets = (
-        (_("Article heading"), {'fields': ('title', 'upper_title', 'updated', 'slug')}),
-        (_("Article contents"), {'fields': ('perex',)}),
-        (_("Metadata"), {'fields': ('category', 'authors', 'source', 'photo')}),
-)
-    raw_id_fields = ('photo',)
-    list_filter = ('category__site', 'created', 'category', 'authors',)
-    search_fields = ('title', 'upper_title', 'perex', 'slug',)
-#    inlines = (ArticleContentInlineOptions, ListingInlineOptions, TaggingInlineOptions, HitCountInlineOptions)
-    inlines = (ArticleContentInlineOptions, ListingInlineOptions, TaggingInlineOptions,)
-    prepopulated_fields = {'slug' : ('title',)}
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'perex':
-            kwargs['widget'] = widgets.RichTextAreaWidget
-        return super(self.__class__, self).formfield_for_dbfield(db_field, **kwargs)
-
-admin.site.register(InfoBox, InfoBoxOptions)
-admin.site.register(Article, ArticleOptions)
 
