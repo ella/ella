2a7891254bd9123cf7ace2cccb18039296d09630
Jakub Vysoky <jakub@borka.cz>
2008-02-04 22:25:27 +0000

    memcache statistics for multiple machines
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@644 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/ellaadmin/memcached.py
+++ ella/ellaadmin/memcached.py
@@ -1,7 +1,7 @@
 # memcached_status.py
 # http://effbot.org/zone/django-memcached-view.htm
 
-import datetime, re
+import datetime
 
 from django import http
 from django.shortcuts import render_to_response
@@ -19,42 +19,54 @@ def cache_status(request):
         return http.HttpResponseForbidden()
 
     # get first memcached URI
-    m = re.match(
-        "memcached://([.\w]+:\d+)", settings.CACHE_BACKEND
-)
-    if not m:
+    if not settings.CACHE_BACKEND.startswith('memcached://'):
         raise http.Http404('invalid cache backend')
-
-    host = memcache._Host(m.group(1))
-    host.connect()
-    host.send_cmd("stats")
+    cache_hosts = settings.CACHE_BACKEND.lstrip('memcached://').rstrip('/')
+    cache_hosts = cache_hosts.split(';')
 
     class Stats:
         pass
+    all_stats = []
 
-    stats = Stats()
+    for h in cache_hosts:
+        host = memcache._Host(h)
 
-    while 1:
-        line = host.readline().split(None, 2)
-        if line[0] == "END":
-            break
-        stat, key, value = line
-        try:
-            # convert to native type, if possible
-            value = int(value)
-            if key == "uptime":
-                value = datetime.timedelta(seconds=value)
-            elif key == "time":
-                value = datetime.datetime.fromtimestamp(value)
-        except ValueError:
-            pass
-        setattr(stats, key, value)
+        stats = Stats()
+        stats.host = '%s:%s' % (host.ip, host.port)
 
-    host.close_socket()
+        connected = host.connect()
+        if not connected:
+            stats.connected = False
+            all_stats.append(stats)
+            continue
+
+        stats.connected = True
+        host.send_cmd("stats")
+
+        while True:
+            line = host.readline().split(None, 2)
+            if line[0] == "END":
+                break
+
+            stat, key, value = line
+            try:
+                # convert to native type, if possible
+                value = int(value)
+                if key == "uptime":
+                    value = datetime.timedelta(seconds=value)
+                elif key == "time":
+                    value = datetime.datetime.fromtimestamp(value)
+            except ValueError:
+                pass
+            setattr(stats, key, value)
+
+        stats.hit_rate = 100 * stats.get_hits / stats.cmd_get
+
+        host.close_socket()
+        all_stats.append(stats)
 
     return render_to_response(
         'ellaadmin/memcached_status.html', dict(
-            stats=stats,
-            hit_rate=100 * stats.get_hits / stats.cmd_get,
+            all_stats=all_stats,
             time=datetime.datetime.now(), # server time
 ))
