fcfe4c555cf55a45be20f3f1b65574a6d5786419
Honza Kral <honza.kral@gmail.com>
2008-02-08 14:03:17 +0000

    Added SITE_NAME variable to context_processor
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@657 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/core/context_processors.py
+++ ella/core/context_processors.py
@@ -1,4 +1,9 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
+from django.template.defaultfilters import slugify
+
+current_site = Site.objects.get_current()
+current_site_name = slugify(current_site.name)
 
 def url_info(request):
     """
@@ -8,6 +13,7 @@ def url_info(request):
 
     return {
         'MEDIA_URL' : settings.MEDIA_URL,
-        'VERSION' : getattr(settings, 'VERSION', 1)
+        'VERSION' : getattr(settings, 'VERSION', 1),
+        'SITE_NAME' : current_site_name
 }
 
