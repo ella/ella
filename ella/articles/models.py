3a0b9a3419cca6d720c205419a2c2e11938a9067
Ales Zoulek <ales.zoulek@gmail.com>
2008-02-26 22:51:50 +0000

    ella.articles.models typo, imports
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@724 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/articles/models.py
+++ ella/articles/models.py
@@ -4,12 +4,15 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.utils.timesince import timesince
 from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.safestring import mark_safe
 
 from ella.db.models import Publishable
 from ella.core.models import Category, Author, Source
 from ella.core.box import Box
 from ella.core.managers import RelatedManager
+from ella.core.cache import get_cached_object, get_cached_list
 from ella.photos.models import Photo
+
 
 class InfoBox(models.Model):
     title = models.CharField(_('Title'), max_length=255)
@@ -45,8 +48,7 @@ class Article(models.Model, Publishable):
 
     @property
     def content(self):
-        from ella.core.cache import get_cached_list
-        if not hasattr(self, '_content'):
+        if not hasattr(self, '_contents'):
             self._contents = get_cached_list(ArticleContents, article=self)
         if self._contents:
             return self._contents[0]
