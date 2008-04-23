108f896ba91e5b8d43441157782519e4157c1897
Ales Zoulek <ales.zoulek@gmail.com>
2008-04-23 12:55:01 +0000

    Tiny improvements to run.tests.sh script
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@924 2d143e24-0a30-0410-89d7-a2e95868dc81

--- tests/regressiontests/run.tests.sh
+++ tests/regressiontests/run.tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 cd $(dirname $0)
-ls */*/manage.py */manage.py | while read i; do echo $i; $i test &>/dev/null && echo OK || echo ERROR; done
+ls */*/manage.py */manage.py | while read i; do
+	echo $i
+	output=`mktemp`
+	if $i &>$output test; then
+		echo OK
+	else
+		echo ERROR
+		if [ "$1" '==' '-v' ]; then
+			cat $output
+		fi
+	fi
+	rm $output
+done
 
