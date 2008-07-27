89468e36b14e99a22511a50a2011b30879c60d54
Jakub Vysoky <jakub@borka.cz>
2008-07-27 13:25:47 +0000

    installed app register
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1232 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ ella/utils/installedapps.py
@@ -0,0 +1,40 @@
+from django.utils.itercompat import is_iterable
+from django.conf import settings
+
+
+INSTALLED_APPS_REGISTER = {}
+
+
+def register(app_name, modules):
+    """
+    simple module registering for later usage
+    we don't want to import admin.py in models.py
+    """
+    global INSTALLED_APPS_REGISTER
+    mod_list = INSTALLED_APPS_REGISTER.get(app_name, [])
+
+    if isinstance(modules, basestring):
+        mod_list.append(modules)
+    elif is_iterable(modules):
+        mod_list.extend(modules)
+
+    INSTALLED_APPS_REGISTER[app_name] = mod_list
+
+def call_modules(auto_discover=()):
+    """
+    this is called in project urls.py
+    for registering desired modules (eg.: admin.py)
+    """
+    for app in settings.INSTALLED_APPS:
+        modules = set(auto_discover)
+        if app in INSTALLED_APPS_REGISTER:
+            modules.update(INSTALLED_APPS_REGISTER[app])
+        for module in modules:
+            try:
+                imp = '%s.%s' % (app, module)
+                mod = __import__(imp, {}, {}, [''])
+                inst = getattr(mod, '__install__', lambda:None)
+                inst()
+            except ImportError:
+                pass
+
