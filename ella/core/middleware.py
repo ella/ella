ac554b008239df5d03cd34ba89ea7793fce95120
Honza Kral <honza.kral@gmail.com>
2008-07-30 18:02:45 +0000

    Cache Middleware simplification
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1268 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/core/middleware.py
+++ ella/core/middleware.py
@@ -47,50 +47,9 @@ class DoubleRenderMiddleware(object):
 
 class CacheMiddleware(CacheMiddleware):
     def process_request(self, request):
-        "Checks whether the page is already cached and returns the cached version if available."
-        if self.cache_anonymous_only:
-            assert hasattr(request, 'user'), "The Django cache middleware with CACHE_MIDDLEWARE_ANONYMOUS_ONLY=True requires authentication middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.auth.middleware.AuthenticationMiddleware' before the CacheMiddleware."
+        resp = super(CacheMiddleware, self).process_request(request)
 
-        if not request.method in ('GET', 'HEAD') or request.GET:
-            request._cache_update_cache = False
-            return None # Don't bother checking the cache.
+        if resp is None:
+            request._cache_middleware_key = get_cache_key(request, self.key_prefix)
 
-        if self.cache_anonymous_only and request.user.is_authenticated():
-            request._cache_update_cache = False
-            return None # Don't cache requests from authenticated users.
-
-        cache_key = get_cache_key(request, self.key_prefix)
-        if cache_key is None:
-            request._cache_update_cache = True
-            return None # No cache information available, need to rebuild.
-
-        cache_key = normalize_key(cache_key)
-
-        response = cache.get(cache_key, None)
-        if response is None:
-            request._cache_update_cache = True
-            return None # No cache information available, need to rebuild.
-        request._cache_middleware_key = cache_key
-        request._cache_update_cache = False
-        return response
-
-    def process_response(self, request, response):
-        "Modified standard django proces response without adding Headers to browser."
-        if not hasattr(request, '_cache_update_cache') or not request._cache_update_cache:
-            # We don't need to update the cache, just return.
-            return response
-
-        if request.method != 'GET':
-            return response
-
-        if not response.status_code == 200:
-            return response
-
-        timeout = self.cache_timeout
-
-        # never cache headers + ETag
-        add_never_cache_headers(response)
-
-        cache_key = learn_cache_key(request, response, timeout, self.key_prefix)
-        cache.set(cache_key, response, timeout)
-        return response
+        return resp
