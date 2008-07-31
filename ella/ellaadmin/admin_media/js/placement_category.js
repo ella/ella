fa9773e8bfe2742dafd8d87b510fee5201ce6040
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2008-07-31 13:42:24 +0000

    Basic functionality for main category button in placement inline.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1279 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/ellaadmin/admin_media/js/placement_category.js
+++ ella/ellaadmin/admin_media/js/placement_category.js
@@ -1,21 +1,16 @@
-/* File listing.js
- * Author: Izmaylov Maxim
- * Copyright: NetCentrum @2007
- * Requirements: jquery.js
- */
-
 function placeButton(){
-	// Select <select>
+
 	var byCategory = false;
 	var byEmptiness = false;
 	var suitable = false;
-	$('select.listing_category').each(function(i){
-		// Select the <select> with exactly the same category as the "Kategorie" field
-		if(!(byCategory || byCategory === 0) && this.selectedIndex == document.getElementById('id_category').selectedIndex){
+
+	$('input#id_core-placement-target_ct-target_id-0-category').each(function(i){
+		// Select the input with exactly the same category as the "Kategorie" field
+		if(!(byCategory || byCategory === 0) && this.value == document.getElementById('id_category').value){
 			byCategory = i;
 		}
 		// Select the first empty select
-		if(!(byEmptiness || byEmptiness === 0) && this.selectedIndex == ''){
+		if(!(byEmptiness || byEmptiness === 0) && this.value == ''){
 			byEmptiness = i;
 		}
 	});
@@ -25,15 +20,15 @@ function placeButton(){
 	} else if(byEmptiness || byEmptiness === 0){
 		suitable = byEmptiness
 	}
-	suitable_select = $('select.listing_category')[suitable];
-	suitable_multi = $('#id_core-placement-target_ct-target_id-' + suitable + '-listings')[0];
+	suitable_input = $('input#id_core-placement-target_ct-target_id-0-category')[suitable];
+//	suitable_multi = $('#id_core-placement-target_ct-target_id-' + suitable + '-listings')[0];
 	// Place a button behind it
-	if(suitable_select){
-		$(suitable_select.parentNode).append(
-			$('<input type="button" class="ignore" value="&uarr; Main category">').bind('click', function(){
-				a = document.getElementById('id_category').selectedIndex
-				suitable_select.selectedIndex = a;
-				suitable_multi.selectedIndex = a-1;
+	if(suitable_input){
+		$(suitable_input.parentNode).append(
+			$('<input type="button" value="&uarr; Main">').bind('click', function(){
+				a = document.getElementById('id_category').value
+				suitable_input.value = a;
+//				suitable_multi.selectedIndex = a-1;
 			})
 		);
 	}
