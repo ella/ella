c9336073f0cc1f1f73c28bebb0fba63025de2b17
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2008-08-12 13:33:03 +0000

    Allow disabling positions. Disabled position is not active.   * TODO (via Dan): Dates from/to is not used, remove it?
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1340 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/positions/admin.py
+++ ella/positions/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from ella.positions.models import Position
 
 class PositionOptions(admin.ModelAdmin):
-    list_display = ('name', 'category', 'box_type', 'is_active', 'is_filled', 'show_title',)
-    list_filter = ('category', 'name', 'active_from', 'active_till',)
+    list_display = ('name', 'category', 'box_type', 'is_active', 'is_filled', 'show_title', 'disabled',)
+    list_filter = ('category', 'name', 'disabled', 'active_from', 'active_till',)
     search_fields = ('box_type', 'text',)
 
 admin.site.register(Position, PositionOptions)
