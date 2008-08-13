b7a11c10ef241d51211bbacaad3d6405f05b36cf
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2008-08-13 18:26:05 +0000

    Added mixin to positions, that solves problem with generic rel icon.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1351 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/positions/admin.py
+++ ella/positions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
+from ella.ellaadmin.options import EllaAdminOptionsMixin
 from ella.positions.models import Position
 
-class PositionOptions(admin.ModelAdmin):
+class PositionOptions(EllaAdminOptionsMixin, admin.ModelAdmin):
     list_display = ('name', 'category', 'box_type', 'is_active', 'is_filled', 'show_title', 'disabled',)
     list_filter = ('category', 'name', 'disabled', 'active_from', 'active_till',)
     search_fields = ('box_type', 'text',)
