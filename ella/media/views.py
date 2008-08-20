084f3f13bd78f06adca57d5478d81e6432819acc
Miloslav Pojman <miloslav.pojman@centrumholdings.com>
2008-08-20 15:12:51 +0000

    - "object" renamed to "media" in playlist template - bookmark links in playlist
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1403 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/media/views.py
+++ ella/media/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from ella.media.models import Media
 
 def player_playlist(request, slug):
-    object = get_object_or_404(Media, slug=slug)
-    response = render_to_response('playlist.xml', {'object':object})
+    media = get_object_or_404(Media, slug=slug)
+    response = render_to_response('playlist.xml', {'media':media})
     response['content-type'] = 'text/xml; charset=utf-8';
     return response
