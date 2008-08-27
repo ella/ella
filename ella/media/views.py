8c45883fd5630ea984fce145fe0027fd265d8ce1
Miloslav Pojman <miloslav.pojman@centrumholdings.com>
2008-08-27 13:04:25 +0000

    - RequestContext for playlist template
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1440 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/media/views.py
+++ ella/media/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.defaultfilters import slugify
+from django.template.context import RequestContext
 from django.utils.translation import ugettext as _
 
 from ella.media.models import Media
@@ -11,6 +12,6 @@ def player_playlist(request, slug):
         'media': media,
         'sendmail_url': '%s%s/%s/' % (media.get_absolute_url(), slugify(_('send mail')), slugify(_('xml')))
 }
-    response = render_to_response('playlist.xml', cx)
+    response = render_to_response('playlist.xml', RequestContext(request, cx))
     response['content-type'] = 'text/xml; charset=utf-8';
     return response
