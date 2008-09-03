973260510160dec179f9356c3c44222993af9d09
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2008-09-03 14:09:11 +0000

    Series admin modified.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1479 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/series/admin.py
+++ ella/series/admin.py
@@ -5,13 +5,18 @@ from ella.ellaadmin.options import EllaAdminOptionsMixin
 from ella.core.admin import PlacementInlineOptions
 from ella.tagging.admin import TaggingInlineOptions
 
+class SeriePartInlineAdmin(EllaAdminOptionsMixin, admin.TabularInline):
+    model = SeriePart
+    extra = 5
+
+
 class SerieAdmin(EllaAdminOptionsMixin, admin.ModelAdmin):
     list_display = ('title', 'started', 'is_active')
     list_filter = ('started', 'finished',)
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'perex',)
 
-    inlines = [PlacementInlineOptions, TaggingInlineOptions]
+    inlines = [PlacementInlineOptions, SeriePartInlineAdmin, TaggingInlineOptions]
 
     rich_text_fields = {None: ('perex', 'description',)}
 
