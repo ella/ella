3216e7d3ed327f7b2d070c73b51028be1fff5f21
Jonas Fiala <jonas.fiala@centrumholdings.com>
2008-09-04 12:22:23 +0000

    ella.comments updated (POST method required for preview, view moved from urls.py to views.py)
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1491 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/comments/urls.py
+++ ella/comments/urls.py
@@ -1,29 +1,7 @@
-from django.http import Http404
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
 
 from ella.core.custom_urls import dispatcher
-
-from ella.comments.views import CommentFormPreview, new_comment, list_comments
-from ella.comments.forms import CommentForm
-
-
-def comments_custom_urls(request, bits, context):
-    if len(bits) == 2:
-        if bits[0] == slugify(_('reply')) and bits[1].isdigit():
-            return new_comment(request, context, reply=int(bits[1]))
-
-    if len(bits) == 1:
-        if bits[0] == slugify(_('preview')):
-            comment_preview = CommentFormPreview(CommentForm)
-            return comment_preview(request, context)
-        elif bits[0] == slugify(_('new')):
-            return new_comment(request, context)
-
-    if len(bits) == 0:
-        return list_comments(request, context)
-
-    raise Http404
 
 dispatcher.register(slugify(_('comments')), comments_custom_urls)
 
--- ella/comments/views.py
+++ ella/comments/views.py
@@ -1,9 +1,12 @@
+import logging
+
 from django.contrib.formtools.preview import FormPreview
 from django.contrib.contenttypes.models import ContentType
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.template.defaultfilters import slugify
 
 from ella.core.cache import get_cached_object_or_404
 from ella.core.views import get_templates_from_placement
@@ -12,6 +15,7 @@ from ella.comments.models import Comment
 from ella.comments.forms import CommentForm
 from ella.comments.defaults import FORM_OPTIONS
 
+log = logging.getLogger('ella.comments')
 
 class CommentFormPreview(FormPreview):
     """comment form preview with extended template calls"""
@@ -46,6 +50,7 @@ class CommentFormPreview(FormPreview):
         return HttpResponseRedirect(url)
 
 def new_comment(request, context, reply=None):
+    import pdb;pdb.set_trace()
     """new comment for specified object"""
     init_props = {
         'target': '%d:%d' % (context['content_type'].id, context['object']._get_pk_val()),
@@ -81,4 +86,24 @@ def list_comments(request, context):
 })
     templates = get_templates_from_placement('comments/list.html', context['placement'])
     return render_to_response(templates, context, context_instance=RequestContext(request))
+
+def comments_custom_urls(request, bits, context):
+    if len(bits) == 2:
+        if bits[0] == slugify(_('reply')) and bits[1].isdigit():
+            return new_comment(request, context, reply=int(bits[1]))
+
+    if len(bits) == 1:
+        if bits[0] == slugify(_('preview')):
+            if request.method == 'GET':
+                log.warning('Preview should be used only with POST.')
+                raise Http404('Preview should be used only with POST.')
+            comment_preview = CommentFormPreview(CommentForm)
+            return comment_preview(request, context)
+        elif bits[0] == slugify(_('new')):
+            return new_comment(request, context)
+
+    if len(bits) == 0:
+        return list_comments(request, context)
+
+    raise Http404
 
