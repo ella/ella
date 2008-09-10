fe6cb3c29f3d75a652fbce7ff783c0caf4ff2a2e
Miloslav Pojman <miloslav.pojman@centrumholdings.com>
2008-09-10 09:03:16 +0000

    - deleted playlist template (it is too specific) - changed playlist path to "page/media/playlist.xml"
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1514 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/media/views.py
+++ ella/media/views.py
@@ -21,7 +21,7 @@ def player_playlist(request, bits, context):
         'object' : object,
         'sendmail_url': '%s%s/%s/' % (media.get_absolute_url(), slugify(_('send mail')), slugify(_('xml')))
 }
-    response = render_to_response('playlist.xml', RequestContext(request, cx))
+    response = render_to_response('page/media/playlist.xml', RequestContext(request, cx))
     response['content-type'] = 'text/xml; charset=utf-8';
     return response
 
