0cb3dcfcdad2250ff435e92e3b9a31f72566264a
Miloslav Pojman <miloslav.pojman@centrumholdings.com>
2008-09-11 14:41:12 +0000

    - Media admin form moved to forms.py
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1522 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/media/admin.py
+++ ella/media/admin.py
@@ -1,69 +1,8 @@
-from os import path, makedirs
-
 from django.contrib import admin
 
-from ella.media.models import Media, Section, Usage
-
-#from ella.core.admin import PlacementInlineOptions
-from ella.photos.models import Photo
-from ella.photos.imageop import get_img_size
-#from ella.tagging.admin import TaggingInlineOptions
 from ella.ellaadmin.options import EllaAdminOptionsMixin
-
-from django.conf import settings
-from django.contrib.admin import widgets
-from django.forms import ModelForm
-from django.template.defaultfilters import slugify
-
-
-class PhotoWidget(widgets.ForeignKeyRawIdWidget):
-    """
-    Widget for photo with option to generate screenshot from video
-
-    todo: more user friendly form
-    """
-    def render(self, name, value, attrs=None):
-        print value
-        return 'Generate photo <input type="checkbox" name="%s" %s /> at <input type="text" name="%s" /> second or use custom: %s' % \
-             (name + '_auto',
-              value and ' ' or 'checked="true"',
-              name + '_time',
-              super(PhotoWidget, self).render(name, value, attrs),)
-
-class MediaForm(ModelForm):
-    """
-    Generates thumb when media is saved
-    """
-    def generate_photo(self, instance):
-        dir_name = Photo._meta.get_field_by_name('image')[0].get_directory_name()
-        file_name = path.join(dir_name, 'screenshot-' + instance.file.token)
-        try:
-            makedirs(path.join(settings.MEDIA_ROOT, dir_name))
-        except OSError:
-            # Directory already exists
-            pass
-        time = None
-        if self.data.has_key('photo_time') and self.data['photo_time']:
-            time = int(self.data['photo_time'])
-        instance.file.create_thumb(path.join(settings.MEDIA_ROOT, file_name), time=time)
-        photo = Photo()
-        photo.title = "%s screenshot" % instance.title
-        photo.slug = slugify(photo.title)
-        photo.image = file_name
-        size = get_img_size(path.join(settings.MEDIA_ROOT, file_name))
-        photo.width = size['width']
-        photo.height = size['height']
-        photo.save()
-        instance.photo = photo
-
-    def save(self, commit=True):
-        instance = super(MediaForm, self).save(False)
-        if (commit and self.data.has_key('photo_auto')):
-            self.generate_photo(instance)
-        if commit:
-            instance.save()
-
-        return instance
+from ella.media.models import Media, Section, Usage
+from ella.media.forms import MediaForm
 
 class SectionInline(admin.TabularInline):
     model = Section
@@ -76,11 +15,6 @@ class MediaOptions(EllaAdminOptionsMixin, admin.ModelAdmin):
     def __init__(self, *args, **kwargs):
         super(MediaOptions, self).__init__(*args, **kwargs)
         self.form = MediaForm
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if (db_field.name == 'photo'):
-            kwargs['widget'] = PhotoWidget(db_field.rel)
-        return super(MediaOptions, self).formfield_for_dbfield(db_field, **kwargs)
 
     prepopulated_fields = {'slug' : ('title',)}
 
