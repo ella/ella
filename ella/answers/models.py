72be2183775d10da77451c870a1e3438bcc94855
Jonas Fiala <jonas.fiala@centrumholdings.com>
2008-09-17 17:28:47 +0000

    answers: data types sizes increased
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1555 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/answers/models.py
+++ ella/answers/models.py
@@ -5,9 +5,9 @@ from django.utils.text import wrap
 
 
 class Question(models.Model):
-    text = models.CharField(_(u'Question'), max_length=100)
+    text = models.CharField(_(u'Question'), max_length=200)
     specification = models.TextField(_(u'Specification'))
-    nick = models.CharField(_('Nickname'), max_length=20)
+    nick = models.CharField(_('Nickname'), max_length=150)
     created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField()
 
@@ -19,7 +19,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField(_('Answer text'))
-    nick = models.CharField(_('Nickname'), max_length=20)
+    nick = models.CharField(_('Nickname'), max_length=150) # odbornici budou udavat adresu atd.
     created = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     is_hidden = models.BooleanField(_('Is hidden'), default=False)
