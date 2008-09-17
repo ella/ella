4cdb692e4d28189fcba5c1a9044ccf046cb790f8
Jakub Vysoky <jakub@borka.cz>
2008-09-17 12:03:04 +0000

    applied r1543
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/branches/video@1547 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ ella/attachments/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from ella.ellaadmin.options import EllaAdminOptionsMixin
+
+from common.attachments.models import Attachment, Type
+
+
+class AttachmentOptions(EllaAdminOptionsMixin, admin.ModelAdmin):
+    list_display = ('name', 'type', 'created',)
+    list_filter = ('type',)
+    prepopulated_fields = {'slug' : ('name',)}
+    rich_text_fields = {'small': ('description',)}
+    raw_id_fields = ('photo',)
+
+admin.site.register(Attachment, AttachmentOptions)
+admin.site.register(Type)
+
