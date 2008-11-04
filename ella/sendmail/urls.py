1f0c6b5cf35bf38e4b8d8a87e16a7d766b641371
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2008-11-04 08:59:18 +0000

    Checks for ajax calls and server right templates, other modifications
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1703 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/sendmail/urls.py
+++ ella/sendmail/urls.py
@@ -1,16 +1,20 @@
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
 from django.http import Http404
+from django.conf import settings
 
 from ella.core.custom_urls import dispatcher
+from ella.sendmail.views import SendMailFormPreview, new_mail, mail_success, mail_error
+from ella.sendmail.forms import SendMailForm
+
+SENDMAIL_AJAX_ONLY = getattr(settings, 'SENDMAIL_AJAX_ONLY', False)
 
 def sendmail_custom_urls(request, bits, context):
 
-    from ella.sendmail.views import SendMailFormPreview, new_mail, mail_success, mail_error
-    from ella.sendmail.forms import SendMailForm
-
     if len(bits) == 1:
         if bits[0] == slugify(_('preview')):
+            if SENDMAIL_AJAX_ONLY and not request.is_ajax():
+                raise Http404, 'Sendmail is now configured to accept only AJAX calls'
             mail_preview = SendMailFormPreview(SendMailForm)
             return mail_preview(request, context)
         elif bits[0] == slugify(_('success')):
@@ -19,6 +23,8 @@ def sendmail_custom_urls(request, bits, context):
             return mail_error(request, context)
 
     if len(bits) == 0:
+        if SENDMAIL_AJAX_ONLY and not request.is_ajax():
+            raise Http404, 'Sendmail is now configured to accept only AJAX calls'
         return new_mail(request, context)
 
     raise Http404
