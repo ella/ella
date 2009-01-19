a13026e3dd2d1f62a2a552ef64eae4e54661501b
Miloslav Pojman <miloslav.pojman@centrumholdings.com>
2009-01-19 16:23:20 +0000

    Added logging.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1935 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/ratings/management/commands/aggregate_ratings.py
+++ ella/ratings/management/commands/aggregate_ratings.py
@@ -1,5 +1,10 @@
 from django.core.management.base import NoArgsCommand
 
+
+from ella.utils import installedapps
+installedapps.init_logger()
+
+# Logging must be inicialized
 from ella.ratings.aggregation import transfer_data
 
 class Command(NoArgsCommand):
