a6f37b65cccc10ec1e015ac69b1046ea88d1e7bc
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-02-05 11:51:38 +0000

    The content-by-hash mechanism seems to work now.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1995 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/media/js/newman.js
+++ ella/newman/media/js/newman.js
@@ -10,9 +10,33 @@
     // Store the #content div, so we need not always mine it from the DOM.
     var $CONTENT = $('#content');
 
+    // We need to remember what URL is loaded in which element,
+    // so we can load or not load content appropriately on hash change.
+    var LOADED_URLS = {};
+
+    // When something is loaded into an element that has no base view (in urls.js),
+    // and the user hits back, we need to reload. But then we don't want to reload again,
+    // so keep information about whether we manipulated the content, so we can
+    // abstain from reloading if we have not.
+    var PAGE_CHANGED = 0;
+
     // Take a container and a URL. Give the container the "loading" class,
     // fetch the URL, remove the class and stuff the content into the container.
-    function load_content($container, url) {
+    function load_content($container, address) {
+        ;;; console.log('loading '+address+' into '+$container.attr('id'));
+
+        // An empty address means we should revert to the base state.
+        // If one is not set up for the given container, reload the whole page.
+        if (address.length == 0) {
+            if (BASES[ $container.attr('id') ]) {
+                address = BASES[ $container.attr('id') ];
+            } else {
+                if (PAGE_CHANGED) location.reload();
+                return;
+            }
+        }
+
+        var url = $('<a>').attr('href', address).get(0).href;
         if ($container && $container.length && $container.jquery) { } else {
             console.log('Could not insert into container:');
             console.log($container);
@@ -21,6 +45,8 @@
         $container.addClass('loading');
         $.get(url, function(data) {
             $container.removeClass('loading').html(data);
+            LOADED_URLS[ $container.attr('id') ] = address;
+            PAGE_CHANGED++;
         });
     }
 
@@ -28,19 +54,20 @@
     // #url means that the result of $.get(url) be loaded into the #content div.
     // #id::url means that the result of $.get(url) be loaded into the #id element.
     // Any number of such specifiers can be concatenated, e.g. #/some/page/#header::/my/header/
+    // If URLS[ foo ] is set (in urls.js), and #foo is present,
+    // then the function is called given the $target as argument
+    // and nothing else is done for this specifier.
     function load_by_hash() {
         var hash = location.hash.substr(1);
-        if (hash.length == 0) {
-            // TODO: A smarter reset
-            location.reload();
-        }
+
+        // Figure out what should be reloaded and what not by comparing the requested things with the loaded ones.
+        var requested = {};
         var specifiers = hash.split('#');
         for (var i = 0; i < specifiers.length; i++) {
             var spec = specifiers[ i ];
-            if (spec.length == 0) continue;
             var address = spec;
             var $target = $CONTENT;
-            if (spec.match(/^([-\w]+)::(.+)/)) {
+            if (spec.match(/^([-\w]+)::(.*)/)) {
                 var id  = RegExp.$1;
                 address = RegExp.$2;
                 $target = $('#' + id);
@@ -49,12 +76,26 @@
                     continue;
                 }
             }
+            requested[ $target.attr('id') ] = address;
+        }
+        for (var k in LOADED_URLS) {
+            // if this specifier didn't change, don't load it again
+            if (LOADED_URLS[k] == requested[k]) continue;
+            // if this specifier is no longer present, reload the base
+            if (!requested[k]) requested[k] = '';
+        }
+
+        for (var target_id in requested) {
+            var $target = $('#'+target_id);
+            var address = requested[ target_id ];
+
+            // A specially treated specifier. The callback should set up LOADED_URLS properly.
             if (URLS[address]) {
                 URLS[address]($target);
                 continue;
             }
-            var url = $('<a>').attr('href', address).get(0).href;
-            load_content($target, url);
+
+            load_content($target, address);
         }
     }
 
--- ella/newman/media/js/urls.js
+++ ella/newman/media/js/urls.js
@@ -4,3 +4,8 @@ URLS = {
         location.href = 'http://google.com';
     }
 };
+
+// mapping for containers to the URLs that give out their base content (what's there when the page loads)
+BASES = {
+    //content: '/password_change/'
+};
