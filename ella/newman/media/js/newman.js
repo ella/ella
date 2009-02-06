07e0d27fae3ca1cf1e42296c038c86ef2159b5dd
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-02-06 16:31:20 +0000

    Serial requests management is now reimplemented: Everything is fetched at once and the results are placed into the document as they become available, in order. Still to do: reload of dependent things.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2000 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/media/js/newman.js
+++ ella/newman/media/js/newman.js
@@ -15,50 +15,126 @@
     // Store the #content div, so we need not always mine it from the DOM.
     var $CONTENT = $('#content');
 
+    // We need to remember what URL is loaded in which element,
+    // so we can load or not load content appropriately on hash change.
+    var LOADED_URLS = {};
+
+    // If the hash changes before all ajax requests complete,
+    // we want to cancel the pending requests. MAX_REQUEST is actually the number
+    // of the last hashchange event. Each ajax request then remembers the state
+    // of this variable when it was issued and if it's obsolete by the time it's
+    // finished, the results are discarded. It's OK to discard it because it
+    // never gets into LOADED_URLS.
+    var MAX_REQUEST = 0;
+
+    // When a sequence of URLs to load into various elements is given,
+    // the requests are stored in this fifo and their results are
+    // rendered into the document as they become ready, but always in order.
+    var LOAD_BUF = [];
+    // These are the indices into the LOAD_BUF array -- MIN_LOAD is the index
+    // of the request to be processed next (there should never be any defined
+    // fields in LOAD_BUF at position less than MIN_LOAD).
+    // MAX_LOAD should pretty much always be LOAD_BUF.length - 1.
+    var MIN_LOAD, MAX_LOAD = -1;
+
     // When something is loaded into an element that has no base view (in urls.js),
     // and the user hits back, we need to reload. But then we don't want to reload again,
     // so keep information about whether we manipulated the content, so we can
     // abstain from reloading if we have not.
     var PAGE_CHANGED = 0;
 
-    // We need to keep track of what URL is loaded into what element
-    // and what should be loaded yet. Each element of this array
-    // represents one '>'-bounded part of the URL and its state of loadedness.
-    var CHAIN = [];
+    // Check if the least present request has finished and if so, shift it
+    // from the queue and render the results, and then call itself recursively.
+    // This effectively renders all finished requests from the first up to the
+    // first pending one, where it stops. If all requests are finished,
+    // the queue gets cleaned and the indices reset.
+    function draw_ready() {
 
-    function Step() {
-        this.changed = true;
-        this.loaded = {};
+        // Slide up to the first defined request or to the end of the queue
+        while (!LOAD_BUF[ MIN_LOAD ] && LOAD_BUF.length > MIN_LOAD+1) MIN_LOAD++;
+
+        // If the queue is empty, clean it
+        if (!LOAD_BUF[ MIN_LOAD ]) {
+            ;;; carp("Emptying buffer");
+            LOAD_BUF = [];
+            MIN_LOAD = undefined;
+            MAX_LOAD = -1;
+            return;
+        }
+        var info = LOAD_BUF[ MIN_LOAD ];
+
+        if (!info.data) return; // Not yet ready
+
+        delete LOAD_BUF[ MIN_LOAD ];
+        while (LOAD_BUF.length > MIN_LOAD+1 && !LOAD_BUF[ ++MIN_LOAD ]) {}
+        var $target = $('#'+info.target_id);
+        if ($target && $target.jquery && $target.length) {} else {
+            carp('Could not find target element: #'+info.target_id);
+            return;
+        }
+        $target.removeClass('loading').html(info.data);
+        LOADED_URLS[ info.target_id ] = info.address;
+        PAGE_CHANGED++;
+
+        // Check next request
+        draw_ready();
+    }
+
+    // This removes a request from the queue and
+    function cancel_request( load_id ) {
+        var info = LOAD_BUF[ load_id ];
+        delete LOAD_BUF[ load_id ];
+        $('#'+info.target_id).removeClass('loading');
+        carp('Failed to load '+info.address+' into '+info.target_id);
     }
 
     // Take a container and a URL. Give the container the "loading" class,
-    // fetch the URL, remove the class and stuff the content into the container.
-    function load_content($container, address, step, options) {
-        ;;; carp('loading '+address+' into '+$container.attr('id'));
+    // fetch the URL, push the request into the queue, and have it checked
+    // when it finishes.
+    function load_content(arg) {
+        var target_id = arg.target_id;
+        var address = arg.address;
+        ;;; carp('loading '+address+' into '+target_id);
 
         // An empty address means we should revert to the base state.
         // If one is not set up for the given container, reload the whole page.
         if (address.length == 0) {
-            if (BASES[ $container.attr('id') ]) {
-                address = BASES[ $container.attr('id') ];
+            if (BASES[ target_id ]) {
+                address = BASES[ target_id ];
             } else {
                 if (PAGE_CHANGED) location.reload();
                 return;
             }
         }
 
+        $('#'+target_id).addClass('loading');
+
         var url = $('<a>').attr('href', address).get(0).href;
-        if ($container && $container.length && $container.jquery) { } else {
-            carp('Could not insert into container:');
-            carp($container);
-            return;
-        }
-        $container.addClass('loading');
-        $.get(url, function(data) {
-            $container.removeClass('loading').html(data);
-            step.loaded[ $container.attr('id') ] = address;
-            PAGE_CHANGED++;
-            if (options && options.next_level) load_by_stepstr(options.next_level);
+        var load_id = ++MAX_LOAD;
+        if (MIN_LOAD == undefined || load_id < MIN_LOAD) MIN_LOAD = load_id;
+        LOAD_BUF[ load_id ] = {
+            target_id: target_id,
+            order: arg.order,
+            address: address
+        };
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function(data) {
+                if (this.request_no < MAX_REQUEST) {
+                    cancel_request( this.load_id );
+                }
+                else {
+                    LOAD_BUF[ this.load_id ].data = data;
+                }
+                draw_ready();
+            },
+            error: function() {
+                cancel_request( this.load_id );
+                draw_ready();
+            },
+            load_id: load_id,
+            request_no: MAX_REQUEST
         });
     }
 
@@ -69,52 +145,26 @@
     // If URLS[ foo ] is set (in urls.js), and #foo is present,
     // then the function is called given the $target as argument
     // and nothing else is done for this specifier.
-    function load_by_stepstr(level) {
-        if (CHAIN.hash == undefined) throw "CHAIN.hash undefined.";
-        else if (CHAIN.hash.length == 0 && !CHAIN[level]) {
-            // We're done
-            for (var i = 0; i < CHAIN.length; i++) CHAIN[i].changed = false;
-            return;
-        }
-        var hash = CHAIN.hash;
-        var stepstr_len = (hash + '>').indexOf('>');
-        var stepstr = hash.substr(0, stepstr_len);
-        var step;
-        if (CHAIN[level]) step = CHAIN[level];
-        else {
-            if (level > 0 && !CHAIN[level-1]) throw "Cannot load_by_stepstr("+level+") until done load_by_stepstr("+(level-1)+").";
-            step = new Step();
-            CHAIN.push(step);
-        }
-        CHAIN.hash = hash.substr(stepstr_len+1);
-        ;;; carp('loading stepstr '+stepstr);
-
-        var loaded = step.loaded;
+    function load_by_hash() {
+        var hash = location.hash.substr(1);
+        ;;; carp('load #'+MAX_REQUEST+'; hash: '+hash)
 
         // Figure out what should be reloaded and what not by comparing the requested things with the loaded ones.
         var requested = {};
-        var specifiers = stepstr.split('#');
-        var last_id;
+        var specifiers = hash.split('#');
         for (var i = 0; i < specifiers.length; i++) {
             var spec = specifiers[ i ];
             var address = spec;
-            var $target = $CONTENT;
+            var target_id = 'content';
             if (spec.match(/^([-\w]+)::(.*)/)) {
-                var id  = RegExp.$1;
+                target_id  = RegExp.$1;
                 address = RegExp.$2;
-                $target = $('#' + id);
-                if (!$target || $target.length == 0) {
-                    carp('Could not find #'+id);
-                    continue;
-                }
             }
-            requested[ last_id = $target.attr('id') ] = address;
+            requested[ target_id ] = address;
         }
-        var keep_present = (level == 0 || !CHAIN[level-1].changed);
-        for (var k in loaded) {
+        for (var k in LOADED_URLS) {
             // if this specifier didn't change, don't load it again
-            // but only if the previous level has not changed
-            if (loaded[k] == requested[k] && keep_present) {
+            if (LOADED_URLS[k] == requested[k]) {
                 delete requested[k];
                 continue;
             }
@@ -122,42 +172,32 @@
             if (!requested[k]) requested[k] = '';
         }
 
-        var no_request = true;
-        for (var _ in requested) no_request = false;
-        if (level == 0 || !CHAIN[level-1].changed) if (no_request) step.changed = false;
-
-        if (no_request) {
-            load_by_stepstr(level+1);
-            return;
-        }
-
+        var ord = 0;
         for (var target_id in requested) {
-            no_request = false;
-            var $target = $('#'+target_id);
             var address = requested[ target_id ];
 
-            // A specially treated specifier. The callback should set up CHAIN[level].loaded properly.
+            // A specially treated specifier. The callback should set up LOADED_URLS properly.
+            // FIXME: Rewrite
             if (URLS[address]) {
-                URLS[address]($target, level);
+                URLS[address](target_id);
                 continue;
             }
 
-            var load_content_options = {};
-            if (target_id == last_id) load_content_options.next_level = level+1;
-            load_content($target, address, step, load_content_options);
+            load_content({
+                target_id: target_id,
+                address: address,
+                order: ord++
+            });
         }
-    }
-
-    function update_content() {
-        CHAIN.hash = location.hash.substr(1);
-        load_by_stepstr(0);
     }
 
     // Simulate a hashchange event fired when location.hash changes
     var CURRENT_HASH = '';
     function hashchange() {
 //        carp('hash: ' + location.hash);
-        update_content();
+        MAX_REQUEST++;
+        $('.loading').removeClass('loading');
+        load_by_hash();
     }
     setTimeout( function() {
         try {
