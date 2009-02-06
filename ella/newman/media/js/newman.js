fe0d93d2b557d1c52b29b2c000581c20cca849dd
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-02-06 09:47:25 +0000

    Implemented serial loading (load some stuff, when it's done, load more etc.)
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@1998 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/media/js/newman.js
+++ ella/newman/media/js/newman.js
@@ -15,19 +15,25 @@
     // Store the #content div, so we need not always mine it from the DOM.
     var $CONTENT = $('#content');
 
-    // We need to remember what URL is loaded in which element,
-    // so we can load or not load content appropriately on hash change.
-    var LOADED_URLS = {};
-
     // When something is loaded into an element that has no base view (in urls.js),
     // and the user hits back, we need to reload. But then we don't want to reload again,
     // so keep information about whether we manipulated the content, so we can
     // abstain from reloading if we have not.
     var PAGE_CHANGED = 0;
 
+    // We need to keep track of what URL is loaded into what element
+    // and what should be loaded yet. Each element of this array
+    // represents one '>'-bounded part of the URL and its state of loadedness.
+    var CHAIN = [];
+
+    function Step() {
+        this.changed = true;
+        this.loaded = {};
+    }
+
     // Take a container and a URL. Give the container the "loading" class,
     // fetch the URL, remove the class and stuff the content into the container.
-    function load_content($container, address) {
+    function load_content($container, address, step, options) {
         ;;; carp('loading '+address+' into '+$container.attr('id'));
 
         // An empty address means we should revert to the base state.
@@ -50,8 +56,9 @@
         $container.addClass('loading');
         $.get(url, function(data) {
             $container.removeClass('loading').html(data);
-            LOADED_URLS[ $container.attr('id') ] = address;
+            step.loaded[ $container.attr('id') ] = address;
             PAGE_CHANGED++;
+            if (options && options.next_level) load_by_stepstr(options.next_level);
         });
     }
 
@@ -62,12 +69,32 @@
     // If URLS[ foo ] is set (in urls.js), and #foo is present,
     // then the function is called given the $target as argument
     // and nothing else is done for this specifier.
-    function load_by_hash() {
-        var hash = location.hash.substr(1);
+    function load_by_stepstr(level) {
+        if (CHAIN.hash == undefined) throw "CHAIN.hash undefined.";
+        else if (CHAIN.hash.length == 0 && !CHAIN[level]) {
+            // We're done
+            for (var i = 0; i < CHAIN.length; i++) CHAIN[i].changed = false;
+            return;
+        }
+        var hash = CHAIN.hash;
+        var stepstr_len = (hash + '>').indexOf('>');
+        var stepstr = hash.substr(0, stepstr_len);
+        var step;
+        if (CHAIN[level]) step = CHAIN[level];
+        else {
+            if (level > 0 && !CHAIN[level-1]) throw "Cannot load_by_stepstr("+level+") until done load_by_stepstr("+(level-1)+").";
+            step = new Step();
+            CHAIN.push(step);
+        }
+        CHAIN.hash = hash.substr(stepstr_len+1);
+        ;;; carp('loading stepstr '+stepstr);
+
+        var loaded = step.loaded;
 
         // Figure out what should be reloaded and what not by comparing the requested things with the loaded ones.
         var requested = {};
-        var specifiers = hash.split('#');
+        var specifiers = stepstr.split('#');
+        var last_id;
         for (var i = 0; i < specifiers.length; i++) {
             var spec = specifiers[ i ];
             var address = spec;
@@ -81,11 +108,13 @@
                     continue;
                 }
             }
-            requested[ $target.attr('id') ] = address;
+            requested[ last_id = $target.attr('id') ] = address;
         }
-        for (var k in LOADED_URLS) {
+        var keep_present = (level == 0 || !CHAIN[level-1].changed);
+        for (var k in loaded) {
             // if this specifier didn't change, don't load it again
-            if (LOADED_URLS[k] == requested[k]) {
+            // but only if the previous level has not changed
+            if (loaded[k] == requested[k] && keep_present) {
                 delete requested[k];
                 continue;
             }
@@ -93,25 +122,42 @@
             if (!requested[k]) requested[k] = '';
         }
 
+        var no_request = true;
+        for (var _ in requested) no_request = false;
+        if (level == 0 || !CHAIN[level-1].changed) if (no_request) step.changed = false;
+
+        if (no_request) {
+            load_by_stepstr(level+1);
+            return;
+        }
+
         for (var target_id in requested) {
+            no_request = false;
             var $target = $('#'+target_id);
             var address = requested[ target_id ];
 
-            // A specially treated specifier. The callback should set up LOADED_URLS properly.
+            // A specially treated specifier. The callback should set up CHAIN[level].loaded properly.
             if (URLS[address]) {
-                URLS[address]($target);
+                URLS[address]($target, level);
                 continue;
             }
 
-            load_content($target, address);
+            var load_content_options = {};
+            if (target_id == last_id) load_content_options.next_level = level+1;
+            load_content($target, address, step, load_content_options);
         }
+    }
+
+    function update_content() {
+        CHAIN.hash = location.hash.substr(1);
+        load_by_stepstr(0);
     }
 
     // Simulate a hashchange event fired when location.hash changes
     var CURRENT_HASH = '';
     function hashchange() {
 //        carp('hash: ' + location.hash);
-        load_by_hash();
+        update_content();
     }
     setTimeout( function() {
         try {
