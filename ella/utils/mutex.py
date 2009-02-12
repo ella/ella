3fca696cfdd7a297bf3c886e9a7876d4bb9c4806
Jan Seifert <jan.seifert@centrumholdings.com>
2009-02-12 10:06:58 +0000

    Added lock manager - EllaMutex
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2027 2d143e24-0a30-0410-89d7-a2e95868dc81

---
+++ ella/utils/mutex.py
@@ -0,0 +1,36 @@
+"""
+mutex.py
+
+Ella lock manager
+"""
+
+from sys import maxint
+from uuid import uuid1
+
+from django.core.cache import cache
+
+class EllaMutex:
+    " EllaMutex(name, lifetime=1800) - create mutex 'name', default lifetime is 30 minutes "
+    def __init__(self, name, lifetime=1800):
+        self.__key = name
+        self.__lifetime = lifetime
+        self.__value = uuid1().hex
+
+    def lock(self):
+        " lock() - lock mutex, return False if mutex has already locked "
+        if cache.get(self.__key) != None:
+            return False
+        else:
+            cache.set(self.__key, self.__value, self.__lifetime)
+            return self.__status
+
+    def unlock(self):
+        " unlock() - unlock mutex "
+        if self.__status == True:
+            cache.delete(self.__key)
+            return not self.__status
+        return False
+
+    @property
+    def __status(self):
+        return cache.get(self.__key) == self.__value and True or False
