37dec90139990eeba2468870927bc76543a80ef4
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-02-12 10:26:29 +0000

    Views moved to NewmanModelAdmin, views.py removed.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2028 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/__init__.py
+++ ella/newman/__init__.py
@@ -1,7 +1,8 @@
 __doc__ = """ella.newman is next (3rd ?) generation of admin for Ella"""
-from ella.newman.sites import NewmanSite, site
-from ella.newman.options import NewmanModelAdmin
 
 # need to import filterspecs in __init__. filterspecs module shoud clean filters before they're assigned to ModelAdmins
 import ella.newman.filterspecs
 import ella.newman.views
+
+from ella.newman.sites import NewmanSite, site
+from ella.newman.options import NewmanModelAdmin
--- ella/newman/options.py
+++ ella/newman/options.py
@@ -6,10 +6,24 @@ from django.contrib import admin
 from django.views.decorators.cache import never_cache
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.admin.options import ModelAdmin
+from django.contrib.admin.options import IncorrectLookupParameters
+from django import template, forms
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import Template
+from django.contrib.admin.views.main import ChangeList, ERROR_FLAG
+from django.shortcuts import render_to_response
+
+from ella.newman.changelist import NewmanChangeList, FilterChangeList
 
 
 class NewmanModelAdmin(ModelAdmin):
     registered_views = []
+
+    def __init__(self, *args, **kwargs):
+        super(NewmanModelAdmin, self).__init__(*args, **kwargs)
+        #NewmanModelAdmin.register(lambda x: x is None, self.changelist_view)
+        #NewmanModelAdmin.register(lambda x: x.endswith('suggest'), self.suggest_view)
+        #NewmanModelAdmin.register(lambda x: x.endswith('filters'), self.filters_view)
 
     def register(cls, test_callback, view_callback):
         cls.registered_views.append({'test': test_callback, 'view': view_callback})
@@ -20,10 +34,81 @@ class NewmanModelAdmin(ModelAdmin):
             if reg['test'](url):
                 return reg['view'](self, request)
 
-        if url and url.endswith('suggest'):
+        if not url:
+            return self.changelist_view(request)
+        elif url.endswith('suggest'):
             return self.suggest_view(request)
-
+        elif url.endswith('filters'):
+            return self.filters_view(request)
         return super(NewmanModelAdmin, self).__call__(request, url)
+
+
+    def filters_view(model_admin, request, extra_context=None):
+        "stolen from: The 'change list' admin view for this model."
+        opts = model_admin.model._meta
+        app_label = opts.app_label
+        try:
+            cl = FilterChangeList(request, model_admin.model, model_admin.list_display, model_admin.list_display_links, model_admin.list_filter,
+                model_admin.date_hierarchy, model_admin.search_fields, model_admin.list_select_related, model_admin.list_per_page, model_admin)
+        except IncorrectLookupParameters:
+            # Wacky lookup parameters were given, so redirect to the main
+            # changelist page, without parameters, and pass an 'invalid=1'
+            # parameter via the query string. If wacky parameters were given and
+            # the 'invalid=1' parameter was already in the query string, something
+            # is screwed up with the database, so display an error page.
+            if ERROR_FLAG in request.GET.keys():
+                return render_to_response('admin/invalid_setup.html', {'title': _('Database error')})
+            return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
+
+        context = {
+            'title': cl.title,
+            'is_popup': cl.is_popup,
+            'cl': cl,
+            'has_add_permission': model_admin.has_add_permission(request),
+            'root_path': model_admin.admin_site.root_path,
+            'app_label': app_label,
+}
+        context.update(extra_context or {})
+        out= render_to_response(
+            'admin/filters.html',
+            context,
+            context_instance=template.RequestContext(request)
+)
+        return HttpResponse(out, mimetype='text/plain;charset=utf-8')
+
+
+    def changelist_view(self, request, extra_context=None):
+        opts = self.model._meta
+        app_label = opts.app_label
+        if not self.has_change_permission(request, None):
+            raise PermissionDenied
+        try:
+            cl = NewmanChangeList(request, self.model, self.list_display, self.list_display_links, self.list_filter,
+                self.date_hierarchy, self.search_fields, self.list_select_related, self.list_per_page, self)
+        except IncorrectLookupParameters:
+            # Wacky lookup parameters were given, so redirect to the main
+            # changelist page, without parameters, and pass an 'invalid=1'
+            # parameter via the query string. If wacky parameters were given and
+            # the 'invalid=1' parameter was already in the query string, something
+            # is screwed up with the database, so display an error page.
+            if ERROR_FLAG in request.GET.keys():
+                return render_to_response('admin/invalid_setup.html', {'title': _('Database error')})
+            return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
+
+        context = {
+            'title': cl.title,
+            'is_popup': cl.is_popup,
+            'cl': cl,
+            'has_add_permission': self.has_add_permission(request),
+            'root_path': self.admin_site.root_path,
+            'app_label': app_label,
+}
+        context.update(extra_context or {})
+        return render_to_response(self.change_list_template or [
+            'admin/%s/%s/change_list.html' % (app_label, opts.object_name.lower()),
+            'admin/%s/change_list.html' % app_label,
+            'admin/change_list.html'
+        ], context, context_instance=template.RequestContext(request))
 
 
     def suggest_view(self, request, extra_context=None):
