9b62b43f0bca605045bba5d474be37e49c5e47c3
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-02-12 10:26:32 +0000

    views removed from __init__.py
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2030 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/__init__.py
+++ ella/newman/__init__.py
@@ -2,7 +2,6 @@ __doc__ = """ella.newman is next (3rd ?) generation of admin for Ella"""
 
 # need to import filterspecs in __init__. filterspecs module shoud clean filters before they're assigned to ModelAdmins
 import ella.newman.filterspecs
-import ella.newman.views
 
 from ella.newman.sites import NewmanSite, site
 from ella.newman.options import NewmanModelAdmin
