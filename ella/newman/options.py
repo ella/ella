c1306ba88c6713938837d36d35b8a22faa928cf8
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-02-18 10:39:47 +0000

    bug fix in has_delete_permission
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2069 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/options.py
+++ ella/newman/options.py
@@ -286,89 +286,9 @@ class NewmanModelAdmin(ModelAdmin):
             return True
         user = request.user
         for role in user.categoryuserrole_set.all():
-            for perm in role.group.permissions.all():
-                if del_perm in perms:
-                    return True
+            if del_perm in role.group.permissions.all():
+                return True
         # no permission found
         return False
 
-    def change_view(self, request, object_id, extra_context=None):
-        "The 'change' admin view for this model."
-        #return super(NewmanModelAdmin, self).change_view(request, object_id, extra_cx)
-        model = self.model
-        opts = model._meta
-
-        try:
-            obj = model._default_manager.get(pk=object_id)
-        except model.DoesNotExist:
-            # Don't raise Http404 just yet, because we haven't checked
-            # permissions yet. We don't want an unauthenticated user to be able
-            # to determine whether a given object exists.
-            obj = None
-
-        if not self.has_change_permission(request, obj):
-            raise PermissionDenied
-
-        if obj is None:
-            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
-
-        if request.method == 'POST' and request.POST.has_key("_saveasnew"):
-            return self.add_view(request, form_url='../../add/')
-
-        ModelForm = self.get_form(request, obj)
-        formsets = []
-        if request.method == 'POST':
-            form = ModelForm(request.POST, request.FILES, instance=obj)
-            if form.is_valid():
-                form_validated = True
-                new_object = self.save_form(request, form, change=True)
-            else:
-                form_validated = False
-                new_object = obj
-            for FormSet in self.get_formsets(request, new_object):
-                formset = FormSet(request.POST, request.FILES,
-                                  instance=new_object)
-                formsets.append(formset)
-
-            if all_valid(formsets) and form_validated:
-                self.save_model(request, new_object, form, change=True)
-                form.save_m2m()
-                for formset in formsets:
-                    self.save_formset(request, form, formset, change=True)
-
-                change_message = self.construct_change_message(request, form, formsets)
-                self.log_change(request, new_object, change_message)
-                return self.response_change(request, new_object)
-
-        else:
-            form = ModelForm(instance=obj)
-            for FormSet in self.get_formsets(request, obj):
-                formset = FormSet(instance=obj)
-                formsets.append(formset)
-
-        adminForm = helpers.AdminForm(form, self.get_fieldsets(request, obj), self.prepopulated_fields)
-        media = self.media + adminForm.media
-
-        inline_admin_formsets = []
-        for inline, formset in zip(self.inline_instances, formsets):
-            fieldsets = list(inline.get_fieldsets(request, obj))
-            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset, fieldsets)
-            inline_admin_formsets.append(inline_admin_formset)
-            media = media + inline_admin_formset.media
-
-        context = {
-            'title': _('Change %s') % force_unicode(opts.verbose_name),
-            'adminform': adminForm,
-            'object_id': object_id,
-            'original': obj,
-            'is_popup': request.REQUEST.has_key('_popup'),
-            'media': mark_safe(media),
-            'inline_admin_formsets': inline_admin_formsets,
-            'errors': helpers.AdminErrorList(form, formsets),
-            'root_path': self.admin_site.root_path,
-            'app_label': opts.app_label,
-}
-        context.update(extra_context or {})
-        return self.render_change_form(request, context, change=True, obj=obj)
-    change_view = transaction.commit_on_success(change_view)
 
