437696546d400470e02ecf67b8fb7d2c90453b29
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2009-02-19 16:13:07 +0000

    Replaced hard link by url tag.
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2083 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/urls.py
+++ ella/newman/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('ella.newman.views',
 
     # send err report / feature request
-    url(r'^err-report/$', 'err_report', name="err-report"),
+    url(r'^err-report/$', 'err_report', name="newman-err-report"),
 )
