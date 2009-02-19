6773ae3d3c20a38863fa7ae86b2419fc44549df9
Vitek Pliska <vitezslav.pliska@centrumholdings.com>
2009-02-19 11:19:52 +0000

    We need only paths to CSS/JS in newman, not rendered media
    git-svn-id: https://svn.netcentrum.cz/svn/pylib/trunk@2076 2d143e24-0a30-0410-89d7-a2e95868dc81

--- ella/newman/options.py
+++ ella/newman/options.py
@@ -20,7 +20,9 @@ from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 
 from ella.newman.changelist import NewmanChangeList, FilterChangeList
-from ella.newman import models
+from ella.newman import models, fields
+from django.contrib.admin.util import unquote
+from django.forms.formsets import all_valid
 
 DEFAULT_LIST_PER_PAGE = getattr(settings, 'NEWMAN_LIST_PER_PAGE', 25)
 
@@ -294,4 +296,106 @@ class NewmanModelAdmin(ModelAdmin):
         # no permission found
         return False
 
+    def change_view(self, request, object_id, extra_context=None):
+        "The 'change' admin view for this model."
+        model = self.model
+        opts = model._meta
+
+        try:
+            obj = model._default_manager.get(pk=unquote(object_id))
+        except model.DoesNotExist:
+            # Don't raise Http404 just yet, because we haven't checked
+            # permissions yet. We don't want an unauthenticated user to be able
+            # to determine whether a given object exists.
+            obj = None
+
+        if not self.has_change_permission(request, obj):
+            raise PermissionDenied
+
+        if obj is None:
+            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
+
+        if request.method == 'POST' and request.POST.has_key("_saveasnew"):
+            return self.add_view(request, form_url='../../add/')
+
+        ModelForm = self.get_form(request, obj)
+        formsets = []
+        if request.method == 'POST':
+            form = ModelForm(request.POST, request.FILES, instance=obj)
+            if form.is_valid():
+                form_validated = True
+                new_object = self.save_form(request, form, change=True)
+            else:
+                form_validated = False
+                new_object = obj
+            for FormSet in self.get_formsets(request, new_object):
+                formset = FormSet(request.POST, request.FILES,
+                                  instance=new_object)
+                formsets.append(formset)
+
+            if all_valid(formsets) and form_validated:
+                self.save_model(request, new_object, form, change=True)
+                form.save_m2m()
+                for formset in formsets:
+                    self.save_formset(request, form, formset, change=True)
+
+                change_message = self.construct_change_message(request, form, formsets)
+                self.log_change(request, new_object, change_message)
+                return self.response_change(request, new_object)
+
+        else:
+            form = ModelForm(instance=obj)
+            for FormSet in self.get_formsets(request, obj):
+                formset = FormSet(instance=obj)
+                formsets.append(formset)
+
+        adminForm = helpers.AdminForm(form, self.get_fieldsets(request, obj), self.prepopulated_fields)
+        media = self.media + adminForm.media
+
+        inline_admin_formsets = []
+        for inline, formset in zip(self.inline_instances, formsets):
+            fieldsets = list(inline.get_fieldsets(request, obj))
+            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset, fieldsets)
+            inline_admin_formsets.append(inline_admin_formset)
+            media = media + inline_admin_formset.media
+
+        # raw media paths for ajax implementation
+        raw_media = []
+        raw_media.extend(media._css['screen'])
+        raw_media.extend(media._js)
+
+        context = {
+            'title': _('Change %s') % force_unicode(opts.verbose_name),
+            'adminform': adminForm,
+            'object_id': object_id,
+            'original': obj,
+            'is_popup': request.REQUEST.has_key('_popup'),
+            'media': raw_media,
+            'inline_admin_formsets': inline_admin_formsets,
+            'errors': helpers.AdminErrorList(form, formsets),
+            'root_path': self.admin_site.root_path,
+            'app_label': opts.app_label,
+}
+        context.update(extra_context or {})
+        return self.render_change_form(request, context, change=True, obj=obj)
+    change_view = transaction.commit_on_success(change_view)
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+
+        # TODO: Only hotfix for django 9791+
+        if 'request' in kwargs:
+            del kwargs['request']
+
+        for css_class, rich_text_fields in getattr(self, 'rich_text_fields', {}).iteritems():
+            if db_field.name in rich_text_fields:
+                kwargs.update({
+                    'required': not db_field.blank,
+                    'label': db_field.verbose_name,
+})
+                rich_text_field = fields.RichTextAreaField(**kwargs)
+                if css_class:
+                    rich_text_field.widget.attrs['class'] += ' %s' % css_class
+                return rich_text_field
+
+        return super(NewmanModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
