2dc314035bee7e39ea24674ebfcc1c37b6f5f1e7
Oldrich Kruza <sixtease@melouch.(none)>
2009-02-23 12:32:59 +0100

    Restored trailing whitespace for newman.js

--- ella/newman/media/js/newman.js
+++ ella/newman/media/js/newman.js
@@ -18,11 +18,11 @@ else {
 }
 
 ( function($) { $(document).ready( function() {
-
+    
     // We need to remember what URL is loaded in which element,
     // so we can load or not load content appropriately on hash change.
     var LOADED_URLS = {};
-
+    
     // If the hash changes before all ajax requests complete,
     // we want to cancel the pending requests. MAX_REQUEST is actually the number
     // of the last hashchange event. Each ajax request then remembers the state
@@ -30,7 +30,7 @@ else {
     // finished, the results are discarded. It's OK to discard it because it
     // never gets into LOADED_URLS.
     var MAX_REQUEST = 0;
-
+    
     // When a sequence of URLs to load into various elements is given,
     // the requests are stored in this fifo and their results are
     // rendered into the document as they become ready, but always in order.
@@ -40,13 +40,13 @@ else {
     // fields in LOAD_BUF at position less than MIN_LOAD).
     // MAX_LOAD should pretty much always be LOAD_BUF.length - 1.
     var MIN_LOAD, MAX_LOAD = -1;
-
+    
     // When something is loaded into an element that has no base view (in urls.js),
     // and the user hits back, we need to reload. But then we don't want to reload again,
     // so keep information about whether we manipulated the content, so we can
     // abstain from reloading if we have not.
     var PAGE_CHANGED = 0;
-
+    
     // Custom callbacks to be called before and after normally handling the hashchange pseudo event.
     var PRE_HASHCHANGE_CALLBACKS = [], POST_HASHCHANGE_CALLBACKS = [];
     window.add_hashchange_callback = function(fn, is_pre) {
@@ -59,7 +59,7 @@ else {
         var queue = is_pre ? PRE_HASHCHANGE_CALLBACKS : POST_HASHCHANGE_CALLBACKS;
         return delete queue[id];
     }
-
+    
     function object_empty(o) {
         for (var k in o) return false;
         return true;
@@ -69,7 +69,7 @@ else {
         for (var k in o) rv.push(k);
         return rv;
     }
-
+    
     function inject_content($target, data, address) {
         // whatever was loaded inside, remove it from LOADED_URLS
         if (!object_empty(LOADED_URLS)) {
@@ -78,7 +78,7 @@ else {
                 delete LOADED_URLS[ this.id ];
             });
         }
-
+        
         $target.removeClass('loading').html(data);
         dec_loading();
         if (address != undefined) {
@@ -86,17 +86,17 @@ else {
         }
         PAGE_CHANGED++;
     }
-
+    
     // Check if the least present request has finished and if so, shift it
     // from the queue and render the results, and then call itself recursively.
     // This effectively renders all finished requests from the first up to the
     // first pending one, where it stops. If all requests are finished,
     // the queue gets cleaned and the indices reset.
     function draw_ready() {
-
+        
         // Slide up to the first defined request or to the end of the queue
         while (!LOAD_BUF[ MIN_LOAD ] && LOAD_BUF.length > MIN_LOAD+1) MIN_LOAD++;
-
+        
         // If the queue is empty, clean it
         if (!LOAD_BUF[ MIN_LOAD ]) {
 //            ;;; carp("Emptying buffer");
@@ -106,9 +106,9 @@ else {
             return;
         }
         var info = LOAD_BUF[ MIN_LOAD ];
-
+        
         if (!info.data) return; // Not yet ready
-
+        
         delete LOAD_BUF[ MIN_LOAD ];
         while (LOAD_BUF.length > MIN_LOAD+1 && !LOAD_BUF[ ++MIN_LOAD ]) {}
         var $target = $('#'+info.target_id);
@@ -116,27 +116,27 @@ else {
             carp('Could not find target element: #'+info.target_id);
             return;
         }
-
+        
         inject_content($target, info.data, info.address);
-
+        
         // Check next request
         draw_ready();
     }
-
+    
     // This removes a request from the queue
     function cancel_request( load_id ) {
         var info = LOAD_BUF[ load_id ];
         delete LOAD_BUF[ load_id ];
         $('#'+info.target_id).removeClass('loading');
         dec_loading();
-
+        
         // Restore the hash so it doesn't look like the request succeeded.
         url_target_id = ((info.target_id == 'content') ? '' : info.target_id+'::');
         adr(url_target_id + (LOADED_URLS[info.target_id] ? LOADED_URLS[info.target_id] : ''));
-
+        
         carp('Failed to load '+info.address+' into '+info.target_id);
     }
-
+    
     // Take a container and a URL. Give the container the "loading" class,
     // fetch the URL, push the request into the queue, and when it finishes,
     // check for requests ready to be loaded into the document.
@@ -144,7 +144,7 @@ else {
         var target_id = arg.target_id;
         var address = arg.address;
         ;;; carp('loading '+address+' into '+target_id);
-
+            
         // An empty address means we should revert to the base state.
         // If one is not set up for the given container, reload the whole page.
         if (address.length == 0) {
@@ -155,10 +155,10 @@ else {
                 return;
             }
         }
-
+        
         $('#'+target_id).addClass('loading');
         show_loading();
-
+        
         var url = $('<a>').attr('href', address).get(0).href;
         var load_id = ++MAX_LOAD;
         if (MIN_LOAD == undefined || load_id < MIN_LOAD) MIN_LOAD = load_id;
@@ -186,7 +186,7 @@ else {
             request_no: MAX_REQUEST
         });
     }
-
+    
     // We want location.hash to exactly describe what's on the page.
     // #url means that the result of $.get(url) be loaded into the #content div.
     // #id::url means that the result of $.get(url) be loaded into the #id element.
@@ -197,7 +197,7 @@ else {
     function load_by_hash() {
         var hash = location.hash.substr(1);
 //        ;;; carp('load #'+MAX_REQUEST+'; hash: '+hash)
-
+        
         // Figure out what should be reloaded and what not by comparing the requested things with the loaded ones.
         var requested = {};
         var specifiers = hash.split('#');
@@ -236,7 +236,7 @@ else {
         var processed = {};
         var reload_target = {};
         while (!object_empty(ids_map)) {
-
+            
             // draw an element that's independent on any other in the list
             var ids = [];
             for (var id in ids_map) ids.push(id);
@@ -262,7 +262,7 @@ else {
                 carp(ids_map);
                 throw('Cyclic graph of elements???');
             }
-
+            
             var result = {};
             for (var par in processed) {
                 // if we went over an ancestor of this element
@@ -283,7 +283,7 @@ else {
                     }
                 }
             }
-
+            
             // If parent didn't force reload or delete,
             if (result.to_reload == undefined) {
                 // and the thing is no longer requested and we don't have the base loaded,
@@ -292,7 +292,7 @@ else {
                     result.to_reload = 1;
                 }
             }
-
+            
             if (result.to_reload == undefined) {
                 // If the requested url changed,
                 if (requested[ indep ] != LOADED_URLS[ indep ]) {
@@ -300,36 +300,36 @@ else {
                     result.to_reload = 1;
                 }
             }
-
+            
             // If we want to reload but no URL is set, default to the base
             if (result.to_reload && !requested[ indep ]) {
                 requested[ indep ] = '';
             }
-
+            
             processed[ indep ] = result;
         }
         // Now we figured out what to reload.
-
+        
         for (var target_id in requested) {
             if (!processed[ target_id ].to_reload) {
                 continue;
             }
             var address = requested[ target_id ];
-
+            
             // A specially treated specifier. The callback should set up LOADED_URLS properly.
             // FIXME: Rewrite
             if (URLS[address]) {
                 URLS[address](target_id);
                 continue;
             }
-
+            
             load_content({
                 target_id: target_id,
                 address: address
             });
         }
     }
-
+    
     // Simulate a hashchange event fired when location.hash changes
     var CURRENT_HASH = '';
     function hashchange() {
@@ -352,7 +352,7 @@ else {
         setTimeout(arguments.callee, 50);
     }, 50);
     // End of hash-driven content management
-
+    
     // Loads stuff from an URL to an element like load_by_hash but:
     // - Only one specifier (id-url pair) can be given.
     // - URL hash doesn't change.
@@ -373,12 +373,12 @@ else {
         if (colon_index >= 0) {
             address = address.substr(colon_index + '::'.length);
         }
-
+        
         if (LOADED_URLS[target_id] == address) return;
-
+        
         var url = adr(specifier, {just_get:1});
         var url = $('<a>').attr('href', url).get(0).href;
-
+        
         var $target = $('#'+target_id);
         if ($target && $target.length) {} else {
             throw("Target '#"+target_id+"' not found.");
@@ -390,7 +390,7 @@ else {
             inject_content($target, data, address);
         });
     }
-
+    
     // Set up event handlers
     $('.simpleload,.simpleload-container a').live('click', function() {
         simple_load($(this).attr('href'));
@@ -403,41 +403,41 @@ else {
 })})(jQuery);
 
 // Manipulate the hash address.
-//
+// 
 // We use http://admin/#/foo/ instead of http://admin/foo/.
 // Therefore, <a href="bar/"> won't lead to http://admin/#/foo/bar/ as we need but to http://admin/bar/.
 // To compensate for this, use <a href="javascript:adr('bar/')> instead.
 // adr('id::bar/') can be used too.
-//
+// 
 // adr('bar/#id::baz/') is the same as adr('bar/'); adr('id::baz/').
 // Absolute paths and ?var=val strings work too.
-//
+// 
 // Alternatively, you can use <a href="bar/" class="hashadr">.
 // The hashadr class says clicks should be captured and delegated to function adr.
 // A third way is to encapsulate a link (<a>) into a .hashadr-container element.
-//
+// 
 // The target_id::rel_base::address syntax in a specifier means that address is taken as relative
 // to the one loaded to rel_base and the result is loaded into target_id.
 // For example, suppose that location.hash == '#id1::/foo/'. Then calling
 // adr('id2::id1::bar/') would be like doing location.hash = '#id1::/foo/#id2::/foo/bar/'.
-//
+// 
 // The second argument is an object where these fields are recognized:
 // - hash: a custom hash string to be used instead of location.hash,
 // - just_get: Instructs the function to merely return the modified address (without the target_id).
 //   Using this option disables the support of multiple '#'-separated specifiers.
 //   Other than the first one are ignored.
 function adr(address, options) {
-
+    
     // '#' chars in the address separate invividual requests for hash modification.
     // First deal with the first one and then recurse on the subsequent ones.
     if (address.charAt(0) == '#') address = address.substr(1);
     var hashpos = (address+'#').indexOf('#');
     var tail = address.substr(hashpos+1);
     address = address.substr(0, hashpos);
-
+    
     if (!options) options = {};
     var hash = (options.hash == undefined) ? location.hash : options.hash;
-
+    
     // Figure out which specifier is concerned.
     var target_id = '';
     // But wait, if target_id::rel_base::address was specified,
@@ -457,7 +457,7 @@ function adr(address, options) {
         target_id = RegExp.$1;
         address   = RegExp.$2;
     }
-
+    
     // If no hash is present, simply use the address.
     if (hash.length <= 1) {
         var newhash;
@@ -473,7 +473,7 @@ function adr(address, options) {
             return;
         }
     }
-
+    
     // Figure out the span in the current hash where the change applies.
     var start = 0;
     var end;
@@ -504,17 +504,17 @@ function adr(address, options) {
         }
     }
     // Now, hash.substr(start,end) is the address we need to modify.
-
+    
     // Figure out whether we replace the address, append to it, or what.
     // Move start appropriately to denote where the part to replace starts.
-
+    
     var newhash;
     var addr_start = start;
-
+    
     // We've not gotten the address from a previous recursive call, thus modify the address as needed.
     if (new_address == undefined) {
         new_address = address;
-
+        
         // empty address -- remove the specifier
         if (address.length == 0) {
             start = hash.lastIndexOf('#',start);
@@ -527,7 +527,7 @@ function adr(address, options) {
         else {
             var left_anchor = hash.lastIndexOf('#', start)+1;
             start = (hash.substr(0, end)+'?').indexOf('?', start);
-
+            
             // cut off the directories as appropriate when the address starts with ../
             while (new_address.substr(0,3) == '../' && hash.substring(left_anchor,start-1).indexOf('/') >= 0) {
                 new_address = new_address.substr(3);
@@ -535,9 +535,9 @@ function adr(address, options) {
             }
         }
     }
-
+    
     newhash = hash.substr(0, start) + specifier_prefix + new_address + hash.substr(end);
-
+    
     if (options.just_get) {
         return hash.substring(addr_start, start) + new_address;
     }
@@ -555,16 +555,16 @@ function load_media(url, succ_fn, err_fn) {
     if (url.match(/\.(\w+)(?:$|\?)/))
         var ext = RegExp.$1;
     else throw('Unexpected URL format: '+url);
-
+    
     var abs_url = $('<a>').attr({href:url}).get(0).href;
-
+    
     function stylesheet_present(url) {
         for (var i = 0; i < document.styleSheets.length; i++) {
             if (document.styleSheets[i].href == url) return document.styleSheets[i];
         }
         return false;
     }
-
+    
     if (ext == 'css') {
         if (stylesheet_present(abs_url)) return true;
         var tries = 100;
@@ -577,7 +577,7 @@ function load_media(url, succ_fn, err_fn) {
                 }
                 var ss;
                 if (ss = stylesheet_present(abs_url)) {
-
+                    
                     if (ss.cssRules.length) succ_fn(url);
                     else {
                         carp('CSS stylesheet empty.');
@@ -626,7 +626,7 @@ function request_media(url) {
 
 
 /////// END OF THE LIBRARY
-///////
+/////// 
 /////// BEGIN CODE FOR SPECIFIC USE
 
 
@@ -664,7 +664,7 @@ $( function() {
             error:   error
         });
     });
-
+    
     // Packing and unpacking filter list. To be removed when filters are reimplemented.
     $('#filters :header').live('click', function() {
         $(this).next(':first').filter('ul').slideToggle('slow');
