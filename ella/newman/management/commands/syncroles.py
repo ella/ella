8892d99c19f932944baa1b8c4fc15ba632892ab0
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-02-26 17:32:01 +0100

    Parameter --notransaction to command syncroles added.

--- ella/newman/management/commands/syncroles.py
+++ ella/newman/management/commands/syncroles.py
@@ -4,17 +4,24 @@ from django.db import connection, transaction
 from ella.newman.models import CategoryUserRole, DenormalizedCategoryUserRole
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--notransaction', action='store_true', dest='start_transaction',
+            help='If specified synchronization won\'t be performed inside transaction.'),
+    )
     help = 'Synchronizes CategoryUserRole objects to DenormalizedCategoryUserRole objects.'
     args = ""
 
     def handle(self, *fixture_labels, **options):
         verbosity = int(options.get('verbosity', 1))
+        verbosity = int(options.get('verbosity', 1))
+        run_transaction = not options.get('start_transaction', False)
         # start transaction
-        transaction.commit_unless_managed()
-        transaction.enter_transaction_management()
-        transaction.managed(True)
-        if verbosity > 1:
-            print 'Transaction started'
+        if run_transaction:
+            transaction.commit_unless_managed()
+            transaction.enter_transaction_management()
+            transaction.managed(True)
+            if verbosity > 1:
+                print 'Transaction started'
 
         cur = connection.cursor()
         cur.execute('DELETE FROM %s' % DenormalizedCategoryUserRole._meta.db_table)
@@ -25,7 +32,8 @@ class Command(BaseCommand):
         if verbosity > 0:
             print 'Denormalized object count: %d' % DenormalizedCategoryUserRole.objects.all().count()
         # commit changes to database
-        transaction.commit()
-        transaction.leave_transaction_management()
-        if verbosity > 1:
-            print 'Transaction committed'
+        if run_transaction:
+            transaction.commit()
+            transaction.leave_transaction_management()
+            if verbosity > 1:
+                print 'Transaction committed'
