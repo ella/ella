50880fc565e0156c286479f116f0bd49a1ed6c7e
Vitek Pliska <whit@jizak.cz>
2009-02-26 19:45:32 +0100

    archive_entry_year is back after commit 08b1e33..., code formatting.

--- ella/core/views.py
+++ ella/core/views.py
@@ -16,9 +16,9 @@ from ella.core.cache.template_loader import render_to_response
 
 # local cache for get_content_type()
 CONTENT_TYPE_MAPPING = {}
-CACHE_TIMEOUT_LONG = getattr(settings, 'CACHE_TIMEOUT_LONG', 60*60)
+CACHE_TIMEOUT_LONG = getattr(settings, 'CACHE_TIMEOUT_LONG', 60 * 60)
 
-def get_content_type( ct_name ):
+def get_content_type(ct_name):
     """
     A helper function that returns ContentType object based on its slugified verbose_name_plural.
 
@@ -34,53 +34,53 @@ def get_content_type( ct_name ):
         ct = CONTENT_TYPE_MAPPING[ct_name]
     except KeyError:
         for model in models.get_models():
-            if ct_name == slugify( model._meta.verbose_name_plural ):
-                ct = ContentType.objects.get_for_model( model )
+            if ct_name == slugify(model._meta.verbose_name_plural):
+                ct = ContentType.objects.get_for_model(model)
                 CONTENT_TYPE_MAPPING[ct_name] = ct
                 break
         else:
             raise Http404
     return ct
 
-def object_detail( request, category, content_type, slug, year=None, month=None, day=None, url_remainder=None ):
-    context = _object_detail( request, category, content_type, slug, year, month, day, url_remainder )
+def object_detail(request, category, content_type, slug, year=None, month=None, day=None, url_remainder=None):
+    context = _object_detail(request, category, content_type, slug, year, month, day, url_remainder)
     return render_to_response(
         get_templates('object.html', slug, context['category'], context['content_type'].app_label, context['content_type'].model),
         context,
-        context_instance = RequestContext( request )
+        context_instance=RequestContext(request)
     )
 
-def category_detail( request, category ):
-    context = _category_detail( request, category )
+def category_detail(request, category):
+    context = _category_detail(request, category)
     return render_to_response(
             (
-            'page/category/%s/category.html' % ( context["category"].path ),
+            'page/category/%s/category.html' % (context["category"].path),
             'page/category.html',
             ),
             context,
-            context_instance=RequestContext( request )
+            context_instance=RequestContext(request)
         )
 
-def list_content_type( request, category=None, year=None, month=None, day=None, content_type=None, paginate_by=20 ):
-    context = _list_content_type( request, category, year, month, day, content_type, paginate_by )
+def list_content_type(request, category=None, year=None, month=None, day=None, content_type=None, paginate_by=20):
+    context = _list_content_type(request, category, year, month, day, content_type, paginate_by)
     if content_type:
-        ct = get_content_type( content_type )
+        ct = get_content_type(content_type)
     else:
         ct = False
     template_list = []
     if ct:
-        template_list.append( 'page/category/%s/content_type/%s.%s/listing.html' % ( context["cat"].path, ct.app_label, ct.model ) )
-    template_list.append( 'page/category/%s/listing.html' % ( context["cat"].path ) )
+        template_list.append('page/category/%s/content_type/%s.%s/listing.html' % (context["cat"].path, ct.app_label, ct.model))
+    template_list.append('page/category/%s/listing.html' % (context["cat"].path))
     if ct:
-        template_list.append( 'page/content_type/%s.%s/listing.html' % ( ct.app_label, ct.model ) )
-    template_list.append( 'page/listing.html' )
+        template_list.append('page/content_type/%s.%s/listing.html' % (ct.app_label, ct.model))
+    template_list.append('page/listing.html')
     return render_to_response(
             template_list,
             context,
-            context_instance=RequestContext( request )
+            context_instance=RequestContext(request)
         )
 
-def home( request ):
+def home(request):
     """
     Homepage of the actual site.
 
@@ -93,14 +93,14 @@ def home( request ):
     context = _home(request)
     return render_to_response(
             (
-            'page/category/%s/category.html' % ( context['category'].path ),
+            'page/category/%s/category.html' % (context['category'].path),
             'page/category.html',
             ),
             context,
-            context_instance=RequestContext( request )
+            context_instance=RequestContext(request)
         )
 
-def _object_detail( request, category, content_type, slug, year=None, month=None, day=None, url_remainder=None ):
+def _object_detail(request, category, content_type, slug, year=None, month=None, day=None, url_remainder=None):
     """
     Detail view that displays a single object based on its main placement.
 
@@ -114,15 +114,15 @@ def _object_detail( request, category, content_type, slug, year=None, month=None
     Raises:
         Http404 if object or placement doesn't exist or dates doesn't match
     """
-    ct = get_content_type( content_type )
+    ct = get_content_type(content_type)
 
     if category:
-        cat = get_cached_object_or_404( Category, tree_path=category, site__id=settings.SITE_ID )
+        cat = get_cached_object_or_404(Category, tree_path=category, site__id=settings.SITE_ID)
     else:
-        cat = get_cached_object_or_404( Category, tree_parent__isnull=True, site__id=settings.SITE_ID )
+        cat = get_cached_object_or_404(Category, tree_parent__isnull=True, site__id=settings.SITE_ID)
 
     if year:
-        placement = get_cached_object_or_404( Placement,
+        placement = get_cached_object_or_404(Placement,
                     publish_from__year=year,
                     publish_from__month=month,
                     publish_from__day=day,
@@ -132,9 +132,9 @@ def _object_detail( request, category, content_type, slug, year=None, month=None
                     static=False
                 )
     else:
-        placement = get_cached_object_or_404( Placement, category=cat, target_ct=ct, slug=slug, static=True )
+        placement = get_cached_object_or_404(Placement, category=cat, target_ct=ct, slug=slug, static=True)
 
-    if not ( placement.is_active() or request.user.is_staff ):
+    if not (placement.is_active() or request.user.is_staff):
         # future placement, render if accessed by logged in staff member
         raise Http404
 
@@ -150,18 +150,18 @@ def _object_detail( request, category, content_type, slug, year=None, month=None
 
     # check for custom actions
     if url_remainder:
-        bits = url_remainder.split( '/' )
-        return dispatcher.call_view( request, bits, context )
-    elif dispatcher.has_custom_detail( obj ):
+        bits = url_remainder.split('/')
+        return dispatcher.call_view(request, bits, context)
+    elif dispatcher.has_custom_detail(obj):
         # increment hit counter
 #        HitCount.objects.hit( placement )
-        return dispatcher.call_custom_detail( request, context )
+        return dispatcher.call_custom_detail(request, context)
 
     # increment hit counter
 #    HitCount.objects.hit( placement )
 
     return context
-    
+
 
 def get_templates(name, slug=None, category=None, app_label=None, model_label=None):
     """
@@ -178,11 +178,11 @@ def get_templates(name, slug=None, category=None, app_label=None, model_label=No
     if category:
         if app_label and model_label:
             if slug:
-                templates.append('page/category/%s/content_type/%s.%s/%s/%s' % ( category.path, app_label, model_label, slug, name ))
-            templates.append('page/category/%s/content_type/%s.%s/%s' % ( category.path, app_label, model_label, name ))
-        templates.append('page/category/%s/%s' % ( category.path, name ))
+                templates.append('page/category/%s/content_type/%s.%s/%s/%s' % (category.path, app_label, model_label, slug, name))
+            templates.append('page/category/%s/content_type/%s.%s/%s' % (category.path, app_label, model_label, name))
+        templates.append('page/category/%s/%s' % (category.path, name))
     if app_label and model_label:
-        templates.append('page/content_type/%s.%s/%s' % ( app_label, model_label, name ))
+        templates.append('page/content_type/%s.%s/%s' % (app_label, model_label, name))
     templates.append('page/%s' % name)
     return templates
 
@@ -199,7 +199,7 @@ def get_templates_from_placement(name, placement, slug=None, category=None, app_
         model_label = placement.target._meta.module_name
     return get_templates(name, slug, category, app_label, model_label)
 
-def _list_content_type( request, category=None, year=None, month=None, day=None, content_type=None, paginate_by=20 ):
+def _list_content_type(request, category=None, year=None, month=None, day=None, content_type=None, paginate_by=20):
     """
     List objects' listings according to the parameters.
 
@@ -216,55 +216,55 @@ def _list_content_type( request, category=None, year=None, month=None, day=None,
     kwa = {}
     dates_kwa = {}
     if year:
-        current_date = datetime( int(year), 1, 1 )
-        dates_kwa['publish_from__year'] = int( year )
+        current_date = datetime(int(year), 1, 1)
+        dates_kwa['publish_from__year'] = int(year)
         date_kind = 'month'
 
     if month:
         try:
-            current_date = datetime( int(year), int(month), 1 )
+            current_date = datetime(int(year), int(month), 1)
         except ValueError:
             raise Http404
-        dates_kwa['publish_from__month'] = int( month )
+        dates_kwa['publish_from__month'] = int(month)
         date_kind = 'day'
 
     if day:
         try:
-            current_date = datetime( int(year), int(month), int(day) )
+            current_date = datetime(int(year), int(month), int(day))
         except ValueError:
             raise Http404
-        dates_kwa['publish_from__day'] = int( day )
+        dates_kwa['publish_from__day'] = int(day)
         date_kind = 'detail'
 
     if category:
-        cat = get_cached_object_or_404( Category, tree_path=category, site__id=settings.SITE_ID )
+        cat = get_cached_object_or_404(Category, tree_path=category, site__id=settings.SITE_ID)
         kwa['category__tree_path__startswith'] = category
     else:
-        cat = get_cached_object_or_404( Category, tree_parent__isnull=True, site__id=settings.SITE_ID )
+        cat = get_cached_object_or_404(Category, tree_parent__isnull=True, site__id=settings.SITE_ID)
         kwa['category'] = cat
 
     if content_type:
-        ct = get_content_type( content_type )
+        ct = get_content_type(content_type)
         kwa['content_types'] = [ ct ]
     else:
         ct = False
 
     # pagination
     if 'p' in request.GET and request.GET['p'].isdigit():
-        page_no = int( request.GET['p'] )
+        page_no = int(request.GET['p'])
     else:
         page_no = 1
 
-    base_qset = Listing.objects.get_queryset( **kwa )
-    kwa.update( dates_kwa )
-    qset = Listing.objects.get_queryset( **kwa )
-    paginator = QuerySetPaginator( qset.filter( **dates_kwa ), paginate_by )
+    base_qset = Listing.objects.get_queryset(**kwa)
+    kwa.update(dates_kwa)
+    qset = Listing.objects.get_queryset(**kwa)
+    paginator = QuerySetPaginator(qset.filter(**dates_kwa), paginate_by)
 
     if page_no > paginator.num_pages or page_no < 1:
         raise Http404
 
     kwa['count'] = paginate_by
-    kwa['offset'] = ( page_no - 1 ) * paginate_by + 1
+    kwa['offset'] = (page_no - 1) * paginate_by + 1
     page = paginator.page(page_no)
     listings = page.object_list
 
@@ -276,16 +276,16 @@ def _list_content_type( request, category=None, year=None, month=None, day=None,
 
     # list of years with published objects
     year_list = [
-            ( d, d.strftime( DATE_REPR["year"] ), url_prepend + d.strftime( YEAR_URLS[date_kind] ) )
-            for d in base_qset.dates( 'publish_from', "year", order='DESC' )
+            (d, d.strftime(DATE_REPR["year"]), url_prepend + d.strftime(YEAR_URLS[date_kind]))
+            for d in base_qset.dates('publish_from', "year", order='DESC')
         ]
 
     # list of dates with published objects
     date_list = None
     if date_kind != 'detail':
         date_list = [
-                ( d, d.strftime( DATE_REPR[date_kind] ), url_prepend + d.strftime( DATE_URLS[date_kind] ) + url_apend )
-                for d in qset.dates( 'publish_from', date_kind, order='DESC' )
+                (d, d.strftime(DATE_REPR[date_kind]), url_prepend + d.strftime(DATE_URLS[date_kind]) + url_apend)
+                for d in qset.dates('publish_from', date_kind, order='DESC')
             ]
 
     context = {
@@ -296,23 +296,35 @@ def _list_content_type( request, category=None, year=None, month=None, day=None,
             'year_list' : year_list,
             'date_list' : date_list,
             'current_date' : current_date,
-            'current_date_text' : current_date.strftime( CURRENT_DATE_REPR[date_kind] ),
+            'current_date_text' : current_date.strftime(CURRENT_DATE_REPR[date_kind]),
             'date_kind' : date_kind,
 
             'content_type' : content_type,
             'listings' : listings,
             'category' : cat,
         }
-    
+
     return context
 
 # format lookups for year_list and date_list
 DATE_URLS = { 'month' : '../%Y/%m/', 'day' : '../../%Y/%m/%d/', }
-YEAR_URLS = { 'month' : '../%Y/', 'day' : '../../%Y/', 'detail' : '../../../%Y/',  }
-DATE_REPR = { 'year' : '%Y', 'month' : '%m/%Y', 'day' : '%d/%m/%Y', 'detail' : '%d/%m/%Y',}
-CURRENT_DATE_REPR = { 'month' : '%Y', 'day' : '%m/%Y', 'detail' : '%d/%m/%Y',}
+YEAR_URLS = { 'month' : '../%Y/', 'day' : '../../%Y/', 'detail' : '../../../%Y/', }
+DATE_REPR = { 'year' : '%Y', 'month' : '%m/%Y', 'day' : '%d/%m/%Y', 'detail' : '%d/%m/%Y', }
+CURRENT_DATE_REPR = { 'month' : '%Y', 'day' : '%m/%Y', 'detail' : '%d/%m/%Y', }
 
-def _home( request ):
+def __archive_entry_year(category):
+    " Return ARCHIVE_ENTRY_YEAR from settings (if exists) or year of the newest object in category "
+    year = getattr(settings, 'ARCHIVE_ENTRY_YEAR', None)
+    if not year:
+        now = datetime.now()
+        try:
+            categories = Category.objects.filter(site__id=settings.SITE_ID, tree_path__startswith=category.tree_path)
+            year = Listing.objects.filter(category__in=categories, publish_from__lte=now)[0].publish_from.year
+        except:
+            year = now.year
+    return year
+
+def _home(request):
     """
     Homepage of the actual site.
 
@@ -322,15 +334,16 @@ def _home( request ):
     Raise:
         Http404 if there is no base category
     """
-    cat = get_cached_object_or_404( Category, tree_parent__isnull=True, site__id=settings.SITE_ID )
+    cat = get_cached_object_or_404(Category, tree_parent__isnull=True, site__id=settings.SITE_ID)
     context = {
                 'category' : cat,
                 'is_homepage': True,
+                'archive_entry_year' : __archive_entry_year(cat),
             }
-    
+
     return context
-    
-def _category_detail( request, category ):
+
+def _category_detail(request, category):
     """
     Homepage of a given category.
 
@@ -341,19 +354,20 @@ def _category_detail( request, category ):
     Raise:
         Http404 if the category doesn't exist
     """
-    cat = get_cached_object_or_404( Category, tree_path=category, site__id=settings.SITE_ID )
+    cat = get_cached_object_or_404(Category, tree_path=category, site__id=settings.SITE_ID)
     context = {
-                'category' : cat
+                'category' : cat,
+                'archive_entry_year' : __archive_entry_year(cat)
             }
     return context
 
-def get_export_key( func, request, count, name='', content_type=None ):
+def get_export_key(func, request, count, name='', content_type=None):
     return 'ella.core.views.export:%d:%d:%s:%s' % (
             settings.SITE_ID, count, name, content_type
         )
 
-@cache_this( get_export_key, timeout=CACHE_TIMEOUT_LONG )
-def export( request, count, name='', content_type=None ):
+@cache_this(get_export_key, timeout=CACHE_TIMEOUT_LONG)
+def export(request, count, name='', content_type=None):
     """
     Export banners.
 
@@ -364,15 +378,15 @@ def export( request, count, name='', content_type=None ):
     """
     t_list = []
     if name:
-        t_list.append( 'page/export/%s.html' % name )
-    t_list.append( 'page/export/banner.html' )
+        t_list.append('page/export/%s.html' % name)
+    t_list.append('page/export/banner.html')
 
-    cat = get_cached_object_or_404( Category, tree_parent__isnull=True, site__id=settings.SITE_ID )
-    listing = Listing.objects.get_listing( count=count, category=cat )
+    cat = get_cached_object_or_404(Category, tree_parent__isnull=True, site__id=settings.SITE_ID)
+    listing = Listing.objects.get_listing(count=count, category=cat)
     return render_to_response(
             t_list,
             { 'category' : cat, 'listing' : listing },
-            context_instance=RequestContext( request ),
+            context_instance=RequestContext(request),
             content_type=content_type
         )
 
@@ -380,12 +394,12 @@ def export( request, count, name='', content_type=None ):
 ##
 # Error handlers
 ##
-def page_not_found( request ):
-    response = render_to_response( 'page/404.html', {}, context_instance=RequestContext( request ) )
+def page_not_found(request):
+    response = render_to_response('page/404.html', {}, context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
-def handle_error( request ):
-    response = render_to_response( 'page/500.html', {}, context_instance=RequestContext( request ) )
+def handle_error(request):
+    response = render_to_response('page/500.html', {}, context_instance=RequestContext(request))
     response.status_code = 500
-    return response 
+    return response
