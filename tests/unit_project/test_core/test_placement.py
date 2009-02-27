62e61c9d8f5ddd912485fa4595a263859e665d11
Honza Kral <Honza.Kral@gmail.com>
2009-02-27 20:11:54 +0100

    Some additional placement tests

--- tests/unit_project/test_core/test_placement.py
+++ tests/unit_project/test_core/test_placement.py
@@ -2,10 +2,13 @@
 from datetime import datetime
 from djangosanetesting import DatabaseTestCase
 
+from django.contrib.redirects.models import Redirect
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
 from ella.core.models import Placement, Category
+# choose Article as an example publishable, we cannot use Publishable directly
+# because it's abstract
 from ella.articles.models import Article
 
 class TestPlacement(DatabaseTestCase):
@@ -46,15 +49,10 @@ class TestPlacement(DatabaseTestCase):
             publish_from=datetime(2008,1,10)
         )
 
-        self.placement_home = Placement.objects.create(
-            target_ct=self.article_ct,
-            target_id=self.article.pk,
-            category=self.category,
-            publish_from=datetime(2008,1,10)
-        )
-
     def test_home_url(self):
-        self.assert_equals('/2008/1/10/articles/first-article/', self.placement_home.get_absolute_url())
+        self.placement.category = self.category
+        self.placement.save()
+        self.assert_equals('/2008/1/10/articles/first-article/', self.placement.get_absolute_url())
 
     def test_url(self):
         self.assert_equals('/nested-category/2008/1/10/articles/first-article/', self.placement.get_absolute_url())
@@ -73,4 +71,29 @@ class TestPlacement(DatabaseTestCase):
         self.placement.save()
         self.assert_equals('/nested-category/2008/1/10/articles/old-article-new-slug/', self.placement.get_absolute_url())
 
+    def test_custom_slug_rename(self):
+        self.placement.slug = 'old-article-new-slug'
+        self.placement.save()
+        self.article.slug = 'first-article-second-slug'
+        self.article.save()
+        self.assert_equals('/nested-category/2008/1/10/articles/old-article-new-slug/', self.placement.get_absolute_url())
 
+    def test_url_change_creates_redirect(self):
+        self.placement.slug = 'old-article-new-slug'
+        self.placement.save()
+        self.assert_equals(1, Redirect.objects.count())
+        r = Redirect.objects.all()[0]
+        self.assert_equals('/nested-category/2008/1/10/articles/first-article/', r.old_path)
+        self.assert_equals('/nested-category/2008/1/10/articles/old-article-new-slug/', r.new_path)
+        self.assert_equals(self.site_id, r.site_id)
+
+    def test_url_change_updates_existing_redirects(self):
+        r = Redirect.objects.create(site_id=self.site_id, new_path='/nested-category/2008/1/10/articles/first-article/', old_path='some-path')
+        self.placement.slug = 'old-article-new-slug'
+        self.placement.save()
+        self.assert_equals(2, Redirect.objects.count())
+        r = Redirect.objects.get(pk=r.pk)
+        self.assert_equals('some-path', r.old_path)
+        self.assert_equals('/nested-category/2008/1/10/articles/old-article-new-slug/', r.new_path)
+        self.assert_equals(self.site_id, r.site_id)
+    
