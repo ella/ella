e50a49a329e9e6ad3bd3a1a8013fae1525dc4045
Vitek Pliska <whit@jizak.cz>
2009-03-10 16:29:00 +0100

    Custom login view that loads newman user's settings into session.

--- ella/newman/sites.py
+++ ella/newman/sites.py
@@ -4,16 +4,21 @@ from django.shortcuts import render_to_response
 from django.utils.functional import update_wrapper
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.db import models
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import EmailMessage
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import authenticate, login
 
 from ella.newman.forms import SiteFilterForm
-from django.http import HttpResponse
-from django.core.mail import EmailMessage
 from ella.newman.models import AdminSetting
 from ella.newman.decorators import require_AJAX
 from ella.newman.utils import json_decode, json_encode
 
 
 NEWMAN_URL_PREFIX = 'nm'
+
+# user config variables
+CATEGORY_FILTER = 'category_filter'
 
 class NewmanSite(AdminSite):
 
@@ -53,6 +58,66 @@ class NewmanSite(AdminSite):
         urlpatterns += super(NewmanSite, self).get_urls()
         return urlpatterns
 
+    @never_cache
+    def login(self, request):
+        """
+        Displays the login form for the given HttpRequest.
+        """
+        from django.contrib.auth.models import User
+
+        ERROR_MESSAGE = _("Please enter a correct username and password. Note that both fields are case-sensitive.")
+        LOGIN_FORM_KEY = 'this_is_the_login_form'
+
+        # If this isn't already the login page, display it.
+        if not request.POST.has_key(LOGIN_FORM_KEY):
+            if request.POST:
+                message = _("Please log in again, because your session has expired.")
+            else:
+                message = ""
+            return self.display_login_form(request, message)
+
+        # Check that the user accepts cookies.
+        if not request.session.test_cookie_worked():
+            message = _("Looks like your browser isn't configured to accept cookies. Please enable cookies, reload this page, and try again.")
+            return self.display_login_form(request, message)
+        else:
+            request.session.delete_test_cookie()
+
+        # Check the password.
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user is None:
+            message = ERROR_MESSAGE
+            if u'@' in username:
+                # Mistakenly entered e-mail address instead of username? Look it up.
+                try:
+                    user = User.objects.get(email=username)
+                except (User.DoesNotExist, User.MultipleObjectsReturned):
+                    message = _("Usernames cannot contain the '@' character.")
+                else:
+                    if user.check_password(password):
+                        message = _("Your e-mail address is not your username."
+                                    " Try '%s' instead.") % user.username
+                    else:
+                        message = _("Usernames cannot contain the '@' character.")
+            return self.display_login_form(request, message)
+
+        # The user data is correct; log in the user in and continue.
+        else:
+            if user.is_active and user.is_staff:
+                login(request, user)
+
+                # load all user's specific settings into session
+                user_config = {}
+                for c in AdminSetting.objects.filter(user=user):
+                    user_config[c.var] = c.val
+                request.session['user_config'] = user_config
+
+                return HttpResponseRedirect(request.get_full_path())
+            else:
+                return self.display_login_form(request, ERROR_MESSAGE)
+
     def index(self, request, extra_context=None):
         """
         Displays the main Newman index page, without installed apps.
@@ -60,9 +125,8 @@ class NewmanSite(AdminSite):
 
         data = {'sites': []}
         try:
-            category_filter = AdminSetting.objects.get(user=request.user, var='category_filter')
-            data['sites'] = json_decode(category_filter.val)
-        except AdminSetting.DoesNotExist:
+            data['sites'] = json_decode(request.session['user_config'][CATEGORY_FILTER])
+        except KeyError:
             data['sites'] = []
 
         site_filter_form = SiteFilterForm(data=data, user=request.user)
@@ -103,10 +167,13 @@ class NewmanSite(AdminSite):
         if site_filter_form.is_valid():
             o, c = AdminSetting.objects.get_or_create(
                 user = request.user,
-                var = 'category_filter'
+                var = CATEGORY_FILTER
             )
             o.val = '%s' % json_encode(site_filter_form.cleaned_data['sites'])
             o.save()
+            conf = request.session['user_config']
+            conf[CATEGORY_FILTER] = o.val
+            request.session['user_config'] = conf
 
             return HttpResponse(content=ugettext('Your settings was saved.'), mimetype='text/plain', status=200)
         else:
