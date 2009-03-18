215cb2381ed27b9178ad27fa194fdef5e5e1a783
Ondrej Kohout <ondrej.kohout@centrumholdings.com>
2009-03-18 10:55:57 +0100

    Tests - Example project - new style configuration

--- tests/example_project/settings/base.py
+++ tests/example_project/settings/base.py
@@ -1,68 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Django settings for example_project project.
-
 from os.path import dirname, join
 
+import django
+
 import example_project
+import ella
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-DISABLE_CACHE_TEMPLATE = DEBUG # TODO IKY
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/tmp/example_project.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Prague'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'cs'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = join(dirname(example_project.__file__), 'static')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/admin_media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '^980$0s46q1(toq*mu23m41_ac_@vwy)+mig=ka_97$m0^fh)v'
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+    'ella.core.cache.template_loader.load_template_source',
+    'ella.utils.template_loaders.load_template_from_app',
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+)
+
+CACHE_TEMPLATE_LOADERS = (
+    'ella.db_templates.loader.load_template_source',
+    'ella.utils.template_loaders.load_template_from_app',
+    'django.template.loaders.filesystem.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,29 +29,36 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'example_project.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    join(dirname(example_project.__file__), 'templates'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'ella.newman.context_processors.newman_media',
 )
 
 INSTALLED_APPS = (
-    'south',
-    'ella.core',
-    'ella.articles',
-    'ella.photos',
-    'ella.positions',
-    'ella.newman',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-)
+    'django.contrib.redirects',
 
-DEFAULT_PAGE_ID = 1
+    'ella.core',
+    'ella.db_templates',
+    'ella.photos',
+    'ella.articles',
+    'ella.newman',
+    'ella.positions',
+
+    'example_project.services',
+
+    'django.contrib.admin',
+    'ella.ellaadmin',
+
+#    'south',
+)
 
 VERSION = 1
--- tests/example_project/settings/config.py
+++ tests/example_project/settings/config.py
@@ -1,5 +1,7 @@
 from tempfile import gettempdir
-from os.path import join
+from os.path import join, dirname
+
+import example_project
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -7,11 +9,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 DISABLE_CACHE_TEMPLATE = DEBUG
-
 
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = join(gettempdir(), 'ella_example_project.db')
@@ -21,12 +21,12 @@ DATABASE_PASSWORD = ''
 DATABASE_HOST = ''
 DATABASE_PORT = ''
 
-
 TIME_ZONE = 'Europe/Prague'
 
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
+USE_I18N = True
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '88b-01f^x4lh$-s5-hdccnicekg07)niir2g6)93!0#k(=mfv$'
@@ -40,3 +40,16 @@ SECRET_KEY = '88b-01f^x4lh$-s5-hdccnicekg07)niir2g6)93!0#k(=mfv$'
 # we want to reset whole cache in test
 # until we do that, don't use cache
 CACHE_BACKEND = 'dummy://'
+
+# session expire
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# disable double render in admin
+DOUBLE_RENDER = False
+
+MEDIA_ROOT = join(dirname(example_project.__file__), 'static')
+
+MEDIA_URL = '/static/'
+
+ADMIN_MEDIA_PREFIX = '/static/admin_media/'
+NEWMAN_MEDIA_PREFIX = '/static/newman_media/'
