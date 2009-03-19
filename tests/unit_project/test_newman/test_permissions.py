387048a57f756f417206f67afc16f9bb25822fdc
Vitek Pliska <whit@jizak.cz>
2009-03-19 14:31:11 +0100

    test_newman NewmanTestCase in __init__.

--- tests/unit_project/test_newman/test_permissions.py
+++ tests/unit_project/test_newman/test_permissions.py
@@ -1,76 +1,24 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User, Group, Permission
-from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
-from djangosanetesting.cases import DatabaseTestCase
-
-from ella.core.models import Category, Author
+from ella.core.models import Category
 from ella.articles.models import Article, ArticleContents
 
 from ella.newman.models import CategoryUserRole
 from ella.newman.permission import has_category_permission, has_object_permission, compute_applicable_categories
 from ella.newman.permission import applicable_categories, permission_filtered_model_qs, is_category_fk
 from ella.newman.permission import model_category_fk, model_category_fk_value, has_model_list_permission
+from unit_project.test_newman import NewmanTestCase
 
-class UserWithPermissionTestCase(DatabaseTestCase):
+class UserWithPermissionTestCase(NewmanTestCase):
 
     def setUp(self):
         super(UserWithPermissionTestCase, self).setUp()
 
-        self.site = Site.objects.get(name='example.com')
-
-        self.user = User.objects.create(
-            username='newman',
-            first_name='Paul',
-            last_name='Newman',
-            email='',
-            is_active=True,
-            is_staff=True,
-            is_superuser=False
-        )
-
-        self.author = Author.objects.create(name='igorko', slug='igorko')
-
-        self.create_categories()
         self.create_permissions()
         self.create_groups()
         self.create_roles()
-
-    def create_categories(self):
-        site = self.site
-        # Category Tree A
-        self.category_top_level = Category.objects.create(slug='a0-top', title='a0 top', description='A top category', site=site)
-        self.nested_first_level = Category.objects.create(
-            tree_parent=self.category_top_level,
-            slug='a1-first-nested-level',
-            title='a1 first nested level',
-            description='nested',
-            site=site
-        )
-        self.nested_first_level_two = Category.objects.create(
-            tree_parent=self.category_top_level,
-            slug='a2-first-nested-level',
-            title='a2 first nested level',
-            description='nested',
-            site=site
-        )
-        self.nested_second_level = Category.objects.create(
-            tree_parent=self.nested_first_level,
-            slug='a4-second-nested-level',
-            title='a4 second nested level',
-            description='nested',
-            site=site
-        )
-        self.nested_second_level_two = Category.objects.create(
-            tree_parent=self.nested_first_level_two,
-            slug='a5-second-nested-level',
-            title='a5 second nested level',
-            description='nested',
-            site=site
-        )
-
-        self.categories = [self.category_top_level, self.nested_first_level, self.nested_first_level_two, self.nested_second_level, self.nested_second_level_two]
 
     def create_permissions(self):
         self.article_ct = ContentType.objects.get_for_model(Article)
