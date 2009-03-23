e67dcb023cb7bbdb029fb7843c1867cb3a96fba6
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-03-23 11:09:38 +0100

    Calendar filter modified. refs #2278

--- ella/newman/admin.py
+++ ella/newman/admin.py
@@ -88,6 +88,18 @@ def created_field_filter(fspec):
     now = datetime.now()
     cal = calendar.Calendar()
     year = now.year
+    qs = fspec.model_admin.queryset(fspec.request)
+    # SELECT created FROM qs._meta.dbtable  GROUP BY created
+    dates =  qs.dates(fspec.field_path, 'day', 'DESC')[:365] 
+    for date in dates:
+        lookup_dict = dict()
+        lookup_dict['%s__day' % fspec.field_path] = date.day
+        lookup_dict['%s__month' % fspec.field_path] = date.month
+        lookup_dict['%s__year' % fspec.field_path] = date.year
+        link_text = '%d. %d. %d' % (date.day, date.month, date.year)
+        link = ( link_text, lookup_dict)
+        fspec.links.append(link)
+    """
     for month in range(1, 13):
         for day in cal.itermonthdays(year, month):
             if not day:
@@ -100,5 +112,6 @@ def created_field_filter(fspec):
             link_text = '%d. %d. %d' % (day, month, year)
             link = ( link_text, lookup_dict)
             fspec.links.append(link)
+    """
     return True
 
