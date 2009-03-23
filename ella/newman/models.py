5fb87d500b93e7c632d759c9d3112b9f66236fbf
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-03-23 12:15:53 +0100

    Missing import fixed.

--- ella/newman/models.py
+++ ella/newman/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User, Group
 from django.conf import settings
