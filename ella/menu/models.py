d85b655981960c33601a726502952fe2964b715e
Honza Kral <Honza.Kral@gmail.com>
2009-03-25 22:45:59 +0100

    Wrong import

--- ella/menu/models.py
+++ ella/menu/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
