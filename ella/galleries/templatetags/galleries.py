ff71ed23c9063f6e9b6811666b81a90a8513cba5
Jan Seifert <jan.seifert@centrumholdings.com>
2009-04-02 09:56:30 +0200

    Added gallery_content_json template tag, improved gallery_item_detail view

--- ella/galleries/templatetags/galleries.py
+++ ella/galleries/templatetags/galleries.py
@@ -1,6 +1,7 @@
 import math
 
 from django import template
+from django.utils import simplejson
 
 register = template.Library()
 
@@ -25,3 +26,26 @@ def gallery_offset(value, param):
 def gallery_width(value, param):
     " Helper template filter to calculate width of the preview pane. "
     return gallery_func(lambda x,y,z: (math.ceil(x / float(y))) * z, value, param)
+
+@register.simple_tag
+def gallery_content_json(gallery, thumb_format):
+    """
+    Returns gallery items as a JSON object
+    Ussage:
+    gallery_content_json gallery "photo_format_name"
+    """
+
+    photos = []
+
+    for item_key in gallery.items:
+        gallery_item, target_object = gallery.items[item_key]
+        photos.append({
+                       'src': target_object.get_formated_photo(thumb_format).url,
+                       'link': gallery_item.get_absolute_url(),
+                       'ajaxLink': gallery_item.get_absolute_url(),
+                       'alt': target_object.description
+                      })
+
+    content = { 'thumbnails': photos }
+
+    return simplejson.dumps( photos, ensure_ascii=False, indent=4 )
