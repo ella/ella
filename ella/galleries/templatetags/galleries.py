8b34b992c243981ffbeb7bd9b19c7833f3cbd914
Jan Seifert <jan.seifert@centrumholdings.com>
2009-04-07 15:35:21 +0200

    Added gallery_navigation template tag

--- ella/galleries/templatetags/galleries.py
+++ ella/galleries/templatetags/galleries.py
@@ -32,7 +32,7 @@ def gallery_content_json(gallery, thumb_format):
     """
     Returns gallery items as a JSON object
     Ussage:
-    gallery_content_json gallery "photo_format_name"
+    gallery_content_json <gallery> "photo_format_name"
     """
 
     photos = []
@@ -49,3 +49,80 @@ def gallery_content_json(gallery, thumb_format):
     content = { 'thumbnails': photos }
 
     return simplejson.dumps( photos, ensure_ascii=False, indent=4 )
+
+@register.tag
+def gallery_navigation(parser, token):
+    """
+    Template tag gallery_navigation
+
+    Usage::
+        {% gallery_navigation for <gallery> step <X> current_position <X> as <result> %}
+
+    Example::
+        {% gallery_navigation for gallery step 5 current_position 8 as navigation %}
+    """
+
+    try:
+        tag_name, for_arg, gallery, step_arg, step, position_arg, position, as_arg, result = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "Four arguments are expected in %r tag" % token.split_contents()[0]
+    
+    params = {}
+    
+    if for_arg == 'for':
+        params['for'] = gallery
+    else:
+        raise template.TemplateSyntaxError, "Unknown argument %r in tag %r" % (for_arg, tag_name)
+    if step_arg == 'step':
+        params['step'] = step
+    else:
+        raise template.TemplateSyntaxError, "Unknown argument %r in tag %r" % (step_arg, tag_name)
+    if position_arg == 'current_position':
+        params['position'] = position
+    else:
+        raise template.TemplateSyntaxError, "Unknown argument %r in tag %r" % (position_arg, tag_name)
+    if as_arg == 'as':
+        params['as'] = result
+    else:
+        raise template.TemplateSyntaxError, "Unknown argument %r in tag %r" % (as_arg, tag_name)
+
+    return GalleryNavigationNode(params)
+
+class GalleryNavigationNode(template.Node):
+    def __init__(self, params):
+        self.params = params
+
+    def render(self, context):
+        gallery = template.Variable( self.params['for'] ).resolve(context)
+        position = template.Variable( self.params['position'] ).resolve(context)
+        step = int( self.params['step'] )
+        count = len( gallery.items )
+        
+        navigation = []
+        
+        if count > 0:
+            keys = gallery.items.keys()
+            
+            stop = count / step
+            if stop * step < count:
+                 stop = stop + 1
+            
+            item_page = position / step
+            if item_page * step < position:
+                item_page = item_page + 1
+            
+            for i in range(0, stop):
+                part_stop  = i * step + step
+                page = i + 1
+                on_current_page = item_page == page and True or False
+
+                navigation.append({
+                                   'url_first': gallery.items[ keys[i * step] ][0].get_absolute_url(),
+                                   'start': i * step + 1,
+                                   'stop': part_stop > count and count or part_stop,
+                                   'page': page,
+                                   'on_current_page': on_current_page
+                                  })
+
+        context[ self.params['as'] ] = navigation
+        return ''
