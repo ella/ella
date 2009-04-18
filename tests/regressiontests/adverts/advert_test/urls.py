1c536808b2c10e04cbf5d9d5c8c5c0329fbb4929
Honza Kral <Honza.Kral@gmail.com>
2009-04-18 14:15:28 +0200

    Removed tests for features that have been ripped out

--- tests/regressiontests/adverts/advert_test/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^adverts_test/', include('adverts_test.foo.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
-)
