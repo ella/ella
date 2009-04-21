84e9c6a07fb1e69b16e386b6bada39eeda1c8dde
Jakub Vysoky <jakub@borka.cz>
2009-04-21 15:10:21 +0200

    use __versionstr__ in setup.py

--- djangobaselibrary/__init__.py
+++ djangobaselibrary/__init__.py
@@ -3,3 +3,4 @@ VERSION = (0, 1, 0)
 
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
+
--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup, find_packages
 import djangobaselibrary
 
-version = "%d.%d.%d" % djangobaselibrary.VERSION
+# all fields marked with TODO: REPLACE
+# must be filled with some meanigful values
 
 setup(
     name = 'djangobaselibrary',
-    version = version,
-    description = 'django base library',
+    version = djangobaselibrary.__versionstr__,
+    description = 'Django Base Library', # TODO: REPLACE
     long_description = '\n'.join((
-        'django base library',
-        'template for other libs',
+        'Django Base Library',
+        '',
+        'this project (python module) is meant as a template',
+        'for any centrumholdings django based',
+        '(even non-django, pure python) libraries',
     )),
-    author = 'centrum holdings s.r.o',
-    author_email = 'author_email@example.com',
-    license = 'BSD',
-    url = 'http://example.com',
+    author = 'centrum holdings s.r.o', # TODO: REPLACE
+    author_email='devel@centrumholdings.com', # TODO: REPLACE
+    license = 'BSD', # TODO: REPLACE
+    url='http://git.netcentrum.cz/projects/django/GIT/django-base-library.git/', # TODO: REPLACE
 
     packages = find_packages(
         where = '.',
@@ -23,14 +27,7 @@ setup(
 
     include_package_data = True,
 
-    entry_points={
-        'setuptools.file_finders': ['dummy = setuptools_entry:dummylsfiles'],
-    },
-
-    install_requires = [
-        'setuptools>=0.6b1',
-    ],
-
+    # TODO: REPLACE
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -40,6 +37,15 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points = {
+        'setuptools.file_finders': ['dummy = setuptools_entry:dummylsfiles'],
+    },
+    install_requires = [
+        'setuptools>=0.6b1',
+    ],
+    setup_requires = [
+#        'setuptools_git', # TODO: add dummy to pypi?
     ],
 )
 
