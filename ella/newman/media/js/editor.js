43f37e53be95743c96abf36c27cd6ff80b147885
Maxim Izmailov <kvakes@gmail.com>
2009-04-29 02:45:21 +0800

    Czechtile -> markdown

--- ella/newman/media/js/editor.js
+++ ella/newman/media/js/editor.js
@@ -1,6 +1,6 @@
 $(function(){
-	$('.rich_text_area').markItUp(CTTASettings);
+	$('.rich_text_area').markItUp(mySettings);
 });
 $(document).bind('content_added', function() {
-	$('.rich_text_area').markItUpRemove().markItUp(CTTASettings);
+	$('.rich_text_area').markItUpRemove().markItUp(mySettings);
 });
