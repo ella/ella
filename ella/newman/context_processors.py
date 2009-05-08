50b8dfd6699fe69ac619e7996716c5919ef7c24a
Vitek Pliska <whit@jizak.cz>
2009-05-08 12:14:56 +0200

    Added DEBUG to context.

--- ella/newman/context_processors.py
+++ ella/newman/context_processors.py
@@ -6,8 +6,10 @@ def newman_media(request):
     settings, if not available, use MEDIA_URL + 'newman_media/' combination
     """
     uri = getattr(settings, 'NEWMAN_MEDIA_PREFIX', None)
+    debug = settings.DEBUG
     if not uri:
         uri = getattr(settings, 'MEDIA_URL') + 'newman_media/'
     return {
-        'NEWMAN_MEDIA_URL' : uri
+        'NEWMAN_MEDIA_URL' : uri,
+        'DEBUG': False
     }
