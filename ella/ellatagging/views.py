0512946ee5ab767590c041fc8a1ba46abb4382e5
Ondrej Kohout <ondrej.kohout@centrumholdings.com>
2009-05-12 16:57:15 +0200

    Tagged_publishables views update  * uses djangotagging  * uses querysets instead of direct sql querying

--- ella/ellatagging/views.py
+++ ella/ellatagging/views.py
@@ -2,71 +2,34 @@
 Tagging related views.
 """
 import urllib
+from datetime import datetime
 
 from django.template import RequestContext, Context
 from django.template.defaultfilters import slugify
 from django.shortcuts import render_to_response
 from django.db import connection
+from django.shortcuts import get_object_or_404
 
 from tagging.models import Tag, TaggedItem
 
 from ella.core.models import Placement
 from ella.core.models import Publishable
 
-
-#@cache_this TODO
-def _get_tagged_placements(tag_name):
-    """ returns placements """
-    tags = Tag.objects.filter(name=tag_name)
-    qn = connection.ops.quote_name
-    if not tags:
-        return []
-    tag = tags[0]
-    sql = """
-        SELECT
-            p.id
-        FROM
-            %(tab_tagged_item)s AS t,
-            %(tab_placement)s AS p
-        WHERE
-            p.target_id = t.object_id
-            AND
-            p.target_ct_id = t.content_type_id
-            AND
-            t.tag_id = %(tag_id)d
-            AND
-            p.publish_from <= NOW()
-        GROUP BY
-            p.id
-        ORDER BY
-            p.publish_from, p.publish_to
-    """ % {
-        'tag_id': tag.pk,
-        'tab_tagged_item': qn(TaggedItem._meta.db_table),
-        'tab_placement': qn(Placement._meta.db_table),}
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    placements = map(lambda row: Placement.objects.get(pk=row[0]), cursor.fetchall())
-    return placements
-
 def tagged_publishables(request, tag):
-    """ return tagged Publishable objects (i.e. Articles, Galleries,...) """
-    things = []
-    for p in _get_tagged_placements(tag):
-        t = p.target
-        if isinstance(t, Publishable):
-            things.append(t)
-    cx = Context({
-        'objects': things,
+    tag = get_object_or_404(Tag, name=tag)
+    qs = Publishable.objects.filter(placement__publish_from__lt=datetime.now())
+    object_list = TaggedItem.objects.get_by_model(qs, tag)
+    context = Context({
+        'objects': object_list,
         'paginate_by': 10,
         'tag': tag,
         'extra_context': {'tag': tag},
-        'object_list': things,})
+        'object_list': object_list,
+    })
     return render_to_response(
         [
             'page/tagging/%s/listing.html' % slugify(tag),
             'page/tagging/listing.html',
-            #'page/tagging/view_publishables.html',
         ],
-        cx,
+        context,
         context_instance=RequestContext(request))
