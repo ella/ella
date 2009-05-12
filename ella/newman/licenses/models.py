7f8837bf15dcac6ec9534ff2eb693ccd9518392f
Vitek Pliska <whit@jizak.cz>
2009-05-12 23:27:35 +0200

    Bugfix in LicenseManager, typo.

--- ella/newman/licenses/models.py
+++ ella/newman/licenses/models.py
@@ -4,17 +4,17 @@ from django.utils.translation import ugettext_lazy as _
 
 from ella.core.cache.utils import CachedGenericForeignKey
 from ella.core.models import Dependency
+from ella.newman.licenses import LICENSED_MODELS
 
 class LicenseManager(models.Manager):
     def unapplicable_for_model(self, model):
         ct = ContentType.objects.get_for_model(model)
-        qs = License.objects.filter(ct=ct).extra(where=['applications=max_applications']).only('id')
-        return [u.id for u in qs]
+        qs = License.objects.filter(ct=ct).extra(where=['applications=max_applications']).only('obj_id')
+        return [u.obj_id for u in qs]
 
     def reflect_changed_dependencies(self, before, after):
-        # TODO
+        """ Update current applications if target model is licensed. """
         pass
-
 
 class License(models.Model):
 
