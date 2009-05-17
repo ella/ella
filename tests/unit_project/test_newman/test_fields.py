c68e2c944aa57d56677774eac2443e9caad24c0c
Honza Kral <Honza.Kral@gmail.com>
2009-05-17 20:40:20 +0200

    Added more tests for license/dependency handling that deals with more than one field on a model

--- tests/unit_project/test_newman/test_fields.py
+++ tests/unit_project/test_newman/test_fields.py
@@ -69,6 +69,30 @@ class TestRichTextFieldDependencyHandling(RichTextFieldTestCase):
 
         self.assert_equals(0, Dependency.objects.all().count())
 
+    def test_object_still_used_in_one_field_doesnt_affect_dependencies(self):
+        dep = Dependency()
+        dep.target = self.category
+        dep.dependent = self.publishable
+        dep.save()
+
+        text = 'some-text {%% box inline for core.category with pk %s %%}{%% endbox %%}' % self.category.pk
+
+        field2 = fields.NewmanRichTextField(
+            instance=self.publishable,
+            model=self.publishable.__class__,
+            field_name = "title",
+        )
+
+        self.publishable.title = field2.clean('no box here')
+        self.publishable.description = self.field.clean(text)
+        self.publishable.save()
+
+        self.assert_equals(1, Dependency.objects.all().count())
+        dep = Dependency.objects.all()[0]
+        self.assert_equals(self.category, dep.target)
+        self.assert_equals(self.publishable, dep.dependent)
+
+
 class TestRichTextFieldLicenseHandling(RichTextFieldTestCase):
 
     def test_use_of_unlicensed_object_doesnt_do_anything(self):
@@ -79,9 +103,8 @@ class TestRichTextFieldLicenseHandling(RichTextFieldTestCase):
         self.assert_equals(0, License.objects.count())
 
     def test_use_of_licensed_object_raises_the_applications_counter(self):
-        l = License()
+        l = License(max_applications=2)
         l.target = self.category
-        l.max_applications = 2
         l.save()
 
         text = 'some-text {%% box inline for core.category with pk %s %%}{%% endbox %%}' % self.category.pk
@@ -90,10 +113,8 @@ class TestRichTextFieldLicenseHandling(RichTextFieldTestCase):
         self.assert_equals(1, License.objects.get(pk=l.pk).applications)
 
     def test_still_using_licensed_object_doesnt_change_its_applications(self):
-        l = License()
+        l = License(max_applications=2, applications=2)
         l.target = self.category
-        l.max_applications = 2
-        l.applications = 2
         l.save()
 
         dep = Dependency()
@@ -108,10 +129,8 @@ class TestRichTextFieldLicenseHandling(RichTextFieldTestCase):
         self.assert_equals(2, License.objects.get(pk=l.pk).applications)
 
     def test_no_longer_using_licensed_object_lowers_its_applications(self):
-        l = License()
+        l = License(max_applications=2, applications=2)
         l.target = self.category
-        l.max_applications = 2
-        l.applications = 2
         l.save()
 
         dep = Dependency()
@@ -125,7 +144,45 @@ class TestRichTextFieldLicenseHandling(RichTextFieldTestCase):
 
         self.assert_equals(1, License.objects.get(pk=l.pk).applications)
 
+    def test_two_uses_in_two_fields_on_one_object_count_as_one(self):
+        l = License(max_applications=2, applications=1)
+        l.target = self.category
+        l.save()
 
+        text = 'some-text {%% box inline for core.category with pk %s %%}{%% endbox %%}' % self.category.pk
+        self.publishable.description = self.field.clean(text)
 
+        field2 = fields.NewmanRichTextField(
+            instance=self.publishable,
+            model=self.publishable.__class__,
+            field_name = "title",
+        )
+        self.publishable.description = field2.clean(text)
+        self.publishable.save()
+
+        self.assert_equals(2, License.objects.get(pk=l.pk).applications)
+
+    def test_object_still_used_in_one_field_doesnt_affect_applications(self):
+        l = License(max_applications=2, applications=1)
+        l.target = self.category
+        l.save()
+
+        dep = Dependency()
+        dep.target = self.category
+        dep.dependent = self.publishable
+        dep.save()
+
+        text = 'some-text {%% box inline for core.category with pk %s %%}{%% endbox %%}' % self.category.pk
+        self.publishable.description = self.field.clean(text)
+
+        field2 = fields.NewmanRichTextField(
+            instance=self.publishable,
+            model=self.publishable.__class__,
+            field_name = "title",
+        )
+        self.publishable.description = field2.clean('no box here')
+        self.publishable.save()
+
+        self.assert_equals(1, License.objects.get(pk=l.pk).applications)
 
 
