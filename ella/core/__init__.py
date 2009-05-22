88e79eeb9746735d0dce7e906331a2e43bcb8786
Honza Kral <Honza.Kral@gmail.com>
2009-05-22 18:26:47 +0200

    keep templatetags in templatetags

--- ella/core/__init__.py
+++ ella/core/__init__.py
@@ -1,13 +1,2 @@
 from django.utils.translation import ugettext_lazy as _
 trans_app_label = _('Core')
-
-# FIXME FOLLOWING part is copied from templatetags/__init__.py, I don't know WARRUM, ABER ES IST NOETIG.
-from django import template
-
-# add core templatetags to builtin so that you don't have to invoke {% load core %} in every template
-template.add_to_builtins('ella.core.templatetags.core')
-# keep this here for backwards compatibility
-template.add_to_builtins('ella.core.templatetags.related')
-# and the same for i18n
-template.add_to_builtins('django.templatetags.i18n')
-
--- ella/core/templatetags/__init__.py
+++ ella/core/templatetags/__init__.py
@@ -2,6 +2,8 @@ from django import template
 
 # add core templatetags to builtin so that you don't have to invoke {% load core %} in every template
 template.add_to_builtins('ella.core.templatetags.core')
+# keep this here for backwards compatibility
+template.add_to_builtins('ella.core.templatetags.related')
 # and the same for i18n
 template.add_to_builtins('django.templatetags.i18n')
 
