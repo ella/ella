1365741664f7a7b2d4ceef69fc6a08bb56d5a80e
Vitek Pliska <whit@jizak.cz>
2009-05-22 17:16:49 +0200

    Mental activity: -10

--- ella/photos/models.py
+++ ella/photos/models.py
@@ -126,7 +126,7 @@ class Photo(models.Model):
                 # due to PIL has read several bytes from image, position in file has to be reset
                 self.image.seek(0)
             super(Photo, self).save(force_insert, force_update)
-            self.width, self.height = get_image_dimensions(self.image)
+            self.width, self.height = get_image_dimensions(self.image.path)
             self.slug = str(self.id) + '-' + self.slug
             force_insert, force_update = False, True
             image_changed = False
