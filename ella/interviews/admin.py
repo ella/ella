b875d9495ae727f660dc4970d62b43a9a186c446
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-05-22 14:42:24 +0200

    Monkeypatching fo rellaadmin

--- ella/interviews/admin.py
+++ ella/interviews/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.conf import settings
 
-from ella.core.admin import PlacementInlineOptions
 from ella.interviews.models import Interview, Question, Interviewee, Answer
 from ella.ellaadmin.options import EllaAdminOptionsMixin, EllaModelAdmin
 
@@ -33,7 +31,7 @@ class InterviewOptions(EllaAdminOptionsMixin, admin.ModelAdmin):
     raw_id_fields = ('photo', 'interviewees',)
     search_fields = ('title', 'perex',) # FIXME: 'tags__tag__name',)
     prepopulated_fields = {'slug' : ('title',)}
-    inlines = [ QuestionInlineOptions, PlacementInlineOptions ]
+    inlines = [ QuestionInlineOptions ]
     rich_text_fields = {None: ('perex', 'content',)}
 #    suggest_fields = {'category': ('tree_path', 'title', 'slug',), 'authors': ('name', 'slug',),
 #        'source': ('name',), 'interviewees': ('name', 'author__name', 'user__first_name', 'user__last_name'),}
