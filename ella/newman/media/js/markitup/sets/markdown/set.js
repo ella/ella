f2cb4b62dd48e9944052477c85bbbe0d4eb24861
Vitek Pliska <whit@jizak.cz>
2009-05-23 10:16:09 +0200

    Box inserting for markdown fixed.

--- ella/newman/media/js/markitup/sets/markdown/set.js
+++ ella/newman/media/js/markitup/sets/markdown/set.js
@@ -12,107 +12,107 @@
 // -------------------------------------------------------------------\
 
 mySettings = {
-	previewParserPath:    BASE_URL + 'nm/editor-preview/',
-	previewParserVar:   "text",
-	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
-	markupSet: [
-		{ name: 'Zvýrazněně', className: 'italic', key: 'I', openWith: '_', closeWith: '_' },
-		{ name: 'Silně', className: 'bold', key: 'B', openWith: '**', closeWith: '**' },
-		{ separator: '---------------' },
-		{ name: 'Odkaz', className: 'url', key: 'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder: 'Text odkazu' },
-		{ separator: '---------------' },
-		{ name: 'Nadpis 1', className: 'h1', key:'1', placeHolder: 'Nadpis 1. úrovně', closeWith: function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{ name: 'Nadpis 2', className: 'h2', key:'2', placeHolder: 'Nadpis 2. úrovně', closeWith: function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-		{ name: 'Nadpis 3', className: 'h3', key:'3', placeHolder: 'Nadpis 3. úrovně', openWith: '### ' },
-		{ separator: '---------------' },
-		{ name: 'Seznam s odrážkami', className: 'list-bullet', openWith: '- ' },
-		{ name: 'Číslovaný seznam', className: 'list-numeric', openWith:function(markItUp) {
-			return markItUp.line+'. ';
-		}},
-		{ separator: '---------------' },
-		{ name: 'Сitát', className: 'quote', openWith: '> ' },
-		{ name: 'Box', className: 'box', call: function(){
-			$('#rich-box').dialog('open');
-		}},
-		{ name: 'Náhled', call: 'preview', className: 'preview'}
-	]
+    previewParserPath:    BASE_URL + 'nm/editor-preview/',
+    previewParserVar:   "text",
+    onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
+    markupSet: [
+        { name: 'Zvýrazněně', className: 'italic', key: 'I', openWith: '_', closeWith: '_' },
+        { name: 'Silně', className: 'bold', key: 'B', openWith: '**', closeWith: '**' },
+        { separator: '---------------' },
+        { name: 'Odkaz', className: 'url', key: 'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder: 'Text odkazu' },
+        { separator: '---------------' },
+        { name: 'Nadpis 1', className: 'h1', key:'1', placeHolder: 'Nadpis 1. úrovně', closeWith: function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
+        { name: 'Nadpis 2', className: 'h2', key:'2', placeHolder: 'Nadpis 2. úrovně', closeWith: function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+        { name: 'Nadpis 3', className: 'h3', key:'3', placeHolder: 'Nadpis 3. úrovně', openWith: '### ' },
+        { separator: '---------------' },
+        { name: 'Seznam s odrážkami', className: 'list-bullet', openWith: '- ' },
+        { name: 'Číslovaný seznam', className: 'list-numeric', openWith:function(markItUp) {
+            return markItUp.line+'. ';
+        }},
+        { separator: '---------------' },
+        { name: 'Сitát', className: 'quote', openWith: '> ' },
+        { name: 'Box', className: 'box', call: function(){
+            $('#rich-box').dialog('open');
+        }},
+        { name: 'Náhled', call: 'preview', className: 'preview'}
+    ]
 }
 
 $(function(){
-	if(!$('#rich-box').length){
-		$('<div id="rich-box" title="Vložit box"></div>').hide().appendTo('body');
-		$.getJSON('/static/newman_media/js/boxes.js', function(data){
-			if(!!data){
-				// HTML
-				$('#rich-box').append('<form id="rich-object"></form>');
-				$('#rich-object')
-					.append('<ul id="o-quick"></ul>')
-					.append('<div style="clear:left;"><select id="rich-select"></select></div>')
-					.append('<div><input type="text" id="rich-object-id" autocomplete="off" /> <input type="button" id="rich-object-choose" value="Choose" /></div>')
-					.append('<div><textarea id="rich-object-parameters" cols="30" rows="5"></textarea></div>')
-					.append('<div><input type="submit" value="Add box" /> <input type="reset" value="CLear" /></div>');
-				$.each(data, function(i, item){
-					$('#rich-select').append('<option value="'+i+'" class="rich-object-'+i+'">'+item.name+'</option>');
-					if(item.quick){
-						$('#o-quick').append('<li title="'+i+'">'+item.name+'</li>');
-					}
-				});
-				// Events
-				$('#rich-select').bind('change', function(){
-					if($(this).val() == 'photos.photo' || $(this).val() == 'galleries.gallery'){
-						$('#rich-object-choose').show();
-					} else {
-						$('#rich-object-choose').hide();
-					}
-				});
-				$('#o-quick').bind('click', function(e){
-					if(e.target.tagName.toLowerCase() == 'li'){
-						$('option[value='+$(e.target).attr('title')+']', '#rich-select').attr('selected', 'selected');
-						$('#rich-select').trigger('change');
-					}
-				});
-				$('#rich-object').bind('submit', function(e){
-					e.preventDefault();
-					var type = $('#rich-select').val();
-					if(!!type){
-						var id = $('#rich-object-id').val() || '0';
-						var params = $('#rich-object-parameters').val().replace(/\n+/g, ' ');
-						// Insert code
-						$.markItUp({
-							openWith:'((box inline '+type+' '+$('#rich-object-id').val()+' '+params+'))'
-						});
-						// Reset and close dialog
-						$('#rich-object').trigger('reset');
-						$('#rich-box').dialog('close');
-					}
-				}).bind('reset', function(){
-					$('#rich-object-choose').show();
-				});
-				$('#rich-object-choose').bind('click', function(){
-					open_overlay($('#rich-select').val(), function(id){
-						$('#rich-object-id').val(id);
-					});
-				})
-			}
-		});
-		$('#rich-box').dialog({
-			modal: true,
-			autoOpen: false,
-			width: 400,
-			height: 300
-		});
-	}
-	$('.rich_text_area').markItUp(mySettings);
+    if(!$('#rich-box').length){
+        $('<div id="rich-box" title="Vložit box"></div>').hide().appendTo('body');
+        $.getJSON('/static/newman_media/js/boxes.js', function(data){
+            if(!!data){
+                // HTML
+                $('#rich-box').append('<form id="rich-object"></form>');
+                $('#rich-object')
+                    .append('<ul id="o-quick"></ul>')
+                    .append('<div style="clear:left;"><select id="rich-select"></select></div>')
+                    .append('<div><input type="text" id="rich-object-id" autocomplete="off" /> <input type="button" id="rich-object-choose" value="Choose" /></div>')
+                    .append('<div><textarea id="rich-object-parameters" cols="30" rows="5"></textarea></div>')
+                    .append('<div><input type="submit" value="Add box" /> <input type="reset" value="CLear" /></div>');
+                $.each(data, function(i, item){
+                    $('#rich-select').append('<option value="'+i+'" class="rich-object-'+i+'">'+item.name+'</option>');
+                    if(item.quick){
+                        $('#o-quick').append('<li title="'+i+'">'+item.name+'</li>');
+                    }
+                });
+                // Events
+                $('#rich-select').bind('change', function(){
+                    if($(this).val() == 'photos.photo' || $(this).val() == 'galleries.gallery'){
+                        $('#rich-object-choose').show();
+                    } else {
+                        $('#rich-object-choose').hide();
+                    }
+                });
+                $('#o-quick').bind('click', function(e){
+                    if(e.target.tagName.toLowerCase() == 'li'){
+                        $('option[value='+$(e.target).attr('title')+']', '#rich-select').attr('selected', 'selected');
+                        $('#rich-select').trigger('change');
+                    }
+                });
+                $('#rich-object').bind('submit', function(e){
+                    e.preventDefault();
+                    var type = $('#rich-select').val();
+                    if(!!type){
+                        var id = $('#rich-object-id').val() || '0';
+                        var params = $('#rich-object-parameters').val().replace(/\n+/g, ' ');
+                        // Insert code
+                        $.markItUp({
+                            openWith:'{% box inline for '+type+' with pk '+$('#rich-object-id').val()+' %}\n'+params+'\n{% endbox %}'
+                        });
+                        // Reset and close dialog
+                        $('#rich-object').trigger('reset');
+                        $('#rich-box').dialog('close');
+                    }
+                }).bind('reset', function(){
+                    $('#rich-object-choose').show();
+                });
+                $('#rich-object-choose').bind('click', function(){
+                    open_overlay($('#rich-select').val(), function(id){
+                        $('#rich-object-id').val(id);
+                    });
+                })
+            }
+        });
+        $('#rich-box').dialog({
+            modal: true,
+            autoOpen: false,
+            width: 400,
+            height: 300
+        });
+    }
+    $('.rich_text_area').markItUp(mySettings);
 });
 
 // mIu nameSpace to avoid conflict.
 miu = {
-	markdownTitle: function(markItUp, char) {
-		heading = '';
-		n = $.trim(markItUp.selection||markItUp.placeHolder).length;
-		for(i = 0; i < n; i++) {
-			heading += char;
-		}
-		return '\n'+heading;
-	}
+    markdownTitle: function(markItUp, char) {
+        heading = '';
+        n = $.trim(markItUp.selection||markItUp.placeHolder).length;
+        for(i = 0; i < n; i++) {
+            heading += char;
+        }
+        return '\n'+heading;
+    }
 }
