069c9a0eb1c0cea169a10f55d8fe732df5df48b3
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-05-26 11:34:02 +0200

    Selecting content type will now open overlay.
    No need to click on lupicka any more
    (content type widget only -- listings).

--- ella/newman/media/js/related_lookup.js
+++ ella/newman/media/js/related_lookup.js
@@ -59,6 +59,14 @@
 
 // Lupicka for listings
 ( function($) {
+    function id2ct(id) {
+        var ct = AVAILABLE_CONTENT_TYPES[ id ];
+        if ( ! ct ) {
+            carp('Unrecognized Content-Type id: '+id);
+            return;
+        }
+        return ct.path;
+    }
     $('.generic-related-lookup').live('click', function() {
         var id_stem = this.id.replace(/^lookup_/, '').replace(/_id$/, '');
         var $id_input = $('#'+id_stem+'_id');
@@ -75,15 +83,33 @@
             .change(function(){$(this).removeClass('highlighted')});
             return false;
         }
-        var ct = AVAILABLE_CONTENT_TYPES[ ct_id ];
-        if ( ! ct ) {
-            carp('Unrecognized Content-Type id: '+ct_id);
-            return false;
-        }
-        var content_type = ct.path.substr(1).replace(/\/$/, '').replace(/\//, '.');
-        open_overlay(content_type, function(id) {
-            $id_input.val( id );
-        });
+        var content_type = id2ct(ct_id);
+        open_overlay(content_type, function(id) { $id_input.val(id); });
         return false;
+    });
+    
+    // When content-type is selected, no need to wait for lupicka to be clicked. Fire the overlay right away.
+    function open_overlay_on_ct_selection() {
+        var ct_id = $(this).val();
+        if ( ! ct_id ) return;
+        var content_type = id2ct(ct_id);
+        var id_input_id = this.id.replace(/ct$/, 'id');
+        if (id_input_id == this.id) {
+            carp('Error attempting to attach open_overlay to select.target_ct onchange: Unexpected ID: '+this.id, this);
+            return;
+        }
+        var $id_input = $('#'+id_input_id);
+        if ($id_input.length == 0) {
+            carp('Error attempting to attach open_overlay to select.target_ct onchange: Failed to get ID input #'+id_input_id);
+            return;
+        }
+        open_overlay(content_type, function(id) { $id_input.val(id); });
+    }
+    $('select.target_ct').unbind('change', open_overlay_on_ct_selection).bind('change', open_overlay_on_ct_selection);
+    $(document).bind('content_added', function(evt) {
+        var $target = $(evt.target);
+        var $ct_sel = $target.find('select.target_ct');
+        if ($ct_sel.length == 0) return;
+        $ct_sel.unbind('change', open_overlay_on_ct_selection).bind('change', open_overlay_on_ct_selection);
     });
 })(jQuery);
