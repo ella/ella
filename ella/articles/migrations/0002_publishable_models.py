bef165d6f3cdd33495338c91c6a6eecb997fc2d2
Jakub Vysoky <jakub@borka.cz>
2009-05-26 13:43:31 +0200

    empty lines removed

--- ella/articles/migrations/0002_publishable_models.py
+++ ella/articles/migrations/0002_publishable_models.py
@@ -4,73 +4,73 @@ from django.db import models
 from ella.articles.models import *
 
 class Migration:
-    
+
     def forwards(self, orm):
-        
+
         # Adding field 'Article.publishable_ptr'
         db.add_column('articles_article', 'publishable_ptr', models.OneToOneField(orm['core.Publishable']))
-        
+
         # Deleting field 'Article.category'
         db.delete_column('articles_article', 'category_id')
-        
+
         # Deleting field 'Article.perex'
         db.delete_column('articles_article', 'perex')
-        
+
         # Dropping ManyToManyField 'Article.authors'
         db.delete_table('articles_article_authors')
-        
+
         # Deleting field 'Article.id'
         db.delete_column('articles_article', 'id')
-        
+
         # Deleting field 'Article.slug'
         db.delete_column('articles_article', 'slug')
-        
+
         # Deleting field 'Article.photo'
         db.delete_column('articles_article', 'photo_id')
-        
+
         # Deleting field 'Article.source'
         db.delete_column('articles_article', 'source_id')
-        
+
         # Deleting field 'Article.title'
         db.delete_column('articles_article', 'title')
-        
-    
-    
+
+
+
     def backwards(self, orm):
-        
+
         # Deleting field 'Article.publishable_ptr'
         db.delete_column('articles_article', 'publishable_ptr_id')
-        
+
         # Adding field 'Article.category'
         db.add_column('articles_article', 'category', models.ForeignKey(orm['core.Category'], verbose_name=_('Category')))
-        
+
         # Adding field 'Article.perex'
         db.add_column('articles_article', 'perex', models.TextField(_('Perex')))
-        
+
         # Adding ManyToManyField 'Article.authors'
         db.create_table('articles_article_authors', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
             ('article', models.ForeignKey(orm.Article, null=False)),
             ('author', models.ForeignKey(orm['core.author'], null=False))
         ))
-        
+
         # Adding field 'Article.id'
         db.add_column('articles_article', 'id', models.AutoField(primary_key=True))
-        
+
         # Adding field 'Article.slug'
         db.add_column('articles_article', 'slug', models.SlugField(_('Slug'), max_length=255))
-        
+
         # Adding field 'Article.photo'
         db.add_column('articles_article', 'photo', models.ForeignKey(orm['photos.Photo'], null=True, verbose_name=_('Photo'), blank=True))
-        
+
         # Adding field 'Article.source'
         db.add_column('articles_article', 'source', models.ForeignKey(orm['core.Source'], null=True, verbose_name=_('Source'), blank=True))
-        
+
         # Adding field 'Article.title'
         db.add_column('articles_article', 'title', models.CharField(_('Title'), max_length=255))
-        
-    
-    
+
+
+
     models = {
         'core.category': {
             'Meta': {'unique_together': "(('site','tree_path'),)", 'app_label': "'core'"},
@@ -124,5 +124,5 @@ class Migration:
             'id': ('models.AutoField', [], {'primary_key': 'True'})
         }
     }
-    
+
     complete_apps = ['articles']
