c962df04cabd24d913624270b277a130b34982b3
Jakub Vysoky <jakub@borka.cz>
2009-05-27 12:20:48 +0200

    automatic south migration moved away

--- ella/articles/migrations/0002_publishable_models.py
+++ ella/articles/migrations/0002_publishable_models.py
@@ -18,8 +18,10 @@ class Migration:
 
         # migrate publishables
         self.forwards_publishable(orm)
+
         # migrate generic relations
         #self.forwards_generic_relations(orm)
+
         # migrate placements
         #self.forwards_placements(orm)
 
@@ -148,73 +150,10 @@ class Migration:
         db.delete_column('core_placement', 'target_ct_id')
         db.delete_column('core_placement', 'target_id')
 
-    def ___forwards_original(self, orm):
-
-        # Adding field 'Article.publishable_ptr'
-        db.add_column('articles_article', 'publishable_ptr', models.OneToOneField(orm['core.Publishable']))
-
-        # Deleting field 'Article.category'
-        db.delete_column('articles_article', 'category_id')
-
-        # Deleting field 'Article.perex'
-        db.delete_column('articles_article', 'perex')
-
-        # Dropping ManyToManyField 'Article.authors'
-        db.delete_table('articles_article_authors')
-
-        # Deleting field 'Article.id'
-        db.delete_column('articles_article', 'id')
-
-        # Deleting field 'Article.slug'
-        db.delete_column('articles_article', 'slug')
-
-        # Deleting field 'Article.photo'
-        db.delete_column('articles_article', 'photo_id')
-
-        # Deleting field 'Article.source'
-        db.delete_column('articles_article', 'source_id')
-
-        # Deleting field 'Article.title'
-        db.delete_column('articles_article', 'title')
-
 
     def backwards(self, orm):
         "Write your backwards migration here"
         print 'there is no way back'
-
-    def ___backwards_original(self, orm):
-
-        # Deleting field 'Article.publishable_ptr'
-        db.delete_column('articles_article', 'publishable_ptr_id')
-
-        # Adding field 'Article.category'
-        db.add_column('articles_article', 'category', models.ForeignKey(orm['core.Category'], verbose_name=_('Category')))
-
-        # Adding field 'Article.perex'
-        db.add_column('articles_article', 'perex', models.TextField(_('Perex')))
-
-        # Adding ManyToManyField 'Article.authors'
-        db.create_table('articles_article_authors', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('article', models.ForeignKey(orm.Article, null=False)),
-            ('author', models.ForeignKey(orm['core.author'], null=False))
-        ))
-
-        # Adding field 'Article.id'
-        db.add_column('articles_article', 'id', models.AutoField(primary_key=True))
-
-        # Adding field 'Article.slug'
-        db.add_column('articles_article', 'slug', models.SlugField(_('Slug'), max_length=255))
-
-        # Adding field 'Article.photo'
-        db.add_column('articles_article', 'photo', models.ForeignKey(orm['photos.Photo'], null=True, verbose_name=_('Photo'), blank=True))
-
-        # Adding field 'Article.source'
-        db.add_column('articles_article', 'source', models.ForeignKey(orm['core.Source'], null=True, verbose_name=_('Source'), blank=True))
-
-        # Adding field 'Article.title'
-        db.add_column('articles_article', 'title', models.CharField(_('Title'), max_length=255))
-
 
 
     models = {
