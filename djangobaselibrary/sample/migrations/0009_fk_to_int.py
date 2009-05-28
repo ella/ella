20bf4fa9a6e9c238edc7a2fdab1f80ab036107a7
Jakub Vysoky <jakub@borka.cz>
2009-05-28 17:23:35 +0200

    hack around removing foreign key constraint

--- djangobaselibrary/sample/migrations/0009_fk_to_int.py
+++ djangobaselibrary/sample/migrations/0009_fk_to_int.py
@@ -4,25 +4,27 @@ from django.db import models
 from djangobaselibrary.sample.models import *
 
 class Migration:
-    
+
     def forwards(self, orm):
-        
+
         # Changing field 'Spam.type'
         db.alter_column('sample_spam', 'type_id', models.IntegerField())
         # and rename
         db.rename_column('sample_spam', 'type_id', 'type')
-        
-    
-    
+        # don't know any other way how to destroy FOREIGN KEY CONSTRAINT
+        db.add_column('sample_spam', 'type_id', models.IntegerField())
+        db.delete_column('sample_spam', 'type_id')
+
+
     def backwards(self, orm):
 
         # rename first
         db.rename_column('sample_spam', 'type', 'type_id')
         # Changing field 'Spam.type'
         db.alter_column('sample_spam', 'type_id', models.ForeignKey(orm['sample.Type']))
-        
-    
-    
+
+
+
     models = {
         'sample.spam': {
             'Meta': {'unique_together': "(('name','expires'),)"},
@@ -39,5 +41,5 @@ class Migration:
             'name': ('models.CharField', [], {'unique': 'True', 'max_length': '255'})
         }
     }
-    
+
     complete_apps = ['sample']
