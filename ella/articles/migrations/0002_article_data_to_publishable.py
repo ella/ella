0a3028f442f1819b4df1a4120725ef7a8f0c7ad4
Jakub Vysoky <jakub@borka.cz>
2009-05-28 20:39:22 +0200

    migration of articles merged to migrations_base

--- ella/articles/migrations/0002_article_data_to_publishable.py
+++ ella/articles/migrations/0002_article_data_to_publishable.py
@@ -3,7 +3,7 @@ from south.db import db
 from django.db import models
 from ella.articles.models import *
 
-from ella.core.migrations_base import BasePublishableDataMigration
+from ella.core.migrations_base import BasePublishableDataMigration, alter_foreignkey_to_int
 
 class Migration(BasePublishableDataMigration):
 
@@ -12,7 +12,13 @@ class Migration(BasePublishableDataMigration):
     table = '%s_%s' % (app_label, model)
 
     publishable_uncommon_cols = {
-        'source_id': 'source_id', 
+        'source_id': 'source_id',
         'description': 'perex',
     }
+
+    def alter_self_foreignkeys(self, orm):
+        alter_foreignkey_to_int('articles_articlecontents', 'article')
+        # TODO: migrate new article IDs to articlecontents
+        alter_foreignkey_to_int('recipes_oldrecipearticleredirect', 'new_id')
+        # TODO: migrate new article IDs to oldrecipearticleredirect
 
