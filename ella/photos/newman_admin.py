5b16b73fc0dd7369c47129a19f55251ccdaac662
Vitek Pliska <whit@jizak.cz>
2009-06-02 11:00:35 +0200

    Json photo info for newman.

--- ella/photos/newman_admin.py
+++ ella/photos/newman_admin.py
@@ -3,16 +3,19 @@ from django.utils.translation import ugettext_lazy as _
 from ella import newman
 
 from ella.photos.models import FormatedPhoto, Format, Photo
+from django.conf.urls.defaults import patterns, url
+from ella.newman.utils import JsonResponse, JsonResponseError
+from ella.newman.config import STATUS_OBJECT_NOT_FOUND
 
-class FormatOptions(newman.NewmanModelAdmin):
+class FormatAdmin(newman.NewmanModelAdmin):
     list_display = ('name', 'max_width', 'max_height', 'stretch', 'resample_quality',)
     list_filter = ('sites', 'stretch', 'nocrop', 'flexible_height',)
     search_fields = ('name',)
 
-class FormatedPhotoInlineOptions(newman.NewmanTabularInline):
+class FormatedPhotoInlineAdmin(newman.NewmanTabularInline):
     model = FormatedPhoto
 
-class PhotoOptions(newman.NewmanModelAdmin):
+class PhotoAdmin(newman.NewmanModelAdmin):
     inlines = []
     list_display = ('title', 'size', 'thumb', 'pk',)
     list_filter = ('created',)
@@ -24,14 +27,37 @@ class PhotoOptions(newman.NewmanModelAdmin):
         return "%dx%d px" % (obj.width, obj.height)
     size.short_description = _('Size')
 
-class FormatedPhotoOptions(newman.NewmanModelAdmin):
+    def get_urls(self):
+        urlpatterns = patterns('',
+            url(r'^(.+)/thumb/$',
+                self.json_photo_info,
+                name='photo-json-info'),
+        )
+        urlpatterns += super(PhotoAdmin, self).get_urls()
+        return urlpatterns
+
+    def json_photo_info(self, request, object_id, extra_context=None):
+        obj = self.get_change_view_object(object_id)
+
+        if obj is None:
+            return JsonResponseError('Photo does not exist', status=STATUS_OBJECT_NOT_FOUND)
+
+        out = {
+            'title': obj.title,
+            'thumb_url': obj.thumb_url()
+        }
+
+        return JsonResponse('', out)
+
+
+class FormatedPhotoAdmin(newman.NewmanModelAdmin):
     list_display = ('image', 'format', 'width', 'height')
     list_filter = ('format',)
     search_fields = ('image',)
     raw_id_fields = ('photo',)
 
 
-newman.site.register(Format, FormatOptions)
-newman.site.register(Photo, PhotoOptions)
-newman.site.register(FormatedPhoto, FormatedPhotoOptions)
+newman.site.register(Format, FormatAdmin)
+newman.site.register(Photo, PhotoAdmin)
+newman.site.register(FormatedPhoto, FormatedPhotoAdmin)
 
