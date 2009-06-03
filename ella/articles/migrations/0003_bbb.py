5ea761f5cde729e7ebf6395e84f475842791a5a1
Jakub Vysoky <jakub@borka.cz>
2009-06-03 20:52:01 +0200

    using frozen models in plugins

--- ella/articles/migrations/0003_bbb.py
+++ ella/articles/migrations/0003_bbb.py
@@ -14,14 +14,34 @@ class Migration(object):
     def backwards(self, orm):
         print 'articles', '0003_bbb', 'down'
 
-    models = {}
+    models = {
+        'core.publishable': {
+            'Meta': {'app_label': "'core'"},
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
+        'articles.article': {
+            'Meta': {'ordering': "('-created',)", '_bases': ['ella.core.models.publishable.Publishable']},
+            'created': ('models.DateTimeField', ["_('Created')"], {'default': 'datetime.datetime.now', 'editable': 'False', 'db_index': 'True'}),
+            'publishable_ptr': ('models.OneToOneField', ["orm['core.Publishable']"], {}),
+            'updated': ('models.DateTimeField', ["_('Updated')"], {'null': 'True', 'blank': 'True'}),
+            'upper_title': ('models.CharField', ["_('Upper title')"], {'max_length': '255', 'blank': 'True'})
+        },
+    }
+
 
 class Plugin(object):
+    @property
+    def orm(self):
+        return Migration.orm
+
     def forwards(self, orm):
         print 'articles', '0003_bbb', 'plugin', 'up'
+        print self.orm.article
 
     def backwards(self, orm):
         print 'articles', '0003_bbb', 'plugin', 'down'
+        print self.orm.article
 
 p = Plugin()
 k = ("core", "0003_bbb")
--- ella/core/migrations/0003_bbb.py
+++ ella/core/migrations/0003_bbb.py
@@ -6,16 +6,23 @@ from django.db import models
 class Migration(object):
     def forwards(self, orm):
         print 'core', '0003_bbb', 'up'
+        print orm.publishable
         plugins = getattr(south, 'plugins', {})
         for p in plugins.get(("core", "0003_bbb"), set()):
             p.forwards(orm)
 
     def backwards(self, orm):
         print 'core', '0003_bbb', 'down'
+        print orm.publishable
         plugins = getattr(south, 'plugins', {})
         for p in plugins.get(("core", "0003_bbb"), set()):
             p.backwards(orm)
 
     models = {
+        'core.publishable': {
+            'Meta': {'app_label': "'core'"},
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
     }
 
