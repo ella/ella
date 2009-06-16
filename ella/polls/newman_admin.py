d32c4d9e1baa2327eb05252d51c76f6f2e16bb8b
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-06-16 09:45:46 +0200

    Polls application newman_admin changed.  Refs #2798. (Question customized inline)

--- ella/polls/newman_admin.py
+++ ella/polls/newman_admin.py
@@ -3,13 +3,15 @@ from django.forms.models import BaseInlineFormSet
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.forms.util import ValidationError
+from django.template.defaultfilters import striptags
+from django.forms import models as modelforms
 
 from ella import newman
+from ella.newman import fields
 
 from ella.core.newman_admin import PlacementInlineAdmin, PublishableAdmin
 from ella.core.cache import get_cached_object_or_404
 from ella.polls.models import Poll, Contest, Contestant, Quiz, Result, Choice, Vote, Question
-from django.template.defaultfilters import striptags
 
 
 class ResultFormset(BaseInlineFormSet):
@@ -82,13 +84,71 @@ class ContestantAdmin(newman.NewmanModelAdmin):
     list_display = ('name', 'surname', 'user', 'datetime', 'contest', 'points', 'winner')
     rich_text_fields = {'small': ('text_announcement', 'text', 'text_results',)}
 
+class QuestionForm(modelforms.ModelForm):
+    # create the field here to pass validation
+    choices =  fields.ChoiceCustomField(label=_('Choices'))
+
+    class Meta:
+        model = Question
+
+    def __init__(self, *args, **kwargs):
+        initial = []
+        if 'initial' in kwargs:
+            pass
+        elif 'instance' in kwargs:
+            inst = kwargs['instance']
+            for choice in Choice.objects.filter(question=inst):
+                initial.append(choice)
+
+        self.base_fields['choices'] = fields.ChoiceCustomField(label=_('Choices'), initial=initial)
+        super(QuestionForm, self).__init__(*args, **kwargs)
+
+    def get_part_id(self, suffix):
+        id_part = self.data.get('choices_widget')
+        if not suffix:
+            return id_part
+        return '%s-%s' % (id_part, suffix)
+
+    def clean(self):
+        import pdb;pdb.set_trace()
+        # no data - nothing to validate
+        if not self.is_valid() or not self.cleaned_data or not self.instance or not self.cleaned_data['choices']:
+            return self.cleaned_data
+        try:
+            choice_ids = map(lambda v: int(v), self.data.getlist('question_set-0-choices-id'))
+            choice_points = map(lambda v: int(v), self.data.getlist('question_set-0-choices-points'))
+        except:
+            raise ValidationError(_(u'Enter a whole number.'))
+        choice_texts = self.data.getlist('question_set-0-choices') # choices text
+        super(QuestionForm, self).clean()
+
+    def save(self, commit=True):
+        import pdb;pdb.set_trace()
+        try:
+            choice_ids = map(lambda v: int(v), self.data.getlist('question_set-0-choices-id'))
+            choice_points = map(lambda v: int(v), self.data.getlist('question_set-0-choices-points'))
+        except:
+            raise ValidationError(_(u'Enter a whole number.'))
+        choice_texts = self.data.getlist('question_set-0-choices') # choices text
+        for chid, text, points in zip(choice_ids, choice_texts, choice_points):
+            if chid <= 0:
+                new_ch = Choice(points=points, choice=text, votes=0, question=self.cleaned_data)
+                continue
+            ch = Choice.objects.get(pk=chid)
+            if ch.choice != text or ch.points != points:
+                ch.choice = text
+                ch.points = points
+                ch.save()
+
 class QuestionInlineAdmin(newman.NewmanTabularInline):
     model = Question
-    inlines = [ChoiceTabularAdmin]
+    form = QuestionForm
+    #inlines = [ChoiceTabularAdmin]
 #    template = 'admin/polls/question/edit_inline/tabular.html'
+    #template = 'newman/edit_inline/poll_question.html'
     extra = 1
-    rich_text_fields = {'small': ('question',)}
-    fieldsets = ((None, {'fields' : ('question', 'allow_multiple', 'allow_no_choice',)}),)
+    #rich_text_fields = {'small': ('question',)}
+    fieldsets = ((None, {'fields' : ('question', 'allow_multiple', 'allow_no_choice', 'choices',)}),)
 
 class ContestAdmin(PublishableAdmin):
     def __call__(self, request, url):
