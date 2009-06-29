e0bda41c4f232b6dfc7a27c3f74b15b4471aa16d
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-06-29 13:36:59 +0200

    Questions and answer options are now editable and emptiness-aware.

--- ella/newman/media/js/polls.js
+++ ella/newman/media/js/polls.js
@@ -1,17 +1,32 @@
+// Poll change forms
 (function() {
-    $('.js-delete-poll-choice').live('click', function(evt) {
+    
+    // Edit the question text by clicking on it
+    $('.js-edit-poll-question-text').live('click', function(evt) {
         if (evt.button != 0) return;
-        var $cont = $(this).closest('.poll-choice-container');
+        $(this).closest('.poll-question-text-container').children('span').toggle()
+        .find(':input').focus();
+    });
+    
+    // Delete / Undelete answer option
+    function toggle_choice_deletion(el) {
+        var $cont = $(el).closest('.poll-choice-container');
+        var $del = $cont.find('.js-delete-poll-choice');
         if ($cont.is('.poll-choice-deleted')) {
             $cont.removeClass('poll-choice-deleted').find('.poll-choice-delete-input').val('off');
-            $(this).text(gettext('Delete'));
+            $del.text(gettext('Delete'));
         }
         else {
             $cont.addClass('poll-choice-deleted').find('.poll-choice-delete-input').val('on');
-            $(this).text(gettext('Undelete'));
+            $del.text(gettext('Undelete'));
         }
+    }
+    $('.js-delete-poll-choice').live('click', function(evt) {
+        if (evt.button != 0) return;
+        toggle_choice_deletion(this)
     });
     
+    // Edit answer option
     $('a.js-edit-poll-choice-text').live('click', function(evt) {
         if (evt.button != 0) return;
         var $cont = $(this).closest('.poll-choice-container');
@@ -20,6 +35,7 @@
         .filter(':input').focus();
     });
     
+    // Edit points for answer option
     $('a.poll-choice-points').live('click', function(evt) {
         if (evt.button != 0) return;
         $(this).closest('p').find('.poll-choice-points').toggle()
@@ -27,21 +43,44 @@
     });
     
     function non_live_handlers() {
-        $(':input.js-edit-poll-choice-text').unbind('blur.hide').bind('blur.hide', function() {
-            $(this).closest('.poll-choice-container').find('.js-edit-poll-choice-text').toggle()
-            .filter('a').text( $(this).val() );
+        
+        // Done editing question text
+        $('.poll-question-text-container textarea').unbind('blur.hide').bind('blur.hide', function() {
+            var $cont = $(this).closest('.poll-question-text-container');
+            var $label = $cont.find('span:first');
+            $cont.children('span').toggle();
+            
+            $label.find('a').text( $(this).val() || gettext('Click to edit question') );
+            
+            if ( $(this).val() == '' ) $label.addClass('empty-poll-question-text');
+            else $label.removeClass('empty-poll-question-text');
         });
+        
+        // Done editing answer option text
+        $(':input.js-edit-poll-choice-text').unbind('blur.hide').bind('blur.hide', function() {
+            var $cont = $(this).closest('.poll-choice-container');
+            $cont.find('.js-edit-poll-choice-text').toggle()
+            .filter('a').children('span').text( $(this).val() || gettext('Click to edit option') );
+            if ( $(this).val() == '' ) {
+                $('a.js-edit-poll-choice-text span').addClass('empty-poll-choice');
+                if ( ! $cont.is('.poll-choice-deleted') ) {
+                    toggle_choice_deletion(this);
+                }
+            }
+            else {
+                $('a.js-edit-poll-choice-text span').removeClass('empty-poll-choice');
+            }
+        });
+        
+        // Done editing points for answer option
         $('input.poll-choice-points').unbind('blur.hide').bind('blur.hide', function() {
+            if ( $(this).val() == '' ) $(this).val('0');
             $(this).closest('p').find('.poll-choice-points').toggle()
             .filter('a').text( $(this).val() );
         });
+        
     }
     non_live_handlers();
     $(document).bind('content_added', non_live_handlers);
     
-    /*$('.poll-choice-deleted').live('click', function(evt) {
-        if ( $(evt.target).is('.js-delete-poll-choice') ) return;
-        if (evt.button != 0) return;
-        return false;
-    });*/
 })();
