9888a70885f637dd9422d04338e9e0d46db87588
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-08-04 17:23:24 +0200

    Added configuration for docs

--- doc/conf.py
+++ doc/conf.py
@@ -10,8 +10,8 @@ master_doc = 'index'
 project = u'Ella'
 copyright = u'2009, Centrum Holdings'
 
-import ella
+import ella as project
 
-version = ".".join(map(str, ella.__version__[:-1]))
+version = ".".join(map(str, project.__version__[:-1]))
 # The full version, including alpha/beta/rc tags.
-release = ella.__versionstr__
+release = project.__versionstr__
