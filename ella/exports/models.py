b3d195f924c6c9c5a1a8be1206bd836436bd586b
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-08-04 16:30:15 +0200

    export models changed.

--- ella/exports/models.py
+++ ella/exports/models.py
@@ -3,13 +3,20 @@ from django.utils.translation import ugettext_lazy as _
 
 from ella.core.models import Publishable, Category
 from ella.photos.models import Photo
+from ella.exports.managers import ExportManager
 
 
 class Export(models.Model):
-    category = models.ForeignKey(Category, verbose_name=_('Category'))
+    " Export group. "
+    #category = models.ForeignKey(Category, verbose_name=_('Category'))
+    name = models.CharField(_('Name'), max_length=64)
+    objects = ExportManager()
+
+    def __unicode__(self):
+        return u'%s: %s' % (unicode(self._meta.verbose_name), self.name)
 
     class Meta:
-        unique_together = ('category',)
+        unique_together = ('name',)
         verbose_name = _('Export')
         verbose_name_plural = _('Exports')
 
@@ -38,10 +45,14 @@ class ExportMeta(models.Model):
 
 
 class ExportPosition(models.Model):
+    " Defines visibility of ExportMeta object. "
     visible_from = models.DateTimeField()
     visible_to = models.DateTimeField()
     object = models.ForeignKey(ExportMeta, verbose_name=_('Export meta'))
+    export = models.ForeignKey(Export, verbose_name=_('Export'))
 
     class Meta:
         verbose_name = _('Export Position')
         verbose_name_plural = _('Export Positions')
+
+# EOF
