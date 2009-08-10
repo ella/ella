3a9d6270e510eec57371e0879252c1b1f9656793
Jonas Fiala <jonas.fiala@centrumholdings.com>
2009-08-10 15:06:26 +0200

    models updated. Refs #2198

--- ella/exports/models.py
+++ ella/exports/models.py
@@ -5,18 +5,24 @@ from ella.core.models import Publishable, Category
 from ella.photos.models import Photo
 from ella.exports.managers import ExportManager
 
+POSITION_IS_NOT_OVERLOADED = 0
+
+class UnexportableException(Exception):
+    pass
 
 class Export(models.Model):
     " Export group. "
-    #category = models.ForeignKey(Category, verbose_name=_('Category'))
-    name = models.CharField(_('Name'), max_length=64)
+    category = models.ForeignKey(Category, verbose_name=_('Category'))
+    title = models.CharField(_('Title'), max_length=255)
+    slug = models.SlugField(_('Slug'), max_length=255)
+    max_visible_items = models.IntegerField(_('Maximum Visible Fields'))
     objects = ExportManager()
 
     def __unicode__(self):
-        return u'%s: %s' % (unicode(self._meta.verbose_name), self.name)
+        return u'%s: %s (%s)' % (unicode(self._meta.verbose_name), self.title, self.category)
 
     class Meta:
-        unique_together = ('name',)
+        unique_together = ( ('title',), ('slug',) )
         verbose_name = _('Export')
         verbose_name_plural = _('Exports')
 
@@ -39,6 +45,22 @@ class ExportMeta(models.Model):
             raise IntegrityError
         super(ExportMeta, self).save(force_insert, force_update)
 
+    def get_overloaded_attribute(self, attribute):
+        if attribute in self.__dict__ and self.__dict__[attribute]:
+            return self.__dict__[attribute]
+        if attribute in self.publishable.__dict__:
+            return self.publishable.__dict__[attribute]
+        return None
+
+    def get_title(self):
+        return self.get_overloaded_attribute('title')
+
+    def get_photo(self):
+        return self.get_overloaded_attribute('photo')
+
+    def get_description(self):
+        return self.get_overloaded_attribute('description')
+
     class Meta:
         verbose_name = _('Export meta')
         verbose_name_plural = _('Export metas')
@@ -47,9 +69,19 @@ class ExportMeta(models.Model):
 class ExportPosition(models.Model):
     " Defines visibility of ExportMeta object. "
     visible_from = models.DateTimeField()
-    visible_to = models.DateTimeField()
+    visible_to = models.DateTimeField(null=True)
+    position = models.IntegerField(blank=True, default=POSITION_IS_NOT_OVERLOADED)
     object = models.ForeignKey(ExportMeta, verbose_name=_('Export meta'))
     export = models.ForeignKey(Export, verbose_name=_('Export'))
+
+    def save(self, **kwargs):
+        if self.export:
+            if self.export.max_visible_items < self.position:
+                raise IntegrityError
+        super(ExportPosition, self).save(**kwargs)
+
+    def __unicode__(self):
+        return u'#%d %s - %s for %s' % (self.position, self.visible_from, self.visible_to, self.object)
 
     class Meta:
         verbose_name = _('Export Position')
