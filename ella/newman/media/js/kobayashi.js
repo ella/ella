8f610e86c90b184891f9b2ec2a58757203492ad4
Oldrich Kruza <oldrich.kruza@centrumholdings.com>
2009-08-12 17:30:05 +0200

    JavaScript redirects seem to work as desired. Fixes #2923
    Callbacks to load_content are now called after the request
    is processed, even if it redirects.
    Redirecting requests now chain correctly in request queue.
    
    load_content now accepts boolean option is_priority_request.
    When set to true, it prepends the request to the queue instead
    of appending it. Should only be used internally for redirects.
    
    New function alloc_loadbuf(is_priority?) returns index to
    LOAD_BUF for a new request. If cannot make a priority request,
    returns an index as for a normal one. A priority request can
    only be made when the queue is empty (in which case priority
    doesn't matter) or when a request has already been processed
    and the queue thus has free slots on the start.
    
    Error callback is now called when the request succeeds but target
    element is not found.
    
    Callbacks to load_content now get no parameters but get the object
    passed to load_content as 'this'.

--- ella/newman/media/js/kobayashi.js
+++ ella/newman/media/js/kobayashi.js
@@ -131,6 +131,7 @@ ContentByHashLib.LOADED_MEDIA = {};
                 literal_address = redirect_to;
             }
             arg.address = literal_address;
+            arg.is_priority_request = true;
             load_content(arg);
             if ( extras.by_hash ) {
                 var literal_target_id = extras.target_id + '::';
@@ -149,13 +150,13 @@ ContentByHashLib.LOADED_MEDIA = {};
                 LOADED_URLS[ $target.attr('id') ] = address;
                 if (extras && extras.by_hash) URL_LOADED_BY_HASH[ $target.attr('id') ] = true;
             }
+            if ($.isFunction( extras.success_callback )) try {
+                extras.success_callback.call(extras);
+            } catch(e) { carp('Failed success callback (load_content)', e, extras) };
             $target.trigger('content_added', extras);
         }
         $(document).trigger('dec_loading');
         PAGE_CHANGED++;
-        if ($.isFunction( extras.custom_success )) try {
-            extras.custom_success(extras);
-        } catch(e) { carp('Failed success callback (load_content)', e, extras) };
     }
     
     // argument is a LOAD_BUF item
@@ -211,6 +212,24 @@ ContentByHashLib.LOADED_MEDIA = {};
     }
     ContentByHashLib.inject_error_message = inject_error_message;
     
+    // get new index to LOAD_BUF (the request queue), either on its end (normal) or at the beginning (is_priority == true)
+    function alloc_loadbuf(is_priority) {
+        if (MIN_LOAD == undefined || MAX_LOAD+1 < MIN_LOAD) {
+            return (MIN_LOAD = ++MAX_LOAD);
+        }
+        if (is_priority) {
+            if (LOAD_BUF[MIN_LOAD] == undefined) return MIN_LOAD;
+            if (MIN_LOAD <= 0) {
+                carp(new Error('Cannot make a priority request when request queue is not empty before first request has been finished.'));
+                return ++MAX_LOAD;
+            }
+            return --MIN_LOAD;
+        }
+        else {
+            return ++MAX_LOAD;
+        }
+    }
+    
     // Check if the least present request has finished and if so, shift it
     // from the queue and render the results, and then call itself recursively.
     // This effectively renders all finished requests from the first up to the
@@ -238,6 +257,9 @@ ContentByHashLib.LOADED_MEDIA = {};
         var $target = $('#'+info.target_id);
         if ($target && $target.jquery && $target.length) {} else {
             carp('Could not find target element: #'+info.target_id);
+            if ($.isFunction(info.error_callback)) try {
+                info.error_callback.call(info);
+            } catch(e) { carp('Failed error callback (load_content)', e, info); }
             $(document).trigger('dec_loading');
             draw_ready();
             return;
@@ -289,8 +311,7 @@ ContentByHashLib.LOADED_MEDIA = {};
         
         var url = prepend_base_path_to(address);
         url = $('<a>').attr('href', url).get(0).href;
-        var load_id = ++MAX_LOAD;
-        if (MIN_LOAD == undefined || load_id < MIN_LOAD) MIN_LOAD = load_id;
+        var load_id = alloc_loadbuf(arg.is_priority_request);
         LOAD_BUF[ load_id ] = {
             target_id: target_id,
             address: address
@@ -301,7 +322,7 @@ ContentByHashLib.LOADED_MEDIA = {};
             type: 'GET',
             complete: function(xhr) {
                 LOAD_BUF[ this.load_id ].xhr = xhr;
-                if ($.httpSuccess(xhr)) {
+                if (this.succeeded) {
                     this._success(xhr);
                 }
                 else {
@@ -317,8 +338,11 @@ ContentByHashLib.LOADED_MEDIA = {};
                 else {
                     LOAD_BUF[ this.load_id ].data = xhr.responseText;
                 }
-                if (this.custom_success) {
-                    LOAD_BUF[ this.load_id ].custom_success = this.custom_success;
+                if (this.success_callback) {
+                    LOAD_BUF[ this.load_id ].success_callback = this.success_callback;
+                }
+                if (this.error_callback) {
+                    LOAD_BUF[ this.load_id ].error_callback = this.error_callback;
                 }
                 draw_ready();
             },
@@ -327,14 +351,14 @@ ContentByHashLib.LOADED_MEDIA = {};
                 cancel_request( this.load_id );
                 $(document).trigger('load_content_failed', [xhr]);
                 draw_ready();
-                if (this.custom_error) try {
-                    this.custom_error(xhr);
+                if ($.isFunction(this.error_callback)) try {
+                    this.error_callback();
                 } catch(e) { carp('Failed error callback (load_content)', e, this); }
             },
             load_id: load_id,
             request_no: MAX_REQUEST,
-            custom_success: arg.success_callback,
-            custom_error: arg.error_callback,
+            success_callback: arg.success_callback,
+            error_callback: arg.error_callback,
             original_options: arg
         });
     }
