86d98d42ffe569f7229e9e337b6100e1c599cbd6
Jakub Vysoky <jakub@borka.cz>
2009-09-03 20:28:31 +0200

    sql typo

--- doc/old/sql/ella.schemas/remove.data.sql
+++ doc/old/sql/ella.schemas/remove.data.sql
@@ -71,7 +71,7 @@ delete from core_placement where target_id in (68) and target_ct_id = 55;
 update articles_article set photo_id = null where id in (485283);
 -- autori
 -- select distinct author_id from articles_article_authors where author_id not in (select id from core_author);
-update articles_article_authors set author_id = 1 where id in (313, 375, 347);
+update articles_article_authors set author_id = 1 where author_id in (313, 375, 347);
 
 
 -- a pak uz jenom
