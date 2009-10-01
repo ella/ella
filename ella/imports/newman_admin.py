53d1d38f913889420d27b351dd953d6c6c3b965e
Vitek Pliska <whit@jizak.cz>
2009-10-01 12:31:26 +0200

    Updated newman options for ella.imports.

--- ella/imports/newman_admin.py
+++ ella/imports/newman_admin.py
@@ -1,3 +1,4 @@
+from django.conf.urls.defaults import patterns, url
 import logging
 logger = logging.getLogger("ella.imports")
 
@@ -8,33 +9,34 @@ from ella.core.cache.utils import get_cached_object_or_404
 
 from ella.imports.models import Server, ServerItem
 
-class ServerOptions(newman.NewmanModelAdmin):
+class ServerAdmin(newman.NewmanModelAdmin):
     list_display = ('title', 'domain', 'url', 'regenerate')
     search_fields = ('domain, title', 'url',)
     prepopulated_fields = {'slug' : ('title',)}
 
-    def __call__(self, request, url):
-        """Defines "*/fetch" page which starts the import procedure and shows the result status."""
-        ### FIXME: fetch does not work with newman!
-        if url and url.endswith('fetch'):
-            pk = url.split('/')[-2]
-            ### pk = url.split('#')[-1].split('/')[-2]
-            
-            server = get_cached_object_or_404(Server, pk=pk)
-            try:
-                server.fetch()
-                return http.HttpResponse('OK')
-            except Exception, e:
-                return http.HttpResponse('KO ' + str(e))
+    def get_urls(self):
+        urls = patterns('',
+            url(r'^(.+)/fetch/$',
+                self.fetch_view,
+                name='server-fetch'),
+        )
+        urls += super(ServerAdmin, self).get_urls()
+        return urls
 
-        return super(ServerOptions, self).__call__(request, url)
+    def fetch_view(self, request, pk, extra_context=None):
+        server = get_cached_object_or_404(Server, pk=pk)
+        try:
+            server.fetch()
+            return http.HttpResponse('OK')
+        except Exception, e:
+            return http.HttpResponse('KO ' + str(e))
 
-class ServerItemOptions(newman.NewmanModelAdmin):
+class ServerItemAdmin(newman.NewmanModelAdmin):
     list_display = ('title', 'server', 'updated','priority')
     list_filter = ('server', 'updated',)
     raw_id_fields = ('photo',)
 
 
-newman.site.register(Server, ServerOptions)
-newman.site.register(ServerItem, ServerItemOptions)
+newman.site.register(Server, ServerAdmin)
+newman.site.register(ServerItem, ServerItemAdmin)
 
