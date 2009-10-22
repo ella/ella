56131219d2befbc0d10eea25ceda93efeca89362
Honza Kral <honza.kral@gmail.com>
2009-10-22 19:53:02 +0200

    We do not need out own form

--- ella/ellacomments/__init__.py
+++ ella/ellacomments/__init__.py
@@ -3,11 +3,11 @@ Change the attributes you want to customize
 """  
      
 from threadedcomments.models import ThreadedComment
-from ella.ellacomments.forms import EllaCommmentForm
+from threadedcomments.forms import ThreadedCommentForm
      
 def get_model():
     return ThreadedComment
      
 def get_form():
-    return EllaCommmentForm
+    return ThreadedCommentForm
 
