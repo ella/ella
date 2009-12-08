984bbeed375607159cb637599be2c904005f1420
Honza Kral <honza.kral@gmail.com>
2009-12-08 15:23:43 +0100

    Moved admin fields to admin definitions

--- ella/positions/admin.py
+++ ella/positions/admin.py
@@ -4,6 +4,31 @@ from ella.ellaadmin.options import EllaAdminOptionsMixin
 from ella.positions.models import Position
 
 class PositionOptions(EllaAdminOptionsMixin, admin.ModelAdmin):
+    def show_title(self):
+        if not self.target:
+            return '-- %s --' % ugettext('empty position')
+        else:
+            return u'%s [%s]' % (self.target.title, ugettext(self.target_ct.name),)
+    show_title.short_description = _('Title')
+
+    def is_filled(self):
+        if self.target:
+            return True
+        else:
+            return False
+    is_filled.short_description = _('Filled')
+    is_filled.boolean = True
+
+    def is_active(self):
+        if self.disabled:
+            return False
+        now = datetime.now()
+        active_from = not self.active_from or self.active_from <= now
+        active_till = not self.active_till or self.active_till > now
+        return active_from and active_till
+    is_active.short_description = _('Active')
+    is_active.boolean = True
+
     list_display = ('name', 'category', 'box_type', 'is_active', 'is_filled', 'show_title', 'disabled',)
     list_filter = ('category', 'name', 'disabled', 'active_from', 'active_till',)
     search_fields = ('box_type', 'text',)
--- ella/positions/newman_admin.py
+++ ella/positions/newman_admin.py
@@ -38,6 +38,32 @@ class PositionAdmin(newman.NewmanModelAdmin):
 
     suggest_fields = {'category': ('__unicode__', 'title', 'slug', 'tree_path',),}
 
+    def show_title(self):
+        if not self.target:
+            return '-- %s --' % ugettext('empty position')
+        else:
+            return u'%s [%s]' % (self.target.title, ugettext(self.target_ct.name),)
+    show_title.short_description = _('Title')
+
+    def is_filled(self):
+        if self.target:
+            return True
+        else:
+            return False
+    is_filled.short_description = _('Filled')
+    is_filled.boolean = True
+
+    def is_active(self):
+        if self.disabled:
+            return False
+        now = datetime.now()
+        active_from = not self.active_from or self.active_from <= now
+        active_till = not self.active_till or self.active_till > now
+        return active_from and active_till
+    is_active.short_description = _('Active')
+    is_active.boolean = True
+
+
     def get_urls(self):
         urlpatterns = patterns('',
             url(r'^category/(\d+)/$',
