456767d889729674b3b388ba8c2135f5a9d8dc4d
Jakub Vysoky <jakub@borka.cz>
2009-12-09 11:53:27 +0100

    we use four-digit version

--- djangobaselibrary/__init__.py
+++ djangobaselibrary/__init__.py
@@ -1,5 +1,5 @@
 
-VERSION = (0, 0, 0)
+VERSION = (0, 0, 0, 0)
 
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import djangobaselibrary
 # all fields marked with TODO: REPLACE
 # must be filled with some meanigful values
 
-VERSION = (0, 0, 0)
+VERSION = (0, 0, 0, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
