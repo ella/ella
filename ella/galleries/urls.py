de6043dec44bc4ef7bcb76b17ea7f74ae14f9165
Honza Kral <honza.kral@gmail.com>
2009-12-25 00:41:45 +0100

    Migrated galleries to use new resolver
    Also dropped the 'items' from item url

--- ella/galleries/urls.py
+++ ella/galleries/urls.py
@@ -1,10 +1,17 @@
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext
+from django.conf.urls.defaults import patterns, url
 
-from ella.galleries.views import gallery_item_detail, items
+from ella.galleries.views import gallery_item_detail
 from ella.galleries.models import Gallery
-from ella.core.custom_urls import dispatcher
+from ella.core.custom_urls import resolver, dispatcher
+
+
+urlpatterns = patterns('',
+    url(r'^(?P<item_slug>[\w-]+)/$', gallery_item_detail, name='gallery-item-detail'),
+)
+
+resolver.register(urlpatterns, model=Gallery)
 
 dispatcher.register_custom_detail(Gallery, gallery_item_detail)
-dispatcher.register(slugify(ugettext('items')), items, model=Gallery)
 
