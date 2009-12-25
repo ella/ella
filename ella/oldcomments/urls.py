d30a46ceba06896900d755b2a442f8933fb1805d
Honza Kral <honza.kral@gmail.com>
2009-12-25 01:40:38 +0100

    Migrated oldcomments to use resolver
    UNTESTED

--- ella/oldcomments/urls.py
+++ ella/oldcomments/urls.py
@@ -1,8 +1,18 @@
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
+from django.conf.urls.defaults import patterns, url
 
 from ella.core.custom_urls import dispatcher
-from ella.oldcomments.views import comments_custom_urls
+from ella.oldcomments.views import new_comment, CommentFormPreview, list_comments
+from ella.oldcomments.forms import CommentForm
+
+urlpatterns = patterns('',
+    url(r'^$', list_comments, name='comments-list'),
+    url(r'^%s/(?P<reply>\d+)/$' % slugify(_('reply')), new_comment, name='comments-reply'),
+    url(r'^%s/$' % slugify(_('new')), new_comment, name='comments-new'),
+    url(r'^%s/$' % slugify(_('preview')), CommentFormPreview(CommentForm), name='comments-reply'),
+
+)
 
 dispatcher.register(slugify(_('comments')), comments_custom_urls)
 
