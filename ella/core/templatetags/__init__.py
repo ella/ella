bb8f3b35ea17a66e31535d226762a0088cfa317f
Vitek Pliska <whit@jizak.cz>
2010-01-05 17:35:47 +0100

    Added custom_url to built-in tags.

--- ella/core/templatetags/__init__.py
+++ ella/core/templatetags/__init__.py
@@ -4,6 +4,7 @@ from django import template
 template.add_to_builtins('ella.core.templatetags.core')
 # keep this here for backwards compatibility
 template.add_to_builtins('ella.core.templatetags.related')
+# and custom urls
+template.add_to_builtins('ella.core.templatetags.custom_urls_tags')
 # and the same for i18n
 template.add_to_builtins('django.templatetags.i18n')
-
