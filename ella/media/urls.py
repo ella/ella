65ef1ff640456d4521416fdeb2dd72d618d16efa
Vitek Pliska <whit@jizak.cz>
2010-02-01 14:48:34 +0100

    playlist url added

--- ella/media/urls.py
+++ ella/media/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
 from ella.core.custom_urls import resolver
-from ella.media.views import player_playlist_for_id
+from ella.media.views import player_playlist_for_id, player_playlist
 
 urlpatterns = patterns('',
-    url(r'^playlist/(\d+)/$', player_playlist_for_id, name='ella-media-playlist'),
+    url(r'^$', player_playlist, name='ella-media-playlist'),
+    url(r'^playlist/(\d+)/$', player_playlist_for_id, name='ella-media-playlist-for-id'),
 )
 
 resolver.register(urlpatterns, prefix='playlist')
-
