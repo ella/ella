d8200222aaa9df934570256f6761df114f74c0d2
Vitek Pliska <whit@jizak.cz>
2010-07-21 14:35:44 +0200

    articles.InfoBox box_class.

--- ella/articles/models.py
+++ ella/articles/models.py
@@ -6,10 +6,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from ella.core.models import Publishable
 from ella.core.cache import get_cached_list
+from ella.core.box import Box
 
 
 class InfoBox(models.Model):
     """Defines embedable text model."""
+
+    box_class = Box
+
     title = models.CharField(_('Title'), max_length=255)
     created = models.DateTimeField(_('Created'), default=datetime.now, editable=False)
     updated = models.DateTimeField(_('Updated'), blank=True, null=True)
@@ -22,6 +26,7 @@ class InfoBox(models.Model):
         ordering = ('-created',)
         verbose_name = _('Info box')
         verbose_name_plural = _('Info boxes')
+
 
 class Article(Publishable):
     """Defines article model."""
