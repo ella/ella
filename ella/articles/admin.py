bab233f66202adba797c958ff8e065d81d4d0dde
Petr Praus <petr@praus.net>
2010-12-29 14:06:42 +0100

    ella.articles.admin now has it's own PlacementInlineAdmin.
    ella.core.admin.PlacementInlineAdmin has newman as a dependency.

--- ella/articles/admin.py
+++ ella/articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from ella.core.admin import PlacementInlineAdmin, PublishableAdmin
+from ella.core.admin import PublishableAdmin, PlacementForm
+from ella.core.models import Placement
 from ella.articles.models import ArticleContents, Article, InfoBox
 from ella.ellaadmin.options import EllaAdminOptionsMixin, EllaModelAdmin, EllaAdminInline
 
@@ -16,6 +17,14 @@ class InfoBoxAdmin(EllaAdminOptionsMixin, EllaModelAdmin):
     list_filter = ('created', 'updated',)
     search_fields = ('title', 'content',)
     rich_text_fields = {None: ('content',)}
+
+class PlacementInlineAdmin(admin.TabularInline):
+    exclude = ('slug',)
+    model = Placement
+    max_num = 1
+    suggest_fields = {'category': ('__unicode__', 'title', 'slug',)}
+    
+    form = PlacementForm
 
 class ArticleAdmin(PublishableAdmin):
     ordering = ('-created',)
