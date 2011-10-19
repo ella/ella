76c2ac8337718e754332c77021195ec9e7686d35
Ales Bublik <ales.bublik@gmail.com>
2011-10-19 17:30:51 +0200

    Use new class based view

--- ella/ellatagging/urls.py
+++ ella/ellatagging/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext # lazy gettext works strange in case of urlpatterns (sometimes doesn't work)
 
-from ella.ellatagging.views import tagged_publishables
+from ella.ellatagging.views import TaggedPublishablesView
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<tag>[\w\s]+)/', tagged_publishables, name="tag_list"),
+    url(r'^(?P<tag>[\w\s]+)/', TaggedPublishablesView.as_view(), name="tag_list"),
 )
