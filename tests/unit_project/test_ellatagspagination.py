67243d93695c21d5a473753a48cab390d161a897
Ales Bublik <ales.bublik@gmail.com>
2011-11-01 15:20:39 +0100

    Added test for page=2 and renamed tests to be more descriptive

--- tests/unit_project/test_ellatagspagination.py
+++ tests/unit_project/test_ellatagspagination.py
@@ -41,19 +41,16 @@ class TestTaggingPagination(DatabaseTestCase):
         self.place = Placement.objects.create(
             publishable=self.pub,
             category=self.category_nested,
-            publish_from=datetime(2008, 1, 10)
+            publish_from=datetime(2008, 1, 20)
             )
         Tag.objects.update_tags(self.only_pub, 'tag1')
         Tag.objects.update_tags(self.only_publishable, 'tag1')
         self.tag = Tag.objects.get(name='tag1')
 
-
     def tearDown(self):
         super(TestTaggingPagination, self).tearDown()
-        template_loader.templates = {}
 
-
-    def test_tagged_paggination_view(self):
+    def test_tagged_paggination_view_without_page_parameter(self):
         url = reverse('tag_list', kwargs={'tag': 'tag1'})
         request = self.factory.get(url)
         tpv = TaggedPublishablesView.as_view()
@@ -63,9 +60,11 @@ class TestTaggingPagination(DatabaseTestCase):
         self.assert_equals(1, response.context_data['results_per_page'])
         self.assert_equals(self.tag, response.context_data['tag'])
         self.assert_equals(1, len(response.context_data['object_list']))
+        self.assert_equals(self.only_publishable,
+                           response.context_data['object_list'][0])
 
 
-    def test_tagged_paggination_view_page1(self):
+    def test_tagged_paggination_view_with_page1_parameter(self):
         url = reverse('tag_list', kwargs={'tag': 'tag1'})
         request = self.factory.get(url, {'p': 1})
         tpv = TaggedPublishablesView.as_view()
@@ -75,4 +74,19 @@ class TestTaggingPagination(DatabaseTestCase):
         self.assert_equals(1, response.context_data['results_per_page'])
         self.assert_equals(self.tag, response.context_data['tag'])
         self.assert_equals(1, len(response.context_data['object_list']))
+        self.assert_equals(self.only_publishable,
+                           response.context_data['object_list'][0])
 
+
+    def test_tagged_paggination_view_with_page2_parameter(self):
+        url = reverse('tag_list', kwargs={'tag': 'tag1'})
+        request = self.factory.get(url, {'p': 2})
+        tpv = TaggedPublishablesView.as_view()
+        response = tpv(request, tag='tag1')
+        self.assert_true(200, response.status_code)
+        self.assert_true(response.context_data['is_paginated'])
+        self.assert_equals(1, response.context_data['results_per_page'])
+        self.assert_equals(self.tag, response.context_data['tag'])
+        self.assert_equals(1, len(response.context_data['object_list']))
+        self.assert_equals(self.only_pub,
+                           response.context_data['object_list'][0])
