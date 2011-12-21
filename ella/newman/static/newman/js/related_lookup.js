a35880846fa4e551cd0fd86945b7e54dae6f2350
Oldrich Kruza <sixtease+github@gmail.com>
2011-12-22 00:17:24 +0100

    Photo mass upload now redirects correctly after save; fixes #26
    In magnifier-glass add-object overlays,
    a button that pushes to address stack (NewmanLib.ADR_STACK) can now have the
    'js-custom-adrstack-callbacks'. This will prevent the default callbacks to be
    attached.
    (those save the ID of the added object and insert it to the original form)
    
    The button that opens mass-upload now uses this special-casing mechanism.
    
    The mass-upload onAllComplete event now calls newman's standard success-handler
    instead of just redirecting to /photos/photo/

--- ella/newman/static/newman/js/related_lookup.js
+++ ella/newman/static/newman/js/related_lookup.js
@@ -200,15 +200,16 @@ log_lookup = new LoggingLib('RELATED LOOKUP:', true);
 
 // Adding objects with lupička
 (function($) {
-    $('.js-adrstack-push').live('click', function(evt) {
+    function default_adrstack_push_callback(evt) {
         if (evt.button != 0) return;
         evt.preventDefault();
-        NewmanLib.ADR_STACK.push( {
-            from: get_hashadr(''),
-            to: get_hashadr($(this).attr('href')),
-            selection_callback: $(Kobayashi.closest_loaded(this).container).data('selection_callback'),
-            form_data: JSON.stringify({ data: $('.change-form').serializeArray() }),
-            onsave: function(popped, action_table_obj) {
+        
+        var onsave_callback, onreturn_callback;
+        if ($(this).is('.js-custom-adrstack-callbacks')) {
+            // Define no callbacks
+        }
+        else {
+            onsave_callback = function(popped, action_table_obj) {
                 if (!action_table_obj.vars.object_id) {
                     ADR_STACK = [];
                     log_lookup.log('Did not get ID of newly added object -- breaking ADR_STACK');
@@ -216,19 +217,35 @@ log_lookup = new LoggingLib('RELATED LOOKUP:', true);
                 }
                 popped.oid = action_table_obj.vars.object_id;
                 popped.str = action_table_obj.vars.object_title;
-            },
-            onreturn: function(popped, action_table_obj) {
+            };
+            onreturn_callback = function(popped, action_table_obj) {
                 $(document).one('content_added', function(evt) {
                     NewmanLib.restore_form(popped.form_data, $('.change-form'), {});
                 });
                 $(document).one('media_loaded', function(){popped.selection_callback(popped.oid,{str: popped.str});});
-            }
+            };
+        }
+        
+        NewmanLib.ADR_STACK.push( {
+            from: get_hashadr(''),
+            to: get_hashadr($(this).attr('href')),
+            selection_callback: $(Kobayashi.closest_loaded(this).container).data('selection_callback'),
+            form_data: JSON.stringify({ data: $('.change-form').serializeArray() }),
+            onsave: onsave_callback,
+            onreturn: onreturn_callback
         } );
+        
         if (adr( $(this).attr('href'), {just_get: 'hash'} ) == location.hash) {
             Kobayashi.reload_content(Kobayashi.DEFAULT_TARGET);
         }
         else {
             adr( $(this).attr('href') );
         }
-    });
+    }
+    $('.js-adrstack-push').live('click', default_adrstack_push_callback);
+    /*$('.js-adrstack-push.js-custom-adrstack-callbacks.js-multi-upload').live('click', function(evt) {
+        // FIXME: Add all the mass-uploaded photos to the gallery.
+        // Would have to collect photo IDs from mass_upload.html
+        // and write custom onsave and onreturn callbacks. #27
+    });*/
 })(jQuery);
