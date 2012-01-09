4bfb7eed28943eb6891e131df29be320ca72c2c2
Honza Kral <honza.kral@gmail.com>
2012-01-09 14:32:28 -0800

    removed legacy unused tests

--- tests/regressiontests/comments/commenttest/sample/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from commenttest.sample.views import *
-
-
-urlpatterns = patterns('',
-        url(r'^list/apples/$', list_apples),
-        url(r'^list/apples/(?P<id>[^/]+)/$', list_apple),
-)
-
