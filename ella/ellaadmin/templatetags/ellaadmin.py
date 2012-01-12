b06da4122069d7c5d943fe19edfdf7fe83d3767a
Honza Kral <honza.kral@gmail.com>
2012-01-11 23:40:28 -0800

    still some leftover HitCount business

--- ella/ellaadmin/templatetags/ellaadmin.py
+++ ella/ellaadmin/templatetags/ellaadmin.py
@@ -17,16 +17,6 @@ class InsertModels(template.Node):
 def insert_admin_apps(parser, token):
     return InsertModels()
 
-@register.tag
-def get_hits_link(parser, token):
-    try:
-        tag_name, o_id = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, \
-            '"%s" tag requires exactly two arguments - content_type_id and object_id.' % token.contents.split()[0]
-    return FormatHitsLink(o_id)
-
-
 class TagSuggesterUrl(template.Node):
     def render(self, context):
         from django.core.urlresolvers import reverse
@@ -36,32 +26,3 @@ class TagSuggesterUrl(template.Node):
 def tag_suggester_url(parser, token):
     return TagSuggesterUrl()
 
-
-class FormatHitsLink(template.Node):
-    def __init__(self, target_id):
-        self.target_id = target_id
-
-    def render(self, context):
-        from django.utils.safestring import mark_safe
-        from ella.core.cache import get_cached_list
-        from ella.core.models import HitCount, Placement
-        try:
-            oid = template.Variable(self.target_id).resolve(context)
-            # TODO: this improve speed but it's dirty
-            #if not tct in [16,28,29,55,32]:
-            #    return ''
-            hl = get_cached_list(
-                    Placement,
-                    publishable=oid
-            )
-            l = ''
-            for h in hl:
-                hit_count = HitCount.objects.get(placement=h)
-                if not hit_count:
-                    continue
-                l += '<li><a href="../../../core/hitcount/%d/" class="viewsitelink">Hits: %d</a></li>' \
-                % (hit_count._get_pk_val(), hit_count.hits)
-            # return mark_safe(l)
-            return mark_safe(u'<!--  here is hidden hitcount button:  %s -->' % l )
-        except (HitCount.DoesNotExist, template.VariableDoesNotExist):
-            return ''
