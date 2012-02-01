3483efb50bf1b06201c604df3b2689c4d762ddb1
xaralis <filip.varecha@fragaria.cz>
2012-02-01 08:47:16 -0500

    Removed tagging from requirements

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import setup, find_packages
 import ella
 
 setup(
-    name = 'ella',
-    version = ella.__versionstr__,
-    description = 'Ella - Django powered CMS',
-    long_description = '\n'.join((
+    name='ella',
+    version=ella.__versionstr__,
+    description='Ella - Django powered CMS',
+    long_description='\n'.join((
         'Ella Django CMS Project',
         '',
         'content management system written in Django',
         '',
     )),
-    author = 'Ella Development Team',
+    author='Ella Development Team',
     author_email='dev@ella-cms.com',
-    license = 'BSD',
+    license='BSD',
     url='http://ella.github.com/',
 
-    packages = find_packages(
-        where = '.',
-        exclude = ('doc', 'test_ella', 'debian',)
+    packages=find_packages(
+        where='.',
+        exclude=('doc', 'test_ella', 'debian',)
     ),
 
-    include_package_data = True,
+    include_package_data=True,
 
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -36,15 +36,14 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires = [
+    install_requires=[
         'setuptools>=0.6b1',
         'Django==1.3.1',
         'south>=0.7',
         'anyjson',
         'feedparser',
-        'django-tagging',
     ],
-    setup_requires = [
+    setup_requires=[
         'setuptools_dummy',
     ],
     test_suite='test_ella.run_tests.run_all'
