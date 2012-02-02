8ae6fafddf20cb4013ab53d9edd52194100470da
xaralis <filip.varecha@fragaria.cz>
2012-02-02 03:16:19 -0500

    Updated conf.py for sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 import sys, os
 
+from django.core.management import setup_environ
+
+import ella
+from test_ella import settings
+
+setup_environ(settings)
+
 extensions = ['sphinx.ext.autodoc']
 templates_path = ['.templates']
 source_suffix = '.rst'
@@ -9,8 +16,6 @@ master_doc = 'index'
 # General information about the project.
 project = 'Ella'
 copyright = '2012 Ella team'
-
-import ella
 
 version = ".".join(map(str, ella.__version__[:-1]))
 # The full version, including alpha/beta/rc tags.
