b192b4831e35bbe867f76d8a006c589806f61b6d
Honza Kral <honza.kral@gmail.com>
2012-04-02 18:44:43 +0200

    create_photo can also be usefull outside of ella

--- ella/utils/test_helpers.py
+++ ella/utils/test_helpers.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from tempfile import mkstemp
+from PIL import Image
+from cStringIO import StringIO
 
 from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
+from ella.photos.models import Photo
 from ella.core.models import Category, Publishable
 # choose Article as an example publishable
 from ella.articles.models import Article
@@ -34,8 +39,8 @@ def create_basic_categories(case):
     )
     case.addCleanup(Category.objects.clear_cache)
 
-def create_and_place_a_publishable(case):
-    case.publishable = Article.objects.create(
+def create_and_place_a_publishable(case, **kwargs):
+    defaults = dict(
         title=u'First Article',
         slug=u'first-article',
         description=u'Some\nlonger\ntext',
@@ -44,5 +49,42 @@ def create_and_place_a_publishable(case):
         published=True,
         content='Some even longer test. \n' * 5
     )
+    defaults.update(kwargs)
+    case.publishable = Article.objects.create(**defaults)
     case.only_publishable = Publishable.objects.get(pk=case.publishable.pk)
 
+def create_photo(case, **kwargs):
+    # prepare image in temporary directory
+    case.image_file_name = mkstemp(suffix=".jpg", prefix="ella-photo-tests-")[1]
+    case.image = Image.new('RGB', (200, 100), "black")
+    case.image.save(case.image_file_name, format="jpeg")
+
+    f = open(case.image_file_name)
+    file = StringIO(f.read())
+    f.close()
+
+    f = InMemoryUploadedFile(
+            file = file,
+            field_name = 'image',
+            name = 'example-photo.jpg',
+            content_type = 'image/jpeg',
+            size = len(file.getvalue()),
+            charset = None
+        )
+
+    data = dict(
+        title = u"Example 中文 photo",
+        slug = u"example-photo",
+        height = 200,
+        width = 100,
+    )
+    data.update(kwargs)
+
+    case.photo = Photo(**data)
+    image_field = Photo._meta.get_field('image')
+    image_field.save_form_data(case.photo, f)
+
+    case.photo.save()
+    case.photo._pil_image = case.image
+
+    return case.photo
--- test_ella/test_photos/fixtures.py
+++ test_ella/test_photos/fixtures.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-from tempfile import mkstemp
-from PIL import Image
-from cStringIO import StringIO
 
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from ella.photos.models import Format, Photo
+from ella.photos.models import Format
+from ella.utils.test_helpers import create_photo
 
 __all__ = ("create_photo_formats",)
 
@@ -25,39 +22,3 @@ def create_photo_formats(case):
     case.basic_format.sites.add(Site.objects.get(pk=getattr(settings, "SITE_ID", 1)))
     case.basic_format.save()
 
-# "fixtures" aka example photo
-def create_photo(case, **kwargs):
-    # prepare image in temporary directory
-    case.image_file_name = mkstemp(suffix=".jpg", prefix="ella-photo-tests-")[1]
-    case.image = Image.new('RGB', (200, 100), "black")
-    case.image.save(case.image_file_name, format="jpeg")
-
-    f = open(case.image_file_name)
-    file = StringIO(f.read())
-    f.close()
-
-    f = InMemoryUploadedFile(
-            file = file,
-            field_name = 'image',
-            name = 'example-photo.jpg',
-            content_type = 'image/jpeg',
-            size = len(file.getvalue()),
-            charset = None
-        )
-
-    data = dict(
-        title = u"Example 中文 photo",
-        slug = u"example-photo",
-        height = 200,
-        width = 100,
-    )
-    data.update(kwargs)
-
-    case.photo = Photo(**data)
-    image_field = Photo._meta.get_field('image')
-    image_field.save_form_data(case.photo, f)
-
-    case.photo.save()
-    case.photo._pil_image = case.image
-
-    return case.photo
