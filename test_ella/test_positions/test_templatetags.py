db2162f70eae4ad543a1fe410fcb03486f41c212
michal dub <michal.dub@sanomamedia.cz>
2012-06-08 13:03:22 +0200

    added some new tests

--- test_ella/test_positions/test_templatetags.py
+++ test_ella/test_positions/test_templatetags.py
@@ -35,13 +35,23 @@ class TestPositionParsing(TestCase):
     def test_getting_category_for_templatag_from_category_variable(self):
         tools.assert_equals(_get_category_from_pars_var(template.Variable('category'), self.context), self.category)
 
+    def test_raising_exception_for_templatag_if_category_is_not_in_context(self):
+        tools.assert_raises(template.VariableDoesNotExist, _get_category_from_pars_var, template.Variable('category'), Context({}))
+
     def test_getting_category_for_templatag_from_category_tree_path_in_variable(self):
         context = Context({'category': '%s' % (self.category.tree_path)})
         tools.assert_equals(_get_category_from_pars_var(template.Variable('category'), context), self.category)
 
+    def test_raising_exception_for_templatag_if_no_category_for_tree_path_in_variable(self):
+        context = Context({'category': '%s' % ("bad_three_path")})
+        tools.assert_raises(Category.DoesNotExist, _get_category_from_pars_var, template.Variable('category'), context)
+
     def test_getting_category_for_templatag_from_category_tree_path(self):
         tools.assert_equals(_get_category_from_pars_var(template.Variable('"%s"' % (self.category.tree_path)), self.context), self.category)
         tools.assert_equals(_get_category_from_pars_var(template.Variable('"%s"' % (self.category_nested.tree_path)), self.context), self.category_nested)
+
+    def test_raising_exception_for_templatag_if_no_category_for_tree_path(self):
+        tools.assert_raises(Category.DoesNotExist, _get_category_from_pars_var, template.Variable('"bad_three_path"'), self.context)
 
     def test_parsing_position_tag(self):
         position_node = _parse_position_tag('position position-name for category'.split(), 'endposition')
@@ -72,22 +82,34 @@ class TestPositionParsing(TestCase):
         t = template.Template('{% load positions %}{% position position-name for category %}{% endposition %}')
         tools.assert_equals('', t.render(self.context))
 
-    def test_raise_position_templatetag_render_with_bad_category_tree_path(self):
+    def test_empty_position_templatetag_render_with_category_tree_path_if_position_does_not_exist(self):
+        t = template.Template('{% load positions %}{% position position-namee for "nested-category" %}{% endposition %}')
+        tools.assert_equals('', t.render(self.context))
+
+    def test_raising_exception_position_templatetag_render_with_bad_category_tree_path(self):
         t = template.Template('{% load positions %}{% position position-name for "nested-categoryy" %}{% endposition %}')
         tools.assert_raises(Category.DoesNotExist, t.render, self.context_for_category_nested)
 
+    def test_raising_exception_position_templatetag_render_with_category_not_defined(self):
+        t = template.Template('{% load positions %}{% position position-name for category %}{% endposition %}')
+        tools.assert_raises(template.VariableDoesNotExist, t.render, Context({}))
+
     def test_ifposition_templatetag_render_with_category_var(self):
-        t = template.Template('{% load positions %}{% ifposition position-name for category %}ON{% else %}OUT{% endifposition %}')
-        tools.assert_equals('ON', t.render(self.context_for_category_nested))
+        t = template.Template('{% load positions %}{% ifposition position-name for category %}IN{% else %}OUT{% endifposition %}')
+        tools.assert_equals('IN', t.render(self.context_for_category_nested))
 
     def test_ifposition_templatetag_render_with_category_tree_path(self):
-        t = template.Template('{% load positions %}{% ifposition position-name for "nested-category" %}ON{% else %}OUT{% endifposition %}')
-        tools.assert_equals('ON', t.render(self.context_for_category_nested))
+        t = template.Template('{% load positions %}{% ifposition position-name for "nested-category" %}IN{% else %}OUT{% endifposition %}')
+        tools.assert_equals('IN', t.render(self.context_for_category_nested))
 
     def test_not_position_for_ifposition_templatetag_render_with_category_var(self):
-        t = template.Template('{% load positions %}{% ifposition position-name for category %}ON{% else %}OUT{% endifposition %}')
+        t = template.Template('{% load positions %}{% ifposition position-name for category %}IN{% else %}OUT{% endifposition %}')
         tools.assert_equals('OUT', t.render(self.context))
 
-    def test_raise_ifposition_templatetag_render_with_bad_category_tree_path(self):
-        t = template.Template('{% load positions %}{% ifposition position-name for "nested-categoryy" %}ON{% else %}OUT{% endifposition %}')
+    def test_raising_exception_for_ifposition_templatetag_render_with_category_var_not_in_context(self):
+        t = template.Template('{% load positions %}{% ifposition position-name for category %}IN{% else %}OUT{% endifposition %}')
+        tools.assert_raises(template.VariableDoesNotExist, t.render, Context({}))
+
+    def test_raising_exception_ifposition_templatetag_render_with_bad_category_tree_path(self):
+        t = template.Template('{% load positions %}{% ifposition position-name for "nested-categoryy" %}IN{% else %}OUT{% endifposition %}')
         tools.assert_raises(Category.DoesNotExist, t.render, self.context)
