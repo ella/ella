63847a3783679b5766bf5d21f8b889f435003562
Honza Kral <honza.kral@gmail.com>
2012-06-14 00:49:00 +0200

    Added {% image %} tag to replace {% img %} in the future
    Motivation is cleaner syntax and enabling format to be passed as a
    variable either containing the format name or the Format instance itself

--- ella/photos/templatetags/photos.py
+++ ella/photos/templatetags/photos.py
@@ -10,6 +10,75 @@ log = logging.getLogger('ella.photos')
 
 register = template.Library()
 
+class ImageTag(template.Node):
+    #{% image <photo_variable> in "format" as foobar %}
+    def __init__(self, format, photo, var_name):
+        self.format, self.photo, self.var_name = format, photo, var_name
+
+    def render(self, context):
+        if isinstance(self.format, template.Variable):
+            try:
+                format = self.format.resolve(context)
+                if isinstance(format, basestring):
+                    format = Format.objects.get_for_name(format)
+            except (template.VariableDoesNotExist, Format.DoesNotExist):
+                context[self.var_name] = None
+                return ''
+        else:
+            format = self.format
+
+        try:
+            # try retrieving just the ID first to avoid DB lookup
+            photo = template.Variable(self.photo + '_id').resolve(context)
+        except template.VariableDoesNotExist:
+            try:
+                photo = template.Variable(self.photo).resolve(context)
+            except template.VariableDoesNotExist:
+                context[self.var_name] = None
+                return ''
+
+        formated_photo = FormatedPhoto.objects.get_photo_in_format(photo, format)
+        context[self.var_name] = formated_photo
+        return ''
+
+def _parse_image(bits):
+    if len(bits) != 6 or bits[2] != 'in' or bits[4] != 'as':
+        raise template.TemplateSyntaxError('{% image <photo_variable> in "format" as foobar %}')
+
+    format = template.Variable(bits[3])
+    if format.literal is not None:
+        try:
+            format = Format.objects.get_for_name(format.literal)
+        except Format.DoesNotExist:
+            logmsg = "Format with name %r does not exist (for site id %d)" % (format.literal, settings.SITE_ID)
+            log.error(logmsg)
+
+            if not settings.TEMPLATE_DEBUG:
+                return template.Node()
+
+            raise template.TemplateSyntaxError(logmsg)
+
+    return ImageTag(format, bits[1], bits[5])
+
+@register.tag
+def image(parser, token):
+    """
+
+    Generates thumbnails for ``Photo`` instances.
+
+    syntax::
+
+        {% image <photo> in <format> as <var_name> %}
+
+    examples::
+
+        {% image article.photo in "thumbnail" as thumb %}
+        {% image article.photo in thumb_format as thumb %}
+
+    """
+    bits = token.split_contents()
+    return _parse_image(bits)
+
 class ImgTag(template.Node):
     def __init__(self, photo, format, var_name):
         self.photo, self.format, self.var_name = photo, format, var_name
@@ -40,23 +109,25 @@ class ImgTag(template.Node):
 @register.tag
 def img(parser, token):
     """
-    Generates thumbnails for ``Photo`` instances.
-    
+
+    Deprecated, use {% image %} instead. Generates thumbnails for ``Photo`` instances.
+
     syntax::
-        
+
         {% img <format> for <var> as <var_name> %}
         {% img <format> with <field_value> as <var_name> %} 
-    
+
     examples::
 
         {% img category_listing for object.photo as thumb %}
         {% img category_listing with pk 1150 as thumb %}
-        
+
     """
+    log.warning('You are using the deprecated {% img %} tag. please upgrade to {% image %}.')
     bits = token.split_contents()
     return _parse_img(bits)
 
-def _parse_img(bits):
+def _parse_img(bits, legacy=True):
     if len(bits) < 2 or bits[-2] != 'as':
         raise template.TemplateSyntaxError, "{% img FORMAT for VAR as VAR_NAME %} or {% img FORMAT with FIELD VALUE as VAR_NAME %}"
 
@@ -90,5 +161,4 @@ def _parse_img(bits):
         raise template.TemplateSyntaxError, "{% img FORMAT for VAR as VAR_NAME %} or {% img FORMAT with FIELD VALUE as VAR_NAME %}"
 
     return ImgTag(formated_photo, format, bits[-1])
-
 
--- test_ella/test_photos/test_templatetags.py
+++ test_ella/test_photos/test_templatetags.py
@@ -1,12 +1,58 @@
-from django.template import Node
+from django.template import Node, Context
 from django.test import TestCase
 from django.conf import settings
 
 from nose import tools, SkipTest
 
-from ella.photos.templatetags.photos import _parse_img, ImgTag
+from ella.photos.templatetags.photos import _parse_img, ImgTag, _parse_image, ImageTag
+from ella.photos.models import redis, REDIS_PHOTO_KEY, REDIS_FORMATTED_PHOTO_KEY
 
 from test_ella.test_photos.fixtures import create_photo_formats
+
+class TestImageParsing(TestCase):
+    def setUp(self):
+        super(TestImageParsing, self).setUp()
+        create_photo_formats(self)
+
+    def tearDown(self):
+        super(TestImageParsing, self).tearDown()
+        if redis:
+            redis.flushdb()
+
+    def test_format_is_resolved_if_literal_string(self):
+        image_node = _parse_image('image some_photo in "basic" as var_name'.split())
+        tools.assert_true(isinstance(image_node, ImageTag))
+        tools.assert_equals(self.basic_format, image_node.format)
+
+    def test_photo_id_and_format_picked_up_from_context(self):
+        if not redis:
+            raise SkipTest()
+        redis.hmset(REDIS_FORMATTED_PHOTO_KEY % (42, self.basic_format.pk), {'sentinel': 42})
+        redis.hmset(REDIS_PHOTO_KEY % 42, {'orig_sentinel': 42})
+
+        c = Context({'article': {'photo_id': 42, 'photo': 'not-42'}, 'image_format': self.basic_format})
+        image_node = _parse_image('image article.photo in image_format as var_name'.split())
+
+        tools.assert_true(isinstance(image_node, ImageTag))
+        tools.assert_equals('', image_node.render(c))
+        tools.assert_true('var_name' in c)
+        tools.assert_equals({'sentinel': '42', 'original': {'orig_sentinel': '42'}}, c['var_name'])
+
+
+    def test_photo_and_format_name_picked_up_from_context(self):
+        if not redis:
+            raise SkipTest()
+        redis.hmset(REDIS_FORMATTED_PHOTO_KEY % (42, self.basic_format.pk), {'sentinel': 42})
+        redis.hmset(REDIS_PHOTO_KEY % 42, {'orig_sentinel': 42})
+
+        c = Context({'article': {'photo': 42}, 'image_format': 'basic'})
+        image_node = _parse_image('image article.photo in image_format as var_name'.split())
+
+        tools.assert_true(isinstance(image_node, ImageTag))
+        tools.assert_equals('', image_node.render(c))
+        tools.assert_true('var_name' in c)
+        tools.assert_equals({'sentinel': '42', 'original': {'orig_sentinel': '42'}}, c['var_name'])
+
 
 class TestImgParsing(TestCase):
     def setUp(self):
