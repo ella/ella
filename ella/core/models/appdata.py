fee733a68e9aecb763daad506aec09e387a765ac
Honza Kral <honza.kral@gmail.com>
2012-08-05 16:04:11 +0200

    notes on what needs to change for app_data to work

--- ella/core/models/appdata.py
+++ ella/core/models/appdata.py
@@ -9,8 +9,10 @@ class AppDataField(JSONField):
     right `AppDataContainer` subclass is returned using
     `AppDataContainerFactory` object.
     """
+    # FIXME: add validation hooks
     def contribute_to_class(self, cls, name):
         super(AppDataField, self).contribute_to_class(cls, name)
+        # FIXME: this blows up if you have more than one app_data field, also possible name conflict
         cls.app_data_registry = NamespaceRegistry()
 
     def to_python(self, value):
@@ -26,15 +28,19 @@ class AppDataField(JSONField):
 
 class AppDataContainerFactory(dict):
     def __init__(self, model, *args, **kwargs):
+        # FIXME: you really want to be passing model instance if possible, not class
         self.model = model
         super(AppDataContainerFactory, self).__init__(*args, **kwargs)
 
     def __getitem__(self, name):
+        # FIXME: what if I want to store list, string or a number? don't default to something controlled, just raise KeyError
+        # FIXME: don't modify __getitem__, instead only wrap things in appropriate class in __getitem__ and use .get_or_create() for this logic.
         class_ = self.model.app_data_registry.get(name, AppDataContainer)
 
         if name in self:
             val = super(AppDataContainerFactory, self).__getitem__(name)
         else:
+            # FIXME: if not in self, add it there!
             val = {}
 
         return class_(self.model, val)
@@ -49,6 +55,7 @@ class AppDataContainer(dict):
         super(AppDataContainer, self).__init__(*args, **kwargs)
 
 
+# FIXME: don't extend dict, use dict internally to store the registry
 class NamespaceRegistry(dict):
     """
     Registry which is bound to each `AppDataMixin` class that holds registered
@@ -64,6 +71,8 @@ class NamespaceRegistry(dict):
         return namespace in self
 
     def register(self, namespace, class_):
+        # FIXME: add a global registry somewhere that also supports per-project overriding via settings
+        # FIXME: have the global registry allow apps to specify/override continers per model class or globally
         if namespace in self:
             raise self.NamespaceConflict(
                 'Namespace %r already assigned to class %r' % (
