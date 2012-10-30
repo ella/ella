d673afca315d4c97f52cdf3f4cc4dc5ab8b415d4
Honza Kral <honza.kral@gmail.com>
2012-10-30 16:42:43 +0100

    tests cleanup

--- test_ella/test_core/test_templatetags.py
+++ test_ella/test_core/test_templatetags.py
@@ -11,7 +11,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator
 from django.test import RequestFactory
 
-from ella.core.templatetags.core import listing_parse, ListingNode, _parse_box, BoxNode, EmptyNode
+from ella.core.templatetags.core import listing_parse, _parse_box, BoxNode, EmptyNode
 from ella.core.templatetags.pagination import _do_paginator
 from ella.core.models import Category
 from ella.core.managers import ListingHandler
@@ -19,8 +19,7 @@ from ella.articles.models import Article
 from ella.photos.models import Photo
 
 from test_ella.test_core import create_basic_categories, create_and_place_a_publishable, \
-        create_and_place_more_publishables, list_all_publishables_in_category_by_hour, \
-        create_and_place_two_publishables_and_listings
+        create_and_place_more_publishables, list_all_publishables_in_category_by_hour
 from test_ella import template_loader
 from test_ella.cases import RedisTestCase as TestCase
 
