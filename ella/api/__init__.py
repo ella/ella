10d1ee571c6d52213c7bbdf861c325c09209b954
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-01-18 04:40:58 -0200

    fix pep8 on api init: W391 blank line at end of file

--- ella/api/__init__.py
+++ ella/api/__init__.py
@@ -1,9 +1,9 @@
 from serialization import object_serializer, response_serializer, FULL
 from .conf import api_settings
 
+
 def render_as_api(request, obj):
     if api_settings.ENABLED:
         for mimetype in (a.split(';')[0] for a in request.META.get('HTTP_ACCEPT', '').split(',')):
             if response_serializer.serializable(mimetype):
                 return response_serializer.serialize(object_serializer.serialize(request, obj, FULL), mimetype)
-
