6531a175bcf0f88e8d131812385ae28510e68d39
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-01-18 04:41:31 -0200

    fix pep8 on api middleware: E302 expected 2 blank lines, found 1

--- ella/api/middleware.py
+++ ella/api/middleware.py
@@ -3,6 +3,7 @@ from django.utils.cache import patch_vary_headers
 
 from ella.api.conf import api_settings
 
+
 class VaryOnAcceptMiddleware(object):
     def __init__(self):
         if not api_settings.ENABLED:
