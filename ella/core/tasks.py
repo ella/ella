31a2dfef0fb365464361b74acb7148a05beb8210
lauramadsen <lwright@whalerock.com>
2015-02-19 12:15:42 -0800

    django 1.6 migration: use RQ instead of celery

--- ella/core/tasks.py
+++ ella/core/tasks.py
@@ -1,18 +1,18 @@
 """
-If celery is installed, register our maintenance commands as periodic tasks to
+If RQ is installed, register our maintenance commands as periodic jobs to
 avoid the requirement for external crond.
 """
 
 try:
     from datetime import timedelta
 
-    from celery.task import periodic_task
+    from scout.decorators import periodic_job
 
     from ella.core.management import generate_publish_signals, regenerate_listing_handlers
 
-    periodic_task(run_every=timedelta(minutes=5))(generate_publish_signals)
-    periodic_task(run_every=timedelta(hours=3))(regenerate_listing_handlers)
+    periodic_job(run_every=timedelta(minutes=5))(generate_publish_signals)
+    periodic_job(run_every=timedelta(hours=3))(regenerate_listing_handlers)
 except ImportError:
-    # celery not installed
+    # RQ not installed
     pass
 
