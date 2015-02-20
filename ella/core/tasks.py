104053cdad2496f18b32f54534a2093927bfed70
Luis Artola <luis@bermanbraun.com>
2015-02-20 11:58:42 -0800

    Reverting back to celery library to remove backward dependency of ella to scout.

--- ella/core/tasks.py
+++ ella/core/tasks.py
@@ -1,18 +1,18 @@
 """
-If RQ is installed, register our maintenance commands as periodic jobs to
+If celery is installed, register our maintenance commands as periodic tasks to
 avoid the requirement for external crond.
 """
 
 try:
     from datetime import timedelta
 
-    from scout.decorators import periodic_job
+    from celery.task import periodic_task
 
     from ella.core.management import generate_publish_signals, regenerate_listing_handlers
 
-    periodic_job(run_every=timedelta(minutes=5))(generate_publish_signals)
-    periodic_job(run_every=timedelta(hours=3))(regenerate_listing_handlers)
+    periodic_task(run_every=timedelta(minutes=5))(generate_publish_signals)
+    periodic_task(run_every=timedelta(hours=3))(regenerate_listing_handlers)
 except ImportError:
-    # RQ not installed
+    # celery not installed
     pass
 
